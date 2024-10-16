b824fc2b4e3cbc9b4497ff5f048bcd32cb3581cb
lfo <louis@dewolf.fr>
2024-10-16 13:11:26 +0200

    fix layout with scrollbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,16 @@
 import Navbar from '@/components/navbar/Navbar'
 import Providers from '@/services/providers/Providers'
+import classNames from 'classnames'
+import styles from './styles.module.css'
 
 interface Props {
   children: React.ReactNode
 }
 
 const NavLayout = ({ children }: Props) => (
-  <div className="flex-col">
+  <div className="flex-col h100">
     <Navbar />
-    <main className="m-2 grow">
+    <main className={classNames(styles.content, 'p15 grow')}>
       <Providers>{children}</Providers>
     </main>
   </div>
