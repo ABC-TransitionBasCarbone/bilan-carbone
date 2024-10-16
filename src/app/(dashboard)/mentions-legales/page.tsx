53a7661333bf5bafb4ce82750ee6eccda5c95314
lfo <louis@dewolf.fr>
2024-10-16 16:53:22 +0200

    apply suggestions

--- src/app/(dashboard)/mentions-legales/page.tsx
+++ src/app/(dashboard)/mentions-legales/page.tsx
@@ -21,7 +21,7 @@ const LegalNotices = () => {
         Conformément aux dispositions des articles 6-III et 19 de la loi pour la Confiance dans l’Économie Numérique,
         nous vous informons que ce site est édité par :
       </p>
-      <h1>Association pour la transition Bas Carbone</h1>
+      <div className={styles.section}>Association pour la transition Bas Carbone</div>
       <p className="flex-col">
         <span>Association loi 1901 à but non lucratif</span>
         <span>SIRET : 538 170 093 00032</span>
@@ -36,10 +36,10 @@ const LegalNotices = () => {
         <span>N° d’identification à la TVA : FR 96 53 81 70 093</span>
       </p>
 
-      <h1>Hébergeur </h1>
+      <div className={styles.section}>Hébergeur </div>
       <p>OVH– 2 rue Kellermann - 59100 Roubaix - France</p>
 
-      <h1>Propriétaire du site </h1>
+      <div className={styles.section}>Propriétaire du site </div>
       <p>
         Le présent site est la propriété de l’Institut de Formation Carbone. Le contenu éditorial, textes, images
         composant le site web sont la propriété de l’Institut de Formation Carbone. Toute représentation totale ou
@@ -50,7 +50,7 @@ const LegalNotices = () => {
       </p>
       <p>Responsable de publication : Anna Creti</p>
 
-      <h1>Propriété intellectuelle et copyright </h1>
+      <div className={styles.section}>Propriété intellectuelle et copyright </div>
       <p>
         L’ensemble des éléments du site (textes, documents…) sont, sauf dispositions contraires, la propriété
         intellectuelle exclusive de la société Institut de Formation Carbone. Par conséquent, toute reproduction,
@@ -63,12 +63,12 @@ const LegalNotices = () => {
         Pour toute exploitation autorisée de tout ou partie du contenu du site, faire figurer le nom de l’auteur, ses
         qualités, l’année de publication et la source.
       </p>
-      <h1>Liens hypertextes </h1>
+      <div className={styles.section}>Liens hypertextes </div>
       <p>
         L’éditeur ne saurait engager sa responsabilité sur le contenu des informations figurant sur les pages auxquelles
         les liens hypertextes du présent site renvoient.
       </p>
-      <h1>Droits d’accès</h1>
+      <div className={styles.section}>Droits d’accès</div>
       <p>
         Conformément au Règlement (UE) 2016/679 relatif à la protection des données à caractère personnel, vous disposez
         sur vos données des droit d’accès, droit de rectification et du droit d’opposition. Pour en savoir plus, vous
@@ -78,7 +78,7 @@ const LegalNotices = () => {
         </a>
         .
       </p>
-      <h1>La marque Bilan Carbone ®</h1>
+      <div className={styles.section}>La marque Bilan Carbone ®</div>
       <p>
         La marque Bilan Carbone® est une marque déposée en France depuis le 2 décembre 2003, enregistrée sous le numéro
         3260464.
@@ -88,7 +88,7 @@ const LegalNotices = () => {
         photographiques de la marque. La reproduction à des fins de diffusion ou d’exploitation de tout ou partie de la
         marque Bilan Carbone® n’est possible qu’avec l’autorisation expresse de l’Association pour la transition Bas
         Carbone. Le cas échéant, aucune modification de forme qui détournerait le sens et l’image Bilan Carbone® ne
-        pourra être apportée. Un lien devra être établi vers l’Association pour la transition Bas Carbone.{' '}
+        pourra être apportée. Un lien devra être établi vers l’Association pour la transition Bas Carbone.
       </p>
     </div>
   )
