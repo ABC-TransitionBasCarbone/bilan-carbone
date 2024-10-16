17991ed3207410b68d19031969cc9462a3e1c37e
lfo <louis@dewolf.fr>
2024-10-16 16:53:41 +0200

    create centralized cypress.login function

--- src/tests/end-to-end/app/legal-notices.cy.ts
+++ src/tests/end-to-end/app/legal-notices.cy.ts
@@ -1,49 +1,36 @@
 describe('Legal Notices', () => {
   beforeEach(() => {
-    cy.intercept('POST', '/api/auth/callback/credentials').as('login')
-
-    cy.visit('/')
-    cy.get('[data-testid="input-email"] > .MuiInputBase-input').should('be.visible').type('bc-test-user-1@yopmail.com')
-    cy.get('[data-testid="input-password"] > .MuiInputBase-input').should('be.visible').type('password-1')
-    cy.get('[data-testid="login-button"]').click()
-
-    cy.wait('@login')
+    cy.login('bc-test-user-1@yopmail.com', 'password-1')
   })
 
-  describe('Access', () => {
-    it('Should be accessible from the profile view', () => {
-      cy.visit('/profil')
-      cy.get('[data-testid="legal-notices-link"]').should('be.visible')
-      cy.get('[data-testid="legal-notices-link"]').click()
-      cy.url().should('include', '/mentions-legales')
-    })
+  it('Should be accessible from the profile view', () => {
+    cy.visit('/profil')
+    cy.get('[data-testid="legal-notices-link"]').should('be.visible')
+    cy.get('[data-testid="legal-notices-link"]').click()
+    cy.url().should('include', '/mentions-legales')
   })
 
-  describe('Layout', () => {
-    it('Displays the body of the legal notices', () => {
-      cy.visit('/mentions-legales')
-      cy.get('[data-testid="legal-notices"]').should('be.visible')
-    })
-    it('Displays the contact mail', () => {
-      cy.visit('/mentions-legales')
-      cy.get('[data-testid="contact-mail"]').should('be.visible')
-      cy.get('[data-testid="contact-mail"]').should('have.text', 'contact@associationbilancarbone.fr')
-      cy.get('[data-testid="contact-mail"]').should('have.attr', 'href').and('include', 'mailto:')
-    })
-    it('Displays the link to the privacy policy', () => {
-      cy.visit('/mentions-legales')
-      cy.get('[data-testid="privacy-link"]').scrollIntoView().should('be.visible')
-      cy.get('[data-testid="privacy-link"]').scrollIntoView().should('have.text', 'politique de protection des données')
-      cy.get('[data-testid="privacy-link"]')
-        .scrollIntoView()
-        .should('have.attr', 'href')
-        .and('include', 'associationbilancarbone.sharepoint.com')
-    })
-    it('Displays the link to the profile', () => {
-      cy.visit('/mentions-legales')
-      cy.get('[data-testid="profile-link"]').should('be.visible')
-      cy.get('[data-testid="profile-link"]').should('have.attr', 'href', '/profil')
-      cy.get('[data-testid="profile-link"]').should('have.text', 'Retour au profil')
-    })
+  it('Should display the body and content of the legal notices', () => {
+    cy.visit('/mentions-legales')
+
+    // Body
+    cy.get('[data-testid="legal-notices"]').should('be.visible')
+
+    // Contact mail button
+    cy.get('[data-testid="contact-mail"]').should('be.visible')
+    cy.get('[data-testid="contact-mail"]').should('have.text', 'contact@associationbilancarbone.fr')
+    cy.get('[data-testid="contact-mail"]').should('have.attr', 'href').and('include', 'mailto:')
+
+    // Privacy policy
+    cy.get('[data-testid="privacy-link"]').scrollIntoView().should('be.visible')
+    cy.get('[data-testid="privacy-link"]').should('have.text', 'politique de protection des données')
+    cy.get('[data-testid="privacy-link"]')
+      .should('have.attr', 'href')
+      .and('include', 'associationbilancarbone.sharepoint.com')
+
+    // Back to profile
+    cy.get('[data-testid="profile-link"]').scrollIntoView().should('be.visible')
+    cy.get('[data-testid="profile-link"]').should('have.attr', 'href', '/profil')
+    cy.get('[data-testid="profile-link"]').should('have.text', 'Retour au profil')
   })
 })
