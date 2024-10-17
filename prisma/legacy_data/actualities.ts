6ddabd627c9f4c6088de65b285bcf8d18623b7fe
lfo <louis@dewolf.fr>
2024-10-17 10:45:47 +0200

    add legacy data

---
+++ prisma/legacy_data/actualities.ts
@@ -0,0 +1,74 @@
+export const ACTUALITIES = [
+  {
+    title: 'Plusieurs nouveautés et mises à jour 5/7 - Matrice R&O',
+    text: "- Analyse : la matrice des risques et opportunités est désormais disponible. Cliquez sur le picto 'SWOT' avec le libellé 'R&O' dans la rubrique 'Analyse' et complétez la pour l'organisation pour laquelle vous intervenez. Vous allez plus loin dans l'analyse et la valeur ajoutée de votre intervention.",
+    createdAt: new Date('2022-03-15 12:00:00.000'),
+    updatedAt: new Date('2022-03-15 12:00:00.000'),
+  },
+  {
+    title: "Création de vos propres facteurs d'émission",
+    text: "Vous pouvez désormais créer vos propres facteurs d'émissions à partir des valeurs de ceux déjà renseignés dans la base. Sélectionnez l'onglet 'Ressources', puis cliquez sur 'Facteur d'émission' et enfin 'Créer un FE'. La fenêtre s'ouvre en affichant sur la gauche, la liste des FE disponibles dans votre base. A croite, un tableau vous permet d'ajouter les composants de votre nouveau FE et les quantités respectives.Complétez les informations requises et vous pourrez ainsi construire un ou plusieurs FE à partir des valeurs des FE disponibles et de la décomposition du nouveau FE dont vous avez besoin. Cette option reste une solution de dépannage qui ne remplace pas la détermination de la valeur d'un FE établ à partir d'une étude complète.",
+    createdAt: new Date('2022-05-31 12:00:02.000'),
+    updatedAt: new Date('2022-05-31 12:00:02.000'),
+  },
+  {
+    title: 'Gestion des pourcentages dans votre résultat consolidé',
+    text: "Si vous utilisiez le tableur Multisite en modifiant les différents pourcentages pour gérer d'éventuels double compte, vous pouvez désormais faire de même dans BC+. Dans la fenêtre 'Résultats' de votre étude, l'onglet 'Pourcentages' vous permet d'ajuster les % à prendre en compte poste par poste et entité par entité. Modifiez les % puis cliquez sur le bouton 'Appliquer' et vos choix seront pris en compte dans l'onglet 'Résultats consolidés'.",
+    createdAt: new Date('2022-05-31 12:00:01.000'),
+    updatedAt: new Date('2022-05-31 12:00:01.000'),
+  },
+  {
+    title: 'Créez vos cartographies de flux directement dans BC+',
+    text: "Vous avez désormais la possiblité de créer cos cartographies de flux directement dans BC+. Une fois votre étude ouverte cliquez sur 'Cartographie' puis 'Créer une nouvelle cartographie'. Une fenêtre spécifique s'ouvre et vous permet de construire votre cartographie et de l'enregistrer directement dans votre étude. Si nécessaire vous pourrez également exporter votre schéma au format image. Consulter le tuto d'aide pour l'explication détaillée.",
+    createdAt: new Date('2022-05-31 12:00:00.000'),
+    updatedAt: new Date('2022-05-31 12:00:00.000'),
+  },
+  {
+    title: 'Divers',
+    text: 'Divers correctifs et améliorations de performance ont été apportés à plusieurs endroits du logiciel.',
+    createdAt: new Date('2022-10-02 12:00:02.000'),
+    updatedAt: new Date('2022-10-02 12:00:02.000'),
+  },
+  {
+    title: 'Simulation Eco : correctif et graphique taxe GES',
+    text: "Dans la fenêtre du simulateur éco, quelques bugs résiduels ont été traités. Dans l'onglet graphique, vous pouvez afficher soit les simulations liées aux scénarios 'hydrocarbure' soit aux scénarios 'Taxe GES'. Toujours exportable en format image pour les intégrer à vos présentations.",
+    createdAt: new Date('2022-10-02 12:00:01.000'),
+    updatedAt: new Date('2022-10-02 12:00:01.000'),
+  },
+  {
+    title: "Bilan total : données d'activité",
+    text: "Dans la fenêtre d'affichage du bilan total, un nouvel onglet affiche les données d'activité selon la même arborescence que les émission de CO2e. Ces données sont également exportables vers un fichier tableur.",
+    createdAt: new Date('2022-10-02 12:00:00.000'),
+    updatedAt: new Date('2022-10-02 12:00:00.000'),
+  },
+  {
+    title: 'Le bilan GES réglementaire nouvelle version',
+    text: "La bilan GES réglementaire dans le format en vigueur depuis le début de l'année est disponible dans une première version. Attention, seul la génération pour le contrôle opérationnel est disponible.\nSi vous détectez un résultat qui ne vous parait pas conforme avec ce qu'il devrait être dans ce nouveau format, merci de nous le signaler par courriel à l'adresse support@bilancarbone-plus.com.",
+    createdAt: new Date('2023-03-28 12:00:00.000'),
+    updatedAt: new Date('2023-03-28 12:00:00.000'),
+  },
+  {
+    title: 'Correctifs et mises à jour',
+    text: "Plusieurs opérations de mises à jour et correctifs/modifications de fonctionnalités sont en cours cette semaine au moins. Ces opérations sont susceptibles de générer des perturbations dans votre utilisation du logiciel. Si ces perturbations persistent au delà de cette période, merci de nous le signaler via l'adresse mél support.",
+    createdAt: new Date('2023-05-15 12:00:00.000'),
+    updatedAt: new Date('2023-05-15 12:00:00.000'),
+  },
+  {
+    title: "Quota d'études : c'est terminé !",
+    text: "Avec la version 2.0.116 et supérieure, ou la version Web Beta, le principe des quotas d'études n'est plus appliqué.\nVous pouvez donc réaliser autant d'études que vous le souhaitez dès à présent sans surcoût.",
+    createdAt: new Date('2023-08-31 12:00:00.000'),
+    updatedAt: new Date('2023-08-31 12:00:00.000'),
+  },
+  {
+    title: 'BC+ Version Windows en cours de ravallement',
+    text: "La version Windows du BC+ est en cours de 'ravallement' pour avoir un aspect similaire à la version Web. Pendant les travaux l'application continue de fonctionner, mais certaines fenêtres pourront avoir un aspect modifié.",
+    createdAt: new Date('2023-10-28 12:00:00.000'),
+    updatedAt: new Date('2023-10-28 12:00:00.000'),
+  },
+  {
+    title: 'Mises à jour estivales du BC+',
+    text: "Des mises à jour du BC+ seront réalisées tout au long de l'été. Pour ce faire nous avons besoin de vous !\nN’hésitez pas à nous faire des retours (et vos demande d’aide sur support@bilancarbone-plus.com)\nVoici les modifications que nous venons de mettre en ligne :\n- Les liens d’aide envoie vers l’adresse support@bilancarbone-plus.com\n- Une FAQ est en cours de construction et désormais accessible dans l'onglet 'Formation'. Elle sera mise à jour en fonction des questions posées sur l'adresse support.\n- L'onglet formation renvoie désormais également vers le site de l'IFC, qui propose une formation à l'utilisation du BC+ pour des personnes déjà formées M1 ou M2.\n- Correction du calcul des incertitudes\n- Correction des unités dans les ratios monétaires\n- Correction d’un erreur sur les facteurs d’émission dans la sélection des sous postes\nLes modifications à venir en Juillet\n- Mise à jour des facteurs d’émission avec les données de l’ADEME et de Légifrance\n- Amélioration de l'onglet résultats\n",
+    createdAt: new Date('2024-07-03 12:00:00.000'),
+    updatedAt: new Date('2024-07-03 12:00:00.000'),
+  },
+]
