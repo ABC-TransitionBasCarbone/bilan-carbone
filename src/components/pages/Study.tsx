2e70a5357fa6d978284cc0fcc0daa8ae56027935
lfo <louis@dewolf.fr>
2024-10-17 15:08:18 +0200

    add study details view

--- src/components/pages/Study.tsx
+++ src/components/pages/Study.tsx
@@ -1,7 +1,10 @@
+import classNames from 'classnames'
 import { Study } from '@prisma/client'
 import { useTranslations } from 'next-intl'
-import LinkButton from '../base/LinkButton'
+import styles from './Study.module.css'
 import Studies from '../study/Studies'
+import LinkButton from '../base/LinkButton'
+import NewspaperIcon from '@mui/icons-material/Newspaper'
 
 interface Props {
   studies: Study[]
@@ -10,8 +13,13 @@ interface Props {
 const StudyPage = ({ studies }: Props) => {
   const t = useTranslations('study')
   return (
-    <div className="flex-col grow">
-      <LinkButton href="/etudes/creer">{t('create')}</LinkButton>
+    <div className={classNames(styles.box, 'flex-col grow m-2 p1')}>
+      <h2 className={classNames(styles.title, 'align-center pb1')}>
+        <NewspaperIcon /> {t('my-studies')}
+      </h2>
+      <LinkButton className="mb1" href="/etudes/creer">
+        {t('create')}
+      </LinkButton>
       <Studies studies={studies} />
     </div>
   )
