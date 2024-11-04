888fda5929b3181222d231a7be0b42a36c48a5de
lfo <louis@dewolf.fr>
2024-11-04 17:15:35 +0100

    add multiple emissions posts : first draft

--- src/components/emission/new/DetailedGESForm.tsx
+++ src/components/emission/new/DetailedGESForm.tsx
@@ -8,111 +8,97 @@ interface DetailedGESFieldsProps {
   control: Control<CreateEmissionCommand>
   detailedGES: boolean
   totalCo2?: number
+  index: number
 }
 
-const DetailedGESFields = ({ control, detailedGES, totalCo2 }: DetailedGESFieldsProps) => {
+const DetailedGESFields = ({ control, detailedGES, index }: DetailedGESFieldsProps) => {
   const t = useTranslations('emissions.create')
   return (
-    <>
-      {detailedGES && (
-        <>
-          <FormTextField
-            data-testid="new-emission-co2f"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="co2f"
-            label={t('co2f')}
-          />
-          <FormTextField
-            data-testid="new-emission-ch4f"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="ch4f"
-            label={t('ch4f')}
-          />
-          <FormTextField
-            data-testid="new-emission-ch4b"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="ch4b"
-            label={t('ch4b')}
-          />
-          <FormTextField
-            data-testid="new-emission-n2o"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="n2o"
-            label={t('n2o')}
-          />
-          <FormTextField
-            data-testid="new-emission-co2b"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="co2b"
-            label={t('co2b')}
-          />
-          <FormTextField
-            data-testid="new-emission-sf6"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="sf6"
-            label={t('sf6')}
-          />
-          <FormTextField
-            data-testid="new-emission-hfc"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="hfc"
-            label={t('hfc')}
-          />
-          <FormTextField
-            data-testid="new-emission-pfc"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="pfc"
-            label={t('pfc')}
-          />
-          <FormTextField
-            data-testid="new-emission-otherGES"
-            control={control}
-            translation={t}
-            slotProps={{ htmlInput: { min: 0 } }}
-            type="number"
-            name="otherGES"
-            label={t('otherGES')}
-          />
-        </>
-      )}
-      <FormTextField
-        disabled={detailedGES}
-        data-testid="new-emission-totalCo2"
-        control={control}
-        translation={t}
-        slotProps={{
-          htmlInput: { min: 0 },
-          inputLabel: { shrink: detailedGES || totalCo2 !== undefined ? true : undefined },
-        }}
-        type="number"
-        name="totalCo2"
-        label={t('totalCo2')}
-      />
-    </>
+    detailedGES && (
+      <>
+        <FormTextField
+          data-testid="new-emission-co2f"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`co2f.${index}`}
+          label={t('co2f')}
+        />
+        <FormTextField
+          data-testid="new-emission-ch4f"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`ch4f.${index}`}
+          label={t('ch4f')}
+        />
+        <FormTextField
+          data-testid="new-emission-ch4b"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`ch4b.${index}`}
+          label={t('ch4b')}
+        />
+        <FormTextField
+          data-testid="new-emission-n2o"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`n2o.${index}`}
+          label={t('n2o')}
+        />
+        <FormTextField
+          data-testid="new-emission-co2b"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`co2b.${index}`}
+          label={t('co2b')}
+        />
+        <FormTextField
+          data-testid="new-emission-sf6"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`sf6.${index}`}
+          label={t('sf6')}
+        />
+        <FormTextField
+          data-testid="new-emission-hfc"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`hfc.${index}`}
+          label={t('hfc')}
+        />
+        <FormTextField
+          data-testid="new-emission-pfc"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`pfc.${index}`}
+          label={t('pfc')}
+        />
+        <FormTextField
+          data-testid="new-emission-otherGES"
+          control={control}
+          translation={t}
+          slotProps={{ htmlInput: { min: 0 } }}
+          type="number"
+          name={`otherGES.${index}`}
+          label={t('otherGES')}
+        />
+      </>
+    )
   )
 }
 
--- src/services/serverFunctions/emission.command.ts
+++ src/services/serverFunctions/emission.command.ts
@@ -1,4 +1,4 @@
-import { SubPost, Unit } from '@prisma/client'
+import { PostType, SubPost, Unit } from '@prisma/client'
 import z from 'zod'
 
 export const CreateEmissionCommandValidation = z.object({
@@ -21,18 +21,30 @@ export const CreateEmissionCommandValidation = z.object({
       required_error: 'totalCo2',
     })
     .min(0, 'totalCo2'),
-  co2f: z.number().min(0, 'co2f').optional(),
-  ch4f: z.number().min(0, 'ch4f').optional(),
-  ch4b: z.number().min(0, 'ch4b').optional(),
-  n2o: z.number().min(0, 'n2o').optional(),
-  co2b: z.number().min(0, 'co2b').optional(),
-  sf6: z.number().min(0, 'sf6').optional(),
-  hfc: z.number().min(0, 'hfc').optional(),
-  pfc: z.number().min(0, 'pfc').optional(),
-  otherGES: z.number().min(0, 'otherGES').optional(),
+  co2f: z.array(z.number().min(0, 'co2f')).optional(),
+  ch4f: z.array(z.number().min(0, 'ch4f')).optional(),
+  ch4b: z.array(z.number().min(0, 'ch4b')).optional(),
+  n2o: z.array(z.number().min(0, 'n2o')).optional(),
+  co2b: z.array(z.number().min(0, 'co2b')).optional(),
+  sf6: z.array(z.number().min(0, 'sf6')).optional(),
+  hfc: z.array(z.number().min(0, 'hfc')).optional(),
+  pfc: z.array(z.number().min(0, 'pfc')).optional(),
+  otherGES: z.array(z.number().min(0, 'otherGES')).optional(),
   attribute: z.string().optional(),
   subPost: z.nativeEnum(SubPost, { required_error: 'subPost' }),
   comment: z.string().optional(),
+  posts: z
+    .array(
+      z.object({
+        name: z.string({ required_error: 'name' }).trim().min(1, 'name'),
+        type: z.nativeEnum(PostType, { required_error: 'type' }),
+        totalCo2: z
+          .number({ invalid_type_error: 'totalCo2', required_error: 'totalCo2' })
+          .min(0, 'totalCo2')
+          .optional(),
+      }),
+    )
+    .optional(),
 })
 
 export type CreateEmissionCommand = z.infer<typeof CreateEmissionCommandValidation>
