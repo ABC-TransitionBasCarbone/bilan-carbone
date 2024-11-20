ff9fb9f0fa7611933bb971f21912366b6dce4fa9
Chloe M <chloe.molle@neuf.fr>
2024-11-20 16:58:34 +0100

    update navbar design

--- src/components/studyNavbar/studyNavbar.tsx
+++ src/components/studyNavbar/studyNavbar.tsx
@@ -5,7 +5,7 @@ import { useTranslations } from "next-intl"
 import Link from "next/link"
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Drawer, IconButton } from "@mui/material"
+import { Divider, Drawer, IconButton } from "@mui/material"
 import { useState } from "react"
 import { UUID } from "crypto"
 
@@ -13,6 +13,7 @@ import { UUID } from "crypto"
 const StudyNavbar = ({ studyId }: { studyId: UUID }) => {
     const t = useTranslations('study.navigation')
     const [open, setOpen] = useState<boolean>(false);
+    const [openAccountingDetails, setOpenAccountingDetails] = useState<boolean>(false);
 
     return <>
         {!open && <div className={styles.toolbarContainer}>
@@ -40,24 +41,39 @@ const StudyNavbar = ({ studyId }: { studyId: UUID }) => {
                         <ChevronLeftIcon />
                     </IconButton>
                 </div>
+                <Divider />
                 <Link className={styles.link} href={`/etudes/${studyId}`} onClick={() => setOpen(false)}>
                     {t('homepage')}
                 </Link>
+                <Divider />
                 <Link className={styles.link} href={`/etudes/${studyId}/cadrage`} onClick={() => setOpen(false)}>
                     {t('framing')}
                 </Link>
+                <Divider />
                 <Link className={styles.link} href={`/etudes/${studyId}/perimetre`} onClick={() => setOpen(false)}>
                     {t('scope')}
                 </Link>
+                <Divider />
                 <div className={styles.link} onClick={() => setOpen(false)}>
                     {t('mobilisation')}
                 </div>
-                <Link className={styles.link} href={`/etudes/${studyId}/accounting/data-entry`} onClick={() => setOpen(false)}>
-                    {t('data-entry')}
-                </Link>
+                <Divider />
+                <div>
+                    <div className={styles.link} onClick={() => setOpenAccountingDetails((prev) => !prev)}>{t('accounting')}</div>
+                    {openAccountingDetails && <div className={styles.childrenLink}>
+                        <Link className={styles.link} href={`/etudes/${studyId}/accounting/data-entry`} onClick={() => setOpen(false)}>
+                            {t('data-entry')}
+                        </Link>
+                        <Link className={styles.link} href={`/etudes/${studyId}/accounting/results`} onClick={() => setOpen(false)}>
+                            {t('results')}
+                        </Link>
+                    </div>}
+                </div>
+                <Divider />
                 <div className={styles.link} onClick={() => setOpen(false)}>
                     {t('transition-plan')}
                 </div>
+                <Divider />
             </Drawer>
         </div>
     </>
