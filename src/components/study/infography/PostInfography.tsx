dfe109ff7627332c63ed2c8db1174909acc3da44
Chloe M <chloe.molle@neuf.fr>
2024-11-26 17:51:04 +0100

    open slowly

--- src/components/study/infography/PostInfography.tsx
+++ src/components/study/infography/PostInfography.tsx
@@ -1,14 +1,12 @@
 'use client'
 
 import { Post, subPostsByPost } from '@/services/posts'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
-import { Box, LinearProgress } from '@mui/material'
 import { Study, SubPost } from '@prisma/client'
 import classNames from 'classnames'
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
-import { useMemo, useState } from 'react'
-import PostIcon from './icons/PostIcon'
 import styles from './PostInfography.module.css'
 import { PostHeader } from './PostHeader'
 
@@ -31,7 +29,10 @@ const colors: Record<Post, string> = {
 }
 
 const PostInfography = ({ study, post }: Props) => {
+  const ref = useRef<HTMLDivElement>(null)
   const [showSubPosts, setShowSubPosts] = useState<boolean>(false)
+  const [hoverEnterTimeout, setHoverEnterTimeout] = useState<NodeJS.Timeout | null>(null);
+  const [hoverLeaveTimeout, setHoverLeaveTimeout] = useState<NodeJS.Timeout | null>(null);
 
   const t = useTranslations('emissionFactors.post')
 
@@ -51,16 +52,59 @@ const PostInfography = ({ study, post }: Props) => {
     return null
   }, [post])
 
+  useEffect(() => {
+    if (ref.current) {
+      if (showSubPosts) {
+        const height = ref.current.scrollHeight
+        ref.current.style.height = `${height}px`
+
+        setTimeout(() => {
+          if (ref.current) {
+            ref.current.style.height = 'auto'
+            ref.current.style.overflow = 'visible'
+          }
+        }, 300)
+      } else {
+        setTimeout(() => {
+          if (ref.current) {
+            ref.current.style.height = '0px'
+            ref.current.style.overflow = 'hidden'
+          }
+        }, 300)
+      }
+    }
+  }, [showSubPosts, ref])
+
   return (
     <Link
-      onMouseEnter={() => setShowSubPosts(true)}
-      onMouseLeave={() => setShowSubPosts(false)}
+      onMouseEnter={() => {
+        if (hoverLeaveTimeout) {
+          clearTimeout(hoverLeaveTimeout);
+          setHoverLeaveTimeout(null);
+        }
+
+        const timeout = setTimeout(() => {
+          setShowSubPosts(true);
+        }, 200);
+        setHoverEnterTimeout(timeout);
+      }}
+      onMouseLeave={() => {
+        if (hoverEnterTimeout) {
+          clearTimeout(hoverEnterTimeout);
+          setHoverEnterTimeout(null);
+        }
+
+        const timeout = setTimeout(() => {
+          setShowSubPosts(false);
+        }, 500);
+        setHoverLeaveTimeout(timeout);
+      }}
       data-testid="post-infography"
       href={`/etudes/${study.id}/comptabilisation/saisie-des-donnees/${mainPost}`}
       className={styles[Object.keys(Post).includes(post) ? colors[post as Post] : 'green']}
     >
       <PostHeader study={study} post={post} mainPost={mainPost} />
-      <div className={classNames(styles.subPostsContainer)}>
+      <div className={classNames(styles.subPostsContainer)} ref={ref}>
         {showSubPosts && subPosts && (
           <div className={classNames(styles.subPosts, 'flex')}>
             <ul className={classNames(styles.list, 'flex-col')}>
