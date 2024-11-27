02f597cfcfe0e7e4966f46e849a8f53b6a769206
lfo <louis@dewolf.fr>
2024-11-27 12:10:37 +0100

    download study post emission sources

--- src/db/emissionFactors.ts
+++ src/db/emissionFactors.ts
@@ -45,6 +45,38 @@ export const getAllEmissionFactorsByIds = (ids: string[], organizationId: string
     orderBy: { createdAt: 'desc' },
   })
 
+export const getAllEmissionFactorsByIds = (ids: string[], organizationId: string) =>
+  prismaClient.emissionFactor.findMany({
+    where: {
+      id: { in: ids },
+      OR: [{ organizationId: null }, { organizationId }],
+    },
+    select: {
+      id: true,
+      status: true,
+      totalCo2: true,
+      location: true,
+      source: true,
+      unit: true,
+      importedFrom: true,
+      reliability: true,
+      technicalRepresentativeness: true,
+      geographicRepresentativeness: true,
+      temporalRepresentativeness: true,
+      completeness: true,
+      metaData: {
+        select: {
+          language: true,
+          title: true,
+          attribute: true,
+          comment: true,
+          location: true,
+        },
+      },
+    },
+    orderBy: { createdAt: 'desc' },
+  })
+
 export const createEmissionFactor = (emissionFactor: Prisma.EmissionFactorCreateInput) =>
   prismaClient.emissionFactor.create({
     data: emissionFactor,
