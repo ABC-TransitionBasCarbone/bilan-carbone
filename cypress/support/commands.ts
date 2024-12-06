0b7ee53423f9451ffe8e714612b5fe68ca1220ed
lfo <louis@dewolf.fr>
2024-12-06 15:02:05 +0100

    fix login / logout behavior

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -6,12 +6,11 @@ Cypress.Commands.add('login', (email = 'bc-default-0@yopmail.com', password = 'p
   cy.get('[data-testid="input-email"] > .MuiInputBase-root > .MuiInputBase-input').should('be.visible').type(email)
   cy.get('[data-testid="input-password"] > .MuiInputBase-root > .MuiInputBase-input').type(password)
   cy.getByTestId('login-button').click()
-  cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+  cy.wait('@login')
 })
 
 Cypress.Commands.add('logout', () => {
-  cy.intercept('POST', '/api/auth/callback/credentials').as('login')
+  cy.intercept('POST', '/api/auth/signout').as('logout')
   cy.visit('/logout')
-
-  cy.url().should('eq', `${Cypress.config().baseUrl}/login`, { timeout: 10000 })
+  cy.wait('@logout')
 })
