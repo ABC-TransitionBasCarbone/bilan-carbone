e55c01c93b010a7044afda3ef0cf1127d9bf6eda
lfo <louis@dewolf.fr>
2024-12-09 12:14:39 +0100

    rename create emission source test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,17 +1,14 @@
 Cypress.Commands.add('getByTestId', (testId: string) => cy.get(`[data-testid="${testId}"]`))
 
 Cypress.Commands.add('login', (email = 'bc-default-0@yopmail.com', password = 'password-0') => {
-  cy.intercept('POST', '/api/auth/callback/credentials').as('login')
   cy.visit('/login')
   cy.get('[data-testid="input-email"] > .MuiInputBase-root > .MuiInputBase-input').should('be.visible').type(email)
   cy.get('[data-testid="input-password"] > .MuiInputBase-root > .MuiInputBase-input').type(password)
   cy.getByTestId('login-button').click()
-  cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+  cy.wait(`@login`)
 })
 
 Cypress.Commands.add('logout', () => {
-  cy.intercept('POST', '/api/auth/callback/credentials').as('login')
   cy.visit('/logout')
-
-  cy.url().should('eq', `${Cypress.config().baseUrl}/login`, { timeout: 10000 })
+  cy.wait(`@logout`)
 })
