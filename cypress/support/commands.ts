3046a9bc78ca9b2573e055c388a7caf5fe248e63
lfo <louis@dewolf.fr>
2024-12-09 12:14:57 +0100

    add uniq login/logout interceptor

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,16 +1,18 @@
 Cypress.Commands.add('getByTestId', (testId: string) => cy.get(`[data-testid="${testId}"]`))
 
 Cypress.Commands.add('login', (email = 'bc-default-0@yopmail.com', password = 'password-0') => {
-  cy.intercept('POST', '/api/auth/callback/credentials').as('login')
+  const uniqLogin = `login-${Date.now()}`
+  cy.intercept('POST', '/api/auth/callback/credentials').as(uniqLogin)
   cy.visit('/login')
   cy.get('[data-testid="input-email"] > .MuiInputBase-root > .MuiInputBase-input').should('be.visible').type(email)
   cy.get('[data-testid="input-password"] > .MuiInputBase-root > .MuiInputBase-input').type(password)
   cy.getByTestId('login-button').click()
-  cy.wait('@login')
+  cy.wait(`@${uniqLogin}`)
 })
 
 Cypress.Commands.add('logout', () => {
-  cy.intercept('POST', '/api/auth/signout').as('logout')
+  const uniqLogout = `logout-${Date.now()}`
+  cy.intercept('POST', '/api/auth/signout').as(uniqLogout)
   cy.visit('/logout')
-  cy.wait('@logout')
+  cy.wait(`@${uniqLogout}`)
 })
