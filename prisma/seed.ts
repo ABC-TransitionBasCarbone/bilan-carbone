0e20c3e00c65e072389901f1a52f3fcebb360ac5
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-12-19 16:55:27 +0100

    fix csv streaming

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { signPassword } from '@/services/auth'
 import { getEmissionFactorsFromAPI } from '@/services/baseEmpreinte/getEmissionFactorsFromAPI'
+import { reCreateBegesRules } from '@/services/exportRules/beges'
 import { getUsersFromFTP } from '@/services/ftpUsers/getUsers'
 import { faker } from '@faker-js/faker'
 import { EmissionFactorStatus, Import, Level, PrismaClient, Role, StudyRole, SubPost, Unit, User } from '@prisma/client'
@@ -313,14 +314,8 @@ const licenses = async () => {
   })
 }
 
-// const main = async (params: Params) => {
-//   await Promise.all([actualities(), licenses(), users(), reCreateBegesRules(), getUsersFromFTP()])
-//   if (params.importFactors) {
-//     await getEmissionFactorsFromAPI(params.importFactors)
-//   }
-// }
 const main = async (params: Params) => {
-  await Promise.all([getUsersFromFTP()])
+  await Promise.all([actualities(), licenses(), users(), reCreateBegesRules(), getUsersFromFTP()])
   if (params.importFactors) {
     await getEmissionFactorsFromAPI(params.importFactors)
   }
--- src/services/ftpUsers/getUsers.ts
+++ src/services/ftpUsers/getUsers.ts
@@ -1,8 +1,22 @@
 import { PrismaClient, Role } from '@prisma/client'
-import { AccessOptions, Client } from "basic-ftp"
+import { AccessOptions, Client } from 'basic-ftp'
 import { Writable } from 'stream'
 
 const prisma = new PrismaClient()
+
+class MyWritableStream extends Writable {
+  private data: string = ''
+
+  _write(chunk: any, encoding: string, callback: Function) {
+    this.data += chunk.toString()
+    callback()
+    return encoding
+  }
+
+  getData() {
+    return this.data
+  }
+}
 
 export const getUsersFromFTP = async () => {
   const client = new Client()
@@ -13,54 +27,43 @@ export const getUsersFromFTP = async () => {
       host: process.env.NEXT_FTP_HOST,
       user: process.env.NEXT_FTP_USER,
       password: process.env.NEXT_FTP_PASSWORD,
+      secureOptions: { rejectUnauthorized: false },
       port: parseInt(process.env.NEXT_FTP_PORT || '22', 10),
       protocol: 'ftp',
-      name: "4486812@abc-transitionbascarbone.scienceontheweb.net",
-      uploadOnSave: false,
-      useTempFile: false,
-      openSsh: false
+      secure: true,
     } as AccessOptions
-    console.log("🚀 ~ getUsersFromFTP ~ accessOptions:", accessOptions)
 
     await client.access(accessOptions)
 
-    // Connect to the SFTP server
-    // await sftp.connect(accessOptions)
-
-    // Fetch the file from the SFTP server
+    // Fetch the file from the FTP server
     const fileName = process.env.NEXT_FTP_FILE_NAME || ''
-    console.log('🚀 ~ getUsersFromFTP ~ fileName:', fileName)
-    const stream = new Writable()
 
+    const stream = new MyWritableStream()
 
-    console.log("🚀 ~ getUsersFromFTP ~ await client.list():", await client.list())
-
-    // await sftp.get(fileName, stream)
-    const test = await client.downloadTo(stream, "/abc-transitionbascarbone.scienceontheweb.net/" + fileName)
-    console.log("🚀 ~ getUsersFromFTP ~ test:", test)
-    console.log("🚀 ~ getUsersFromFTP ~ stream:", stream)
-
+    await client.downloadTo(stream, '/abc-transitionbascarbone.scienceontheweb.net/' + fileName)
 
     // Convert the stream to a string (assuming it's a text file)
-    const data = stream.toString()
-    console.log("🚀 ~ getUsersFromFTP ~ data:", data)
+    const data = stream.getData()
 
     // Insert data into PostgreSQL using Prisma
-    const values = data.split('\n').map((line) => line.split(',')) // Adjust based on your file format
+    const values = data
+      .split('\n')
+      .map((line) => line.split(','))
+      .filter((data) => data.length > 1) // Adjust based on your file format
+    values.shift()
+
     for (const value of values) {
       await prisma.user.create({
         data: {
           email: value[0],
           role: Role.DEFAULT,
-          firstName: value[1],
-          lastName: value[2],
-          isActive: false
+          firstName: value[0],
+          lastName: value[0],
+          isActive: false,
         },
       })
     }
-  } catch (err) {
-    console.error('Error:', err)
-  } finally {
-    await prisma.$disconnect()
+  } catch (e) {
+    console.error(e)
   }
 }
