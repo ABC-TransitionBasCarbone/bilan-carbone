8ff53269cbc6abe622030b88c786bcc964245b32
lfo <louis@dewolf.fr>
2024-12-19 19:46:44 +0100

    add import emission factors from legifrance csv

---
+++ src/scripts/legifrance/getEmissionFactors.ts
@@ -0,0 +1,22 @@
+import { Command } from 'commander'
+import { getEmissionFactorsFromCSV } from '../../services/importEmissionFactor/legifrance/getEmissionFactors'
+
+const program = new Command()
+
+program
+  .name('legifrance-import-emission')
+  .description("Script pour importer les facteurs d'émission depuis la base LégiFrance")
+  .version('1.0.0')
+  .requiredOption('-n, --name <value>', 'Nom de la version')
+  .requiredOption('-f, --file <value>', 'Import from CSV file')
+  .requiredOption('-r, --reseau <value>', `Type de reseau ('froid', 'chaud')`, (value) => {
+    if (!['froid', 'chaud'].includes(value)) {
+      throw new Error(`reseau doit être 'froid' ou 'chaud'`)
+    }
+    return value
+  })
+  .parse(process.argv)
+
+const params = program.opts()
+
+getEmissionFactorsFromCSV(params.name, params.file, params.reseau)
