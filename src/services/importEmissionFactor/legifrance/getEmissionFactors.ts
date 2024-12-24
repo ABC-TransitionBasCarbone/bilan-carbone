58e04ad4d74c19ff8c794e5cdf8e15a374c5fd67
lfo <louis@dewolf.fr>
2024-12-24 16:39:24 +0100

    apply suggestion

--- src/services/importEmissionFactor/legifrance/getEmissionFactors.ts
+++ src/services/importEmissionFactor/legifrance/getEmissionFactors.ts
@@ -2,20 +2,20 @@ import { Import, SubPost } from '@prisma/client'
 import { getEmissionFactorsFromCSV as getEmissionFactors } from '../getEmissionFactorsFromCSV'
 import { ImportEmissionFactor, mapEmissionFactors } from '../import'
 
+const subPostByNetworkType = {
+  froid: [SubPost.ReseauxDeFroid],
+  chaud: [SubPost.ReseauxDeChaleurEtDeVapeur],
+}
+
+const getSubPostsFunc = (networkType: 'froid' | 'chaud') => () => subPostByNetworkType[networkType]
+
 const mapLegifranceEmissionFactors = (
   emissionFactor: ImportEmissionFactor,
   versionId: string,
-  reseau: 'froid' | 'chaud',
-) => {
-  const typeMatrix = {
-    froid: [SubPost.ReseauxDeFroid],
-    chaud: [SubPost.ReseauxDeChaleurEtDeVapeur],
-  }
-  const getSubPostsFunc = () => typeMatrix[reseau]
-  return mapEmissionFactors(emissionFactor, Import.Legifrance, versionId, getSubPostsFunc)
-}
+  networkType: 'froid' | 'chaud',
+) => mapEmissionFactors(emissionFactor, Import.Legifrance, versionId, getSubPostsFunc(networkType))
 
-export const getEmissionFactorsFromCSV = async (name: string, file: string, reseau: 'froid' | 'chaud') =>
+export const getEmissionFactorsFromCSV = async (name: string, file: string, networkType: 'froid' | 'chaud') =>
   getEmissionFactors(name, file, Import.NegaOctet, (emissionFactor: ImportEmissionFactor, versionId: string) =>
-    mapLegifranceEmissionFactors(emissionFactor, versionId, reseau),
+    mapLegifranceEmissionFactors(emissionFactor, versionId, networkType),
   )
