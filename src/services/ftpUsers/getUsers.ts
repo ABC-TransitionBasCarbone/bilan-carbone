3a3839acd4539c9acf06fb33cd5676ec9b8bd3f9
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-12-24 15:20:45 +0100

    fix lint

--- src/services/ftpUsers/getUsers.ts
+++ src/services/ftpUsers/getUsers.ts
@@ -44,7 +44,7 @@ export const getUsersFromFTP = async () => {
           firstName: value[0],
           lastName: value[0],
           isActive: false,
-          isValidated: false
+          isValidated: false,
         })),
       })
     })
