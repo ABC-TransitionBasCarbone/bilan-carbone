ce115a003c1411040540d9ccbae95080d882d03b
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-01-13 14:00:14 +0100

    chore: cache FE (#350)
    * chore: cache FE
    
    * sort alphabetically
    
    * fix end2end
    
    ---------
    
    Co-authored-by: Chloe M <chloe.molle@neuf.fr>

--- src/services/emissionFactors.ts
+++ src/services/emissionFactors.ts
@@ -1,5 +1,6 @@
 import { getAllEmissionFactors, getAllEmissionFactorsByIds } from '@/db/emissionFactors'
 import { auth } from './auth'
+import { sortAlphabetically } from './utils'
 
 export const getEmissionFactors = async (locale: string) => {
   const session = await auth()
@@ -9,10 +10,12 @@ export const getEmissionFactors = async (locale: string) => {
 
   const emissionFactors = await getAllEmissionFactors(session.user.organizationId)
 
-  return emissionFactors.map((emissionFactor) => ({
-    ...emissionFactor,
-    metaData: emissionFactor.metaData.find((metadata) => metadata.language === locale),
-  }))
+  return emissionFactors
+    .map((emissionFactor) => ({
+      ...emissionFactor,
+      metaData: emissionFactor.metaData.find((metadata) => metadata.language === locale),
+    }))
+    .sort((a, b) => sortAlphabetically(a?.metaData?.title, b?.metaData?.title))
 }
 
 export type EmissionFactorWithMetaData = AsyncReturnType<typeof getEmissionFactors>[0]
@@ -24,8 +27,10 @@ export const getEmissionFactorsByIds = async (ids: string[], locale: string) => 
     return []
   }
   const emissionFactors = await getAllEmissionFactorsByIds(ids, session.user.organizationId)
-  return emissionFactors.map((emissionFactor) => ({
-    ...emissionFactor,
-    metaData: emissionFactor.metaData.find((metadata) => metadata.language === locale),
-  }))
+  return emissionFactors
+    .map((emissionFactor) => ({
+      ...emissionFactor,
+      metaData: emissionFactor.metaData.find((metadata) => metadata.language === locale),
+    }))
+    .sort((a, b) => sortAlphabetically(a?.metaData?.title, b?.metaData?.title))
 }
