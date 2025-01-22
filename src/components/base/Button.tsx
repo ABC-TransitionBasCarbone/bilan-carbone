307e09f24935e45103d829f2bae630c33219126f
lfo <louis@dewolf.fr>
2025-01-22 09:56:33 +0100

    add LoadingButton on studyFlow

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -2,8 +2,8 @@ import { Button as ButtonMUI, ButtonProps } from '@mui/material'
 import classNames from 'classnames'
 import styles from './Button.module.css'
 
-const Button = ({ className, ...rest }: ButtonProps) => {
-  return <ButtonMUI className={classNames(styles.button, className)} {...rest} />
+const Button = ({ className, color, ...rest }: ButtonProps) => {
+  return <ButtonMUI className={classNames(styles.button, className, { [styles.error]: color === 'error' })} {...rest} />
 }
 
 export default Button
