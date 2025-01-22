4fa2c68c24422d61cc63b246ffd7e253cfa86f75
lfo <louis@dewolf.fr>
2025-01-22 10:05:32 +0100

    add new LoadingButtons

--- src/components/team/InvitationsToValidateActions.tsx
+++ src/components/team/InvitationsToValidateActions.tsx
@@ -4,11 +4,12 @@ import { TeamMember } from '@/db/user'
 import { deleteMember, validateMember } from '@/services/serverFunctions/user'
 import CheckIcon from '@mui/icons-material/Check'
 import DeleteIcon from '@mui/icons-material/Delete'
-import { Button } from '@mui/material'
 import classNames from 'classnames'
 import { User } from 'next-auth'
 import { useTranslations } from 'next-intl'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import LoadingButton from '../base/LoadingButton'
 import styles from './InvitationsActions.module.css'
 import SelectRole from './SelectRole'
 
@@ -19,38 +20,44 @@ interface Props {
 
 const InvitationsToValidateActions = ({ user, member }: Props) => {
   const t = useTranslations('team')
+  const [validating, setValidating] = useState(false)
+  const [deleting, setDeleting] = useState(false)
   const router = useRouter()
   return (
     <div className={classNames(styles.buttons, 'flex')}>
       <SelectRole user={user} email={member.email} currentRole={member.role} />
-      <Button
-        variant="contained"
-        color="success"
-        onClick={async () => {
-          const result = await validateMember(member.email)
-          if (!result) {
-            router.refresh()
-          }
-        }}
+      <LoadingButton
         aria-label={t('resend')}
         title={t('resend')}
-      >
-        <CheckIcon />
-      </Button>
-      <Button
-        variant="contained"
-        color="error"
+        loading={validating}
         onClick={async () => {
-          const result = await deleteMember(member.email)
+          setValidating(true)
+          const result = await validateMember(member.email)
+          setValidating(false)
           if (!result) {
             router.refresh()
           }
         }}
+        iconButton
+      >
+        <CheckIcon />
+      </LoadingButton>
+      <LoadingButton
         aria-label={t('delete')}
         title={t('delete')}
+        loading={deleting}
+        onClick={async () => {
+          setDeleting(true)
+          const result = await deleteMember(member.email)
+          setDeleting(false)
+          if (!result) {
+            router.refresh()
+          }
+        }}
+        iconButton
       >
         <DeleteIcon />
-      </Button>
+      </LoadingButton>
     </div>
   )
 }
