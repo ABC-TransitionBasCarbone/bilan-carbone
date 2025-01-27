a27ee800b8d44aefcfac138ab56b1d6de756e06e
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-01-27 11:31:16 +0100

    feat: manage suppression of value in EF creation

--- src/constants/emissions.ts
+++ src/constants/emissions.ts
@@ -1,5 +1,1 @@
 export const gazKeys = ['co2f', 'ch4f', 'ch4b', 'n2o', 'co2b', 'sf6', 'hfc', 'pfc', 'otherGES'] as const
-export const defaultGazValues = gazKeys.reduce((acc, key) => ({ ...acc, [key]: 0 }), {}) as Record<
-  (typeof gazKeys)[number],
-  number
->
--- src/services/serverFunctions/emissionFactor.command.ts
+++ src/services/serverFunctions/emissionFactor.command.ts
@@ -4,15 +4,15 @@ import z from 'zod'
 export const maxParts = 5
 
 const GESschema = z.object({
-  co2f: z.number().min(0, 'co2f').default(0),
-  ch4f: z.number().min(0, 'ch4f').default(0),
-  ch4b: z.number().min(0, 'ch4b').default(0),
-  n2o: z.number().min(0, 'n2o').default(0),
-  co2b: z.number().min(0, 'co2b').default(0),
-  sf6: z.number().min(0, 'sf6').default(0),
-  hfc: z.number().min(0, 'hfc').default(0),
-  pfc: z.number().min(0, 'pfc').default(0),
-  otherGES: z.number().min(0, 'otherGES').default(0),
+  co2f: z.nan().or(z.number().min(0, 'co2f')).optional(),
+  ch4f: z.nan().or(z.number().min(0, 'ch4f')).optional(),
+  ch4b: z.nan().or(z.number().min(0, 'ch4b')).optional(),
+  n2o: z.nan().or(z.number().min(0, 'n2o')).optional(),
+  co2b: z.nan().or(z.number().min(0, 'co2b')).optional(),
+  sf6: z.nan().or(z.number().min(0, 'sf6')).optional(),
+  hfc: z.nan().or(z.number().min(0, 'hfc')).optional(),
+  pfc: z.nan().or(z.number().min(0, 'pfc')).optional(),
+  otherGES: z.nan().or(z.number().min(0, 'otherGES')).optional(),
 })
 
 export const CreateEmissionFactorCommandValidation = z.intersection(
