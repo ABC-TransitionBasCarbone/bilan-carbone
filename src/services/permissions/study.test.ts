680ae121fa0e94063f1e8b4b4a3c2172f3214bfc
lfo <louis@dewolf.fr>
2025-01-28 11:50:47 +0100

    use utils; update canCreateStudy call

--- src/services/permissions/study.test.ts
+++ src/services/permissions/study.test.ts
@@ -1,49 +1,9 @@
 import * as dbUserModule from '@/db/user'
+import { getMockedDbUser, getMockedStudy, mockedOrganizationId } from '@/tests/utils/models'
 import { expect } from '@jest/globals'
-import { Level, Prisma, Role } from '@prisma/client'
-import { User } from 'next-auth'
+import { Level } from '@prisma/client'
 import * as organizationModule from './organization'
 import { canCreateStudy } from './study'
-
-const mockedUserId = 'mocked-user-id'
-const mockedOrganizationId = 'mocked-organization-id'
-
-const mockedUser = {
-  id: '6d2af85f-f6f8-42ec-9fa4-965405e52d12',
-  email: 'mocked@email.com',
-  firstName: 'Mocke',
-  lastName: 'User',
-  organizationId: mockedOrganizationId,
-  role: Role.ADMIN,
-  level: Level.Initial,
-}
-const mockedDbUser = {
-  ...mockedUser,
-  createdAt: '2025-01-01T00:00:00.000Z',
-  updatedAt: '2025-01-01T00:00:00.000Z',
-  isActive: true,
-  isValidated: true,
-}
-const mockedStudy = {
-  name: 'Mocked Study',
-  startDate: '2025-01-01T00:00:00.000Z',
-  endDate: '2025-01-01T00:00:00Z',
-  isPublic: true,
-  level: Level.Initial,
-  exports: { createMany: { data: [] } },
-  createdBy: { connect: { id: mockedUserId } },
-  organization: { connect: { id: mockedOrganizationId } },
-  version: { connect: { id: 'mocked-version-id' } },
-  allowedUsers: { createMany: { data: [{ role: 'Validator', userId: mockedUserId }] } },
-  sites: {
-    createMany: {
-      data: [{ siteId: 'mocked-site-id', etp: 64, ca: 6906733.42 }],
-    },
-  },
-}
-
-const getMockedDbUser = (props: Partial<User>): User => ({ ...mockedDbUser, ...props })
-const getMockedStudy = (props: Partial<Prisma.StudyCreateInput>) => ({ ...mockedStudy, ...props })
 
 // mocked called function
 jest.mock('@/db/user', () => ({ getUserByEmail: jest.fn() }))
@@ -57,9 +17,9 @@ jest.mock('../serverFunctions/emissionFactor', () => ({ getEmissionFactorByIds: 
 const mockGetUserByEmail = dbUserModule.getUserByEmail as jest.Mock
 const mockCheckOrganization = organizationModule.checkOrganization as jest.Mock
 
-const advancedStudy = getMockedStudy({ level: Level.Advanced }) as Prisma.StudyCreateInput
-const standardStudy = getMockedStudy({ level: Level.Standard }) as Prisma.StudyCreateInput
-const initialStudy = getMockedStudy({ level: Level.Initial }) as Prisma.StudyCreateInput
+const advancedStudy = getMockedStudy({ level: Level.Advanced })
+const standardStudy = getMockedStudy({ level: Level.Standard })
+const initialStudy = getMockedStudy({ level: Level.Initial })
 
 describe('Study permissions service', () => {
   describe('canCreateStudy', () => {
@@ -74,17 +34,17 @@ describe('Study permissions service', () => {
       })
 
       it('User should be able to create an "Advanced" study', async () => {
-        const result = await canCreateStudy(mockedUser, advancedStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', advancedStudy, mockedOrganizationId)
         expect(result).toBe(true)
       })
 
       it('User should be able to create a "Standard" study', async () => {
-        const result = await canCreateStudy(mockedUser, standardStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', standardStudy, mockedOrganizationId)
         expect(result).toBe(true)
       })
 
       it('User should be able to create an "Initial" study', async () => {
-        const result = await canCreateStudy(mockedUser, initialStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', initialStudy, mockedOrganizationId)
         expect(result).toBe(true)
       })
     })
@@ -95,17 +55,17 @@ describe('Study permissions service', () => {
       })
 
       it('User should not be able to create an "Advanced" study', async () => {
-        const result = await canCreateStudy(mockedUser, advancedStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', advancedStudy, mockedOrganizationId)
         expect(result).toBe(false)
       })
 
       it('User should be able to create a "Standard" study', async () => {
-        const result = await canCreateStudy(mockedUser, standardStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', standardStudy, mockedOrganizationId)
         expect(result).toBe(true)
       })
 
       it('User should be able to create an "Initial" study', async () => {
-        const result = await canCreateStudy(mockedUser, initialStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', initialStudy, mockedOrganizationId)
         expect(result).toBe(true)
       })
     })
@@ -116,17 +76,17 @@ describe('Study permissions service', () => {
       })
 
       it('User should not be able to create an "Advanced" study', async () => {
-        const result = await canCreateStudy(mockedUser, advancedStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', advancedStudy, mockedOrganizationId)
         expect(result).toBe(false)
       })
 
       it('User should not be able to create a "Standard" study', async () => {
-        const result = await canCreateStudy(mockedUser, standardStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', standardStudy, mockedOrganizationId)
         expect(result).toBe(false)
       })
 
       it('User should be able to create an "Initial" study', async () => {
-        const result = await canCreateStudy(mockedUser, initialStudy, mockedOrganizationId)
+        const result = await canCreateStudy('mocked-email', initialStudy, mockedOrganizationId)
         expect(result).toBe(true)
       })
     })
