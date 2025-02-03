9e00bb4a7c46a4789180be8cf20897dd78509f9c
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2025-02-03 16:41:23 +0100

    test script ftp

--- src/scripts/ftp/importUsers.ts
+++ src/scripts/ftp/importUsers.ts
@@ -1,11 +1,12 @@
 import { AccessOptions, Client } from 'basic-ftp'
 import dotenv from 'dotenv'
 import fs from 'fs'
-import path from 'path'
+import { Writable } from 'stream'
+
+
 
 export const getUsersFromFTP = async () => {
   const client = new Client()
-  client.ftp.verbose = true
 
   const accessOptions = {
     host: process.env.FTP_HOST,
@@ -17,40 +18,20 @@ export const getUsersFromFTP = async () => {
   await client.access(accessOptions)
 
   const fileName = process.env.FTP_FILE_NAME || ''
-  const filePath = process.env.FTP_FILE_PATH || ''
-  const fullPath = path.join(filePath, fileName)
 
-  // Validate file name
-  const invalidChars = /[<>:"/\\|?*\x00-\x1F]/g
-  if (invalidChars.test(fileName)) {
-    console.error(`File name ${fileName} contains invalid characters`)
-    client.close()
-    return
-  }
+  const writableStream = fs.createWriteStream(fileName)
 
+  const test = await client.downloadTo(writableStream, fileName)
 
-  console.log(await client.list())
+  writableStream.on("data", (chunk) => {
+    console.log("chunk : ", chunk)
+    console.log(`Received ${chunk.length} bytes of data.`)
+  })
 
-  // Check if the file exists and is a regular file
-  const fileList = await client.list(filePath)
-  const file = fileList.find(f => f.name === fileName)
-  if (!file) {
-    console.error(`File ${fileName} does not exist at path ${filePath}`)
-    client.close()
-    return
-  }
-  if (file.type === 0) {
-    console.error(`File ${fileName} is not a regular file`)
-    client.close()
-    return
-  }
-  const writableStream = fs.createWriteStream(file.name)
+  writableStream.on('end', () => {
+    console.log('No more data.');
+  });
 
-  await client.uploadFrom("README.md", "README_FTP.md")
-
-  await client.downloadTo("C:/Users/RomainCABC/Documents/git/bcplus2/bilan-carbone/src/scripts/ftp/" + fileName, fullPath)
-  // const test = await client.downloadTo(writableStream, "README_FTP.md")
-  // console.log(test)
   client.close()
 }
 
