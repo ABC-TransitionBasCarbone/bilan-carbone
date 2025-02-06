cfd952ef783dd98dd9646a606d500dc9f4f1682c
lfo <louis@dewolf.fr>
2025-02-06 12:44:21 +0100

    fix emission factors for user without organization

--- src/components/emissionFactor/EmissionFactors.tsx
+++ src/components/emissionFactor/EmissionFactors.tsx
@@ -2,7 +2,6 @@
 
 import { getLocale } from '@/i18n/locale'
 import { getEmissionFactors } from '@/services/emissionFactors'
-import NotFound from '../pages/NotFound'
 import EmissionFactorsTable from './Table'
 
 interface Props {
@@ -12,10 +11,6 @@ interface Props {
 const EmissionFactors = async ({ userOrganizationId }: Props) => {
   const locale = await getLocale()
   const emissionFactors = await getEmissionFactors(locale)
-
-  if (!userOrganizationId) {
-    return <NotFound />
-  }
 
   return <EmissionFactorsTable emissionFactors={emissionFactors} userOrganizationId={userOrganizationId} />
 }
