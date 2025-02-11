f3db19cbe4d69e66503650257c9bf56fead17fcf
RomainCrevecoeur <162474058+RomainCrevecoeur@users.noreply.github.com>
2025-02-11 10:16:47 +0100

    Modification du script de CSV à JSON (#501)
    * json KO
    
    * test script ftp
    
    * script functionnal
    
    * add to gitignore
    
    * fix lint
    
    * fix const instead of let
    
    * file date + fix lint
    
    * add role gestionnaire + fix console log
    
    * change siretOrSiren
    
    * isCR with purchased products
    level of training
    
    * remove login + training level
    
    * remove gitignore package.json.local
    
    * add date + skip non abc users + isCR
    
    * fix lint
    
    * add folder  + default value  + remove TODO + Role Gestionnaire
    
    * add migration to user in db
    
    * change script
    
    * remove variables in package.json
    
    * change readme
    
    * fix lint
    
    * remove_default_value_import_file_date
    
    * remove includes only ABC users
    
    * fix seed
    
    * fix lint
    
    * fix seed
    
    * fix lint
    
    * fix seed
    
    * non mandatory variabel
    
    * update readme
    
    * fix readme
    
    * squash commits prisma

--- src/scripts/ftp/importUsers.ts
+++ src/scripts/ftp/importUsers.ts
@@ -1,10 +1,10 @@
-import { Prisma, Role } from '@prisma/client'
+import { Level, Prisma, Role } from '@prisma/client'
 import { AccessOptions, Client } from 'basic-ftp'
-import { parse } from 'csv-parse'
 import dotenv from 'dotenv'
+import fs from 'fs'
 import { prismaClient } from '../../db/client'
 
-export const getUsersFromFTP = async () => {
+const getUsersFromFTP = async () => {
   const client = new Client()
 
   const accessOptions = {
@@ -16,49 +16,74 @@ export const getUsersFromFTP = async () => {
 
   await client.access(accessOptions)
 
-  const fileName = process.env.FTP_FILE_NAME || ''
+  const folderPath = process.env.FTP_FILE_PATH + '/'
+  const fileName = `${folderPath + process.env.FTP_FILE_NAME}`
+  const writableStream = fs.createWriteStream(fileName)
+
+  await client.downloadTo(writableStream, fileName)
+  const fileList = await client.list()
+  const file = fileList.find((f) => f.name === fileName)
+  const fileDate = new Date(file?.rawModifiedAt || Date.now())
+  client.close()
+
+  const data = await fs.promises.readFile(fileName, 'utf-8')
+  const values = JSON.parse(data)
+  console.log(`Users parsed : ${values.length} rows`)
 
   const users: Prisma.UserCreateManyInput[] = []
-  const parseStream = parse({ delimiter: ';', columns: true, bom: true })
-    .on('data', (value) => {
-      if (users.length % 10 === 0) {
-        console.log(`Processed ${users.length} lines...`)
-      }
-      const email = value['User Email']
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i]
+    const email = value['User_Email']
+    const siretOrSiren = value['SIRET']
+    const sessionCodeTraining = value['Session_Code']
 
-      users.push({
-        email,
-        role: Role.DEFAULT,
-        firstName: email,
-        lastName: email,
-        isActive: false,
-        isValidated: false,
-        organizationId: value['SIRET'],
+    console.log(`Processing ${email}`)
+    if (i % 50 === 0) {
+      console.log(`${i}/${values.length}`)
+    }
+    const user: Prisma.UserCreateManyInput = {
+      email,
+      role: Role.GESTIONNAIRE,
+      firstName: '',
+      lastName: '',
+      isActive: false,
+      isValidated: false,
+      importedFileDate: fileDate,
+    }
+
+    if (sessionCodeTraining) {
+      user.level = sessionCodeTraining.includes('BCM2') ? Level.Advanced : Level.Initial
+      user.role = Role.ADMIN
+    }
+
+    if (siretOrSiren) {
+      let organisation = await prismaClient.organization.findFirst({
+        where: { siret: { startsWith: siretOrSiren } },
       })
-    })
-    .on('end', async () => {
-      console.log('Parsing complete, saving users to database...')
+      if (!organisation) {
+        const name = value['Company_Name']
+        const purchasedProducts = value['Purchased_Products']
 
-      for (const user of users) {
-        if (user.organizationId) {
-          const result = await prismaClient.organization.findFirst({
-            where: { siret: { startsWith: user.organizationId } },
-          })
-          user.organizationId = result?.id
-        }
+        organisation = await prismaClient.organization.create({
+          data: {
+            siret: siretOrSiren,
+            name,
+            isCR: ['adhérent_conseil', 'licence_exploitation'].includes(purchasedProducts),
+            importedFileDate: fileDate,
+          },
+        })
       }
+      user.organizationId = organisation.id
+    }
+    users.push(user)
+  }
 
-      await prismaClient.user.createMany({ data: users, skipDuplicates: true })
-      console.log(`Done! ${users.length} users imported.`)
-    })
-    .on('error', (err) => {
-      console.error('Error during parsing:', err)
-      client.close()
-    })
+  const created = await prismaClient.user.createMany({
+    data: users,
+    skipDuplicates: true,
+  })
 
-  console.log('Downloading file...')
-  await client.downloadTo(parseStream, fileName)
-  client.close()
+  console.log(`${created.count} users created`)
 }
 
 dotenv.config()
