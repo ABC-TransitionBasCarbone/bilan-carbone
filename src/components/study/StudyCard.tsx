f6a010780861b1016ec536a94aa522b125cf96f4
lfo <louis@dewolf.fr>
2025-02-14 17:17:40 +0100

    update studies list view

---
+++ src/components/study/StudyCard.tsx
@@ -0,0 +1,63 @@
+import { getStudyValidatedEmissionsSources } from '@/db/study'
+import Leaf from '@mui/icons-material/Spa'
+import LinearProgress from '@mui/material/LinearProgress'
+import { Study } from '@prisma/client'
+import classNames from 'classnames'
+import { getTranslations } from 'next-intl/server'
+import Box from '../base/Box'
+import LinkButton from '../base/LinkButton'
+import styles from './StudyCard.module.css'
+
+interface Props {
+  study: Study
+}
+
+const StudyCard = async ({ study }: Props) => {
+  const t = await getTranslations('study')
+  const values = await getStudyValidatedEmissionsSources(study.id)
+
+  if (!values) {
+    return null
+  }
+
+  const percent = values.validated ? Math.floor((values.validated / values.total) * 100) : 0
+
+  const color = values.validated > 0 && percent === 100 ? '--green-500' : '--orange-500'
+
+  return (
+    <li data-testid="study" className="flex">
+      <Box className={classNames(styles.card, 'flex-col grow')}>
+        <div className={classNames(styles.studyName, 'align-center')}>
+          <Leaf />
+          {study.name}
+        </div>
+        <Box>
+          <p className="mb1">
+            {t.rich('validatedSources', {
+              validated: values.validated,
+              total: values.total,
+              data: (children) => (
+                <span className={classNames(styles.validated, { [styles.success]: percent === 100 })}>{children}</span>
+              ),
+            })}
+          </p>
+          <LinearProgress
+            variant="determinate"
+            value={percent}
+            sx={{
+              backgroundColor: 'var(--greyscale-200)',
+              '& .MuiLinearProgress-bar': {
+                backgroundColor: `var(${color})`,
+              },
+            }}
+          />
+        </Box>
+        <div className="justify-end">
+          <LinkButton href={`/etudes/${study.id}`}>{t('see')}</LinkButton>
+        </div>
+      </Box>
+    </li>
+  )
+}
+
+export default StudyCard
