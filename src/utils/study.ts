f08be57dcab3eb680dc642f3f6bf2baf1f7ed0fb
lfo <louis@dewolf.fr>
2025-02-17 16:20:19 +0100

    fix user access to public studies

--- src/utils/study.ts
+++ src/utils/study.ts
@@ -1,11 +1,14 @@
 import { FullStudy } from '@/db/study'
 import { isAdminOnStudyOrga } from '@/services/permissions/study'
-import { StudyRole } from '@prisma/client'
+import { Role, StudyRole } from '@prisma/client'
 import { User } from 'next-auth'
 
 export const getUserRoleOnStudy = (user: User, study: FullStudy) => {
   if (isAdminOnStudyOrga(user, study)) {
     return StudyRole.Validator
+  }
+  if (study.isPublic && study.organizationId === user.organizationId) {
+    return user.role === Role.DEFAULT ? StudyRole.Editor : StudyRole.Reader
   }
   const right = study.allowedUsers.find((right) => right.user.email === user.email)
   return right ? right.role : null
