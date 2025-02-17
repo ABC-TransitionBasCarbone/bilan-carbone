dc65d1cca0404bc8f33d0b2094d4e85d5c6c23df
Chloe M <chloe.molle@neuf.fr>
2025-02-17 14:07:12 +0100

    remove temporarily the orga info

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,5 @@
 import withAuth, { UserProps } from '@/components/hoc/withAuth'
 import Navbar from '@/components/navbar/Navbar'
-import OrganizationCard from '@/components/organizationCard/OrganizationCard'
-import { getUserOrganizations } from '@/db/user'
 import classNames from 'classnames'
 import styles from './layout.module.css'
 
@@ -10,14 +8,10 @@ interface Props {
 }
 
 const NavLayout = async ({ children, user }: Props & UserProps) => {
-  const organizations = await getUserOrganizations(user.email)
   return (
     <div className="flex-col h100">
       <Navbar user={user} />
-      {user.organizationId && <OrganizationCard user={user} organizations={organizations} />}
-      <main className={classNames(styles.content, { [styles.withOrganizationCard]: user.organizationId })}>
-        {children}
-      </main>
+      <main className={classNames(styles.content)}>{children}</main>
     </div>
   )
 }
