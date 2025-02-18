0cec436542a5fb8714b4c37f87a17c3cb63857cb
lfo <louis@dewolf.fr>
2025-02-18 15:21:40 +0100

    add form to login view

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,13 +1,18 @@
 'use client'
 
-import { TextField } from '@mui/material'
+import { LoginCommand, LoginCommandValidation } from '@/services/serverFunctions/user.command'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormControl } from '@mui/material'
 import classNames from 'classnames'
 import { signIn } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { FormEvent, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import Form from '../base/Form'
 import LoadingButton from '../base/LoadingButton'
+import { FormTextField } from '../form/TextField'
 import authStyles from './Auth.module.css'
 import styles from './LoginForm.module.css'
 
@@ -15,17 +20,24 @@ const LoginForm = () => {
   const t = useTranslations('login.form')
   const router = useRouter()
   const [submitting, setSubmitting] = useState(false)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+
+  const form = useForm<LoginCommand>({
+    resolver: zodResolver(LoginCommandValidation),
+    mode: 'onBlur',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setError('')
     setSubmitting(true)
     const result = await signIn('credentials', {
-      email,
-      password,
+      ...form.getValues(),
       redirect: false,
     })
 
@@ -38,47 +50,48 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onSubmit} className={classNames(authStyles.form, authStyles.medium)}>
-      <TextField
-        data-testid="input-email"
-        className={authStyles.input}
-        required
-        label={t('email')}
-        type="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <TextField
-        data-testid="input-password"
-        className={authStyles.input}
-        required
-        label={t('password')}
-        value={password}
-        type="password"
-        onChange={(event) => setPassword(event.target.value)}
-        helperText={error}
-        error={!!error}
-      />
-      <Link
-        data-testid="reset-password-link"
-        className={styles.link}
-        href={`/reset-password?email=${email}`}
-        prefetch={false}
-      >
-        {t('forgotPassword')}
-      </Link>
-      <div>
-        <LoadingButton data-testid="login-button" type="submit" loading={submitting} fullWidth>
-          {t('login')}
-        </LoadingButton>
-      </div>
-      <div className={styles.activation}>
-        {t('firstConnection')}
-        <Link data-testid="activation-button" className="ml-2" href="/activation" prefetch={false}>
-          {t('activate')}
+    <Form onSubmit={onSubmit} className={classNames(authStyles.form, authStyles.medium)}>
+      <FormControl>
+        <FormTextField
+          className="grow"
+          control={form.control}
+          name="email"
+          label={t('email')}
+          translation={t}
+          data-testid="input-email"
+        />
+        <FormTextField
+          className={classNames(authStyles.input, 'grow')}
+          control={form.control}
+          name="password"
+          label={t('password')}
+          translation={t}
+          data-testid="input-password"
+          type="password"
+          helperText={error}
+          error={!!error}
+        />
+        <Link
+          data-testid="reset-password-link"
+          className={styles.link}
+          href={`/reset-password?email=${form.getValues().email}`}
+          prefetch={false}
+        >
+          {t('forgotPassword')}
         </Link>
-      </div>
-    </form>
+        <div>
+          <LoadingButton data-testid="login-button" type="submit" loading={submitting} fullWidth>
+            {t('login')}
+          </LoadingButton>
+        </div>
+        <div className={styles.activation}>
+          {t('firstConnection')}
+          <Link data-testid="activation-button" className="ml-2" href="/activation" prefetch={false}>
+            {t('activate')}
+          </Link>
+        </div>
+      </FormControl>
+    </Form>
   )
 }
 
