698d1857efaf99753f6d293f6b60b581954d5424
lfo <louis@dewolf.fr>
2025-02-20 10:05:09 +0100

    do not authorize users with no organization to create emission factor

--- src/app/(dashboard)/facteurs-d-emission/creer/page.tsx
+++ src/app/(dashboard)/facteurs-d-emission/creer/page.tsx
@@ -1,7 +1,16 @@
 import withAuth from '@/components/hoc/withAuth'
 import NewEmissionFactorPage from '@/components/pages/NewEmissionFactor'
+import NotFound from '@/components/pages/NotFound'
+import { User } from '@prisma/client'
 
-const NewEmissionFactor = async () => {
+interface Props {
+  user: User
+}
+
+const NewEmissionFactor = async ({ user }: Props) => {
+  if (!user.organizationId) {
+    return <NotFound />
+  }
   return <NewEmissionFactorPage />
 }
 
