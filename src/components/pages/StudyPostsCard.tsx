669ff137dedce02f4c451e7bfa80674fa8df9877
lfo <louis@dewolf.fr>
2025-02-20 10:02:47 +0100

    improve study post card layout

--- src/components/pages/StudyPostsCard.tsx
+++ src/components/pages/StudyPostsCard.tsx
@@ -1,12 +1,14 @@
 import { FullStudy } from '@/db/study'
 import { Post, subPostsByPost } from '@/services/posts'
+import { colors, postColors } from '@/utils/study'
 import Leaf from '@mui/icons-material/Spa'
 import { LinearProgress } from '@mui/material'
 import { StudyRole } from '@prisma/client'
 import classNames from 'classnames'
 import { useTranslations } from 'next-intl'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useMemo } from 'react'
 import Box from '../base/Box'
+import PostIcon from '../study/infography/icons/PostIcon'
 import SelectStudySite from '../study/site/SelectStudySite'
 import styles from './StudyPostsCard.module.css'
 
@@ -17,12 +19,16 @@ interface Props {
   studySite: string
   setSite: Dispatch<SetStateAction<string>>
 }
-
 const StudyPostsCard = ({ study, post, userRole, studySite, setSite }: Props) => {
   const t = useTranslations('study')
+  const tRole = useTranslations('study.role')
   const tPost = useTranslations('emissionFactors.post')
-  const emissionSources = study.emissionSources.filter((emissionSource) =>
-    subPostsByPost[post].includes(emissionSource.subPost),
+
+  const postColor = useMemo(() => (post ? postColors[post] : 'green'), [post])
+
+  const emissionSources = study.emissionSources.filter(
+    (emissionSource) =>
+      subPostsByPost[post].includes(emissionSource.subPost) && emissionSource.studySite.id === studySite,
   )
   const validated = emissionSources.filter((emissionSource) => emissionSource.validated).length
   const percent = Math.floor((validated / emissionSources.length) * 100)
@@ -31,14 +37,19 @@ const StudyPostsCard = ({ study, post, userRole, studySite, setSite }: Props) =>
   return (
     <div className="justify-center">
       <Box className={classNames(styles.card, 'flex-col')}>
-        <div className={classNames(styles.post, 'text-center')}>{tPost(post)}</div>
+        <div
+          className={classNames(styles.post, 'flex-cc')}
+          style={{ borderColor: colors[postColor].dark, color: colors[postColor].dark }}
+        >
+          <PostIcon className={styles.icon} post={post} />
+          {tPost(post)}
+        </div>
         <div className={classNames(styles.name, 'justify-between align-center')}>
           <div className="align-center">
             <Leaf />
             <span className="ml-2">{study.name}</span>
           </div>
-
-          <div className={classNames(styles.role, 'text-center')}>{userRole}</div>
+          <div className={classNames(styles.role, 'text-center')}>{tRole(userRole)}</div>
         </div>
         <p className="text-center">{t('selectSite')}</p>
         <SelectStudySite study={study} studySite={studySite} setSite={setSite} withLabel={false} />
