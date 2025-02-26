dcd86e31054e9ab715210924fa1053e1fc2472b3
lfo <louis@dewolf.fr>
2025-02-26 10:54:14 +0100

    add zustand first store

--- src/components/RouteChangeListener.tsx
+++ src/components/RouteChangeListener.tsx
@@ -1,13 +1,8 @@
 'use client'
 
+import { Context, ORGANIZATION, OTHER, STUDY, useAppContextStore } from '@/store/AppContext'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
-
-export const STUDY = 'STUDY'
-export const ORGANIZATION = 'ORGANIZATION'
-export const OTHER = 'OTHER'
-
-export type Context = typeof STUDY | typeof ORGANIZATION | typeof OTHER
 
 const uuidv4Pattern = /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}/
 const etudesPattern = new RegExp(`^/etudes/${uuidv4Pattern.source}`)
@@ -15,8 +10,10 @@ const organizationsPattern = new RegExp(`^/organisations/${uuidv4Pattern.source}
 
 const RouteChangeListener = () => {
   const pathname = usePathname()
-  const [context, setContext] = useState<Context>('OTHER')
-  const [targetId, setTargetId] = useState('')
+  const [context, setLocalContext] = useState<Context>('OTHER')
+  const [contextId, setLocalContextId] = useState('')
+
+  const { setContext, setContextId } = useAppContextStore()
 
   const getContext = (pathname: string): Context => {
     if (etudesPattern.test(pathname)) {
@@ -36,11 +33,13 @@ const RouteChangeListener = () => {
   useEffect(() => {
     const newContext = getContext(pathname)
     if (context !== newContext) {
+      setLocalContext(newContext)
       setContext(newContext)
     }
-    const newTargetId = getTargetId(pathname)
-    if (targetId !== newTargetId) {
-      setTargetId(newTargetId)
+    const newContextId = getTargetId(pathname)
+    if (contextId !== newContextId) {
+      setLocalContextId(newContextId)
+      setContextId(newContextId)
     }
   }, [pathname])
 
