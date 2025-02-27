428451d4267f3424af176280686320d69fc74cb3
jilink <jimmyfac1@gmail.com>
2025-02-27 11:41:00 +0100

    refactor: using post object with subpost array instead of one subpost

--- src/services/serverFunctions/emissionFactor.command.ts
+++ src/services/serverFunctions/emissionFactor.command.ts
@@ -38,7 +38,7 @@ export const EmissionFactorCommandValidation = z.intersection(
       })
       .min(0, 'totalCo2'),
     attribute: z.string().optional(),
-    subPost: z.nativeEnum(SubPost, { required_error: 'subPost' }),
+    subPosts: z.record(z.array(z.nativeEnum(SubPost, { required_error: 'subPost' }))),
     comment: z.string().optional(),
     parts: z
       .array(
