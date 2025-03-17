6adcb7cfe85215c1facd6006c9fab16f781e8456
Laurent BERTHOLLE <laurent.bertholle@aconume.com>
2025-03-17 16:43:41 +0100

    Parse studies, study sites and study exports.

--- src/scripts/oldBC/transition/studies.ts
+++ src/scripts/oldBC/transition/studies.ts
@@ -21,12 +21,72 @@ export enum RequiredStudyExportsColumns {
 
 interface Study {
   id: string
+  name: string
+  startDate: string
+  endDate: string
+  sites: Site[]
+  exports: Export[]
+}
+
+interface Site {
+  id: string
+}
+
+interface Export {
+  type: string
+  control: string
 }
 
 const parseStudies = (indexes: Record<string, number>, data: (string | number)[][]): Study[] => {
   return data.slice(1).map((row) => ({
     id: row[indexes[RequiredStudiesColumns.id]] as string,
+    name: row[indexes[RequiredStudiesColumns.name]] as string,
+    startDate: row[indexes[RequiredStudiesColumns.startDate]] as string,
+    endDate: row[indexes[RequiredStudiesColumns.endDate]] as string,
+    sites: [],
+    exports: [],
   }))
+}
+
+const parseSites = (indexes: Record<string, number>, data: (string | number)[][]): Map<string, Site[]> => {
+  return data
+    .slice(1)
+    .map<[string, Site]>((row) => [
+      row[indexes[RequiredStudySitesColumns.studyId]] as string,
+      {
+        id: row[indexes[RequiredStudySitesColumns.id]] as string,
+      },
+    ])
+    .reduce((accumulator, currentValue) => {
+      const sites = accumulator.get(currentValue[0])
+      if (sites) {
+        sites.push(currentValue[1])
+      } else {
+        accumulator.set(currentValue[0], [currentValue[1]])
+      }
+      return accumulator
+    }, new Map<string, Site[]>())
+}
+
+const parseExports = (indexes: Record<string, number>, data: (string | number)[][]): Map<string, Export[]> => {
+  return data
+    .slice(1)
+    .map<[string, Export]>((row) => [
+      row[indexes[RequiredStudyExportsColumns.studyId]] as string,
+      {
+        type: row[indexes[RequiredStudyExportsColumns.type]] as string,
+        control: row[indexes[RequiredStudyExportsColumns.control]] as string,
+      },
+    ])
+    .reduce((accumulator, currentValue) => {
+      const sites = accumulator.get(currentValue[0])
+      if (sites) {
+        sites.push(currentValue[1])
+      } else {
+        accumulator.set(currentValue[0], [currentValue[1]])
+      }
+      return accumulator
+    }, new Map<string, Export[]>())
 }
 
 export const uploadStudies = async (
@@ -41,6 +101,26 @@ export const uploadStudies = async (
   console.log('Import des études...')
 
   const studies = parseStudies(studiesIndexes, studiesData)
+  const studySites = parseSites(studySitesIndexes, studySitesData)
+  const studyExports = parseExports(studyExportsIndexes, studyExportsData)
+
+  studySites.entries().forEach(([studyId, sites]) => {
+    const study = studies.find((study) => study.id === studyId)
+    if (study) {
+      study.sites = sites
+    } else {
+      console.warn(`Study of id ${studyId} not found.`)
+    }
+  })
+
+  studyExports.entries().forEach(([studyId, exports]) => {
+    const study = studies.find((study) => study.id === studyId)
+    if (study) {
+      study.exports = exports
+    } else {
+      console.warn(`Study of id ${studyId} not found.`)
+    }
+  })
 
   console.log(studies)
 
