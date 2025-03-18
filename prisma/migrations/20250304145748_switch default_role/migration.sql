2774bb4a911b2a8d9c73bcc4283acf9084c6ef59
lfo <louis@dewolf.fr>
2025-03-18 16:37:28 +0100

    do not switch role for untrained default members

--- prisma/migrations/20250304145748_switch default_role/migration.sql
+++ prisma/migrations/20250304145748_switch default_role/migration.sql
@@ -1,2 +1,2 @@
 UPDATE "licenses" SET "rights" = array_replace("rights", 'DEFAULT', 'COLLABORATOR') WHERE 'DEFAULT' = ANY("rights");
-UPDATE "users" SET "role" = 'COLLABORATOR' WHERE "role" = 'DEFAULT';
+UPDATE "users" SET "role" = 'COLLABORATOR' WHERE "role" = 'DEFAULT' AND "level" IS NOT NULL;
