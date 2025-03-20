208ba418e817f84e8f8e784d2d50443179bf0976
jilink <jimmyfac1@gmail.com>
2025-03-20 16:06:23 +0100

    feat: redirect to home for restricted pages

--- src/environments/core/EnvironmentInitializer.tsx
+++ src/environments/core/EnvironmentInitializer.tsx
@@ -1,15 +1,30 @@
 'use client'
 
 import { switchEnvironment } from '@/i18n/environment'
-import { useAppEnvironmentStore } from '@/store/AppEnvironment'
+import { CUT, Environment, useAppEnvironmentStore } from '@/store/AppEnvironment'
+import { usePathname, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+
+const RESTRICTED_ROUTES: Partial<Record<Environment, string[]>> = {
+  [CUT]: ['/parametres', '/facteurs-d-emission'],
+}
 
 const EnvironmentInitializer = () => {
   const { environment } = useAppEnvironmentStore()
+  const router = useRouter()
+  const pathname = usePathname()
 
   useEffect(() => {
     switchEnvironment(environment)
   }, [environment])
+
+  useEffect(() => {
+    const restrictedRoutes = RESTRICTED_ROUTES[environment] || []
+    const isRestricted = restrictedRoutes.some((route) => pathname.startsWith(route))
+    if (isRestricted) {
+      router.push('/')
+    }
+  }, [environment, pathname])
 
   return <></>
 }
