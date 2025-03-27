1bfbe715d6bd8f5157b770be299c123e63db500f
jilink <jimmyfac1@gmail.com>
2025-03-27 11:48:32 +0100

    refactor: account in session with user data

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,13 @@
-import { getUserByEmailWithSensibleInformations } from '@/db/user'
 import { getUserByEmail } from '@/db/userImport'
-import { Level, Role, UserStatus } from '@prisma/client'
+import { getUserByEmailWithSensibleInformations } from '@/db/user'
+import { Level, PrismaClient, Role, UserStatus } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 import { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession, NextAuthOptions } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import { DAY } from '../utils/time'
+
+const prisma = new PrismaClient()
 
 export const signPassword = async (password: string) => {
   const salt = await bcrypt.genSalt(10)
@@ -26,25 +28,50 @@ export const authOptions: NextAuthOptions = {
     },
     async jwt({ token, trigger, user }) {
       if (user) {
-        return {
-          ...token,
-          id: user.id,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          role: user.role,
-          organizationId: user.organizationId,
-          level: user.level,
+        // TODO GET THE RIGHT ACCOUNT
+        const account = await prisma.account.findFirst({
+          where: { userId: user.id },
+          select: {
+            id: true,
+            role: true,
+            organizationId: true,
+            user: {
+              select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                level: true,
+                email: true,
+              },
+            },
+          },
+        })
+
+        if (account) {
+          return {
+            ...token,
+            id: account.user.id,
+            accountId: account.id,
+            firstName: account.user.firstName,
+            lastName: account.user.lastName,
+            organizationId: account.organizationId,
+            role: account.role,
+            level: account.user.level,
+          }
         }
       }
+
       if (trigger === 'update') {
         const dbUser = await getUserByEmail(token.email || '')
 
         // TODO GET THE RIGHT ACCOUNT
-        const account = dbUser?.accounts[0] || { role: Role.DEFAULT, organizationId: '' }
+        const account = dbUser?.accounts[0] || { id: '', role: Role.DEFAULT, organizationId: '' }
+
         return dbUser
           ? {
               ...token,
               id: dbUser.id,
+              accountId: account.id,
               firstName: dbUser.firstName,
               lastName: dbUser.lastName,
               role: account.role,
@@ -53,13 +80,16 @@ export const authOptions: NextAuthOptions = {
             }
           : token
       }
+
       return token
     },
     async session({ session, token }) {
+      // TODO GET THE RIGHT ACCOUNT
       if (session.user) {
         session.user = {
           ...session.user,
           id: token.id as string,
+          accountId: token.accountId as string,
           firstName: token.firstName as string,
           lastName: token.lastName as string,
           organizationId: token.organizationId as string,
@@ -84,8 +114,8 @@ export const authOptions: NextAuthOptions = {
         }
 
         const user = await getUserByEmailWithSensibleInformations(credentials.email)
-        const account = user?.accounts[0]
         // TODO GET THE RIGHT ACCOUNT
+        const account = user?.accounts[0]
         if (!user || !user.password || user.status !== UserStatus.ACTIVE) {
           return null
         }
@@ -97,6 +127,7 @@ export const authOptions: NextAuthOptions = {
 
         return {
           id: user.id,
+          accountId: account?.id,
           firstName: user.firstName,
           lastName: user.lastName,
           role: account?.role,
