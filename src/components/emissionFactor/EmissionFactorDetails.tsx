12ea8ea90c4709cf742887c1e0668ac588a320bc
lfo <louis@dewolf.fr>
2025-04-04 16:00:56 +0200

    refacto emission factor details

--- src/components/emissionFactor/EmissionFactorDetails.tsx
+++ src/components/emissionFactor/EmissionFactorDetails.tsx
@@ -1,11 +1,13 @@
 import { gazKeys } from '@/constants/emissions'
-import { subPostsByPost } from '@/services/posts'
+import { Post, subPostsByPost } from '@/services/posts'
 import { EmissionFactorWithMetaData } from '@/services/serverFunctions/emissionFactor'
 import { getQualityRating, qualityKeys } from '@/services/uncertainty'
-import { Import } from '@prisma/client'
+import ShrinkIcon from '@mui/icons-material/ZoomInMap'
+import ExpandIcon from '@mui/icons-material/ZoomOutMap'
+import { Import, SubPost } from '@prisma/client'
 import classNames from 'classnames'
 import { useTranslations } from 'next-intl'
-import { useMemo } from 'react'
+import { Fragment, useMemo, useState } from 'react'
 import styles from './Detail.module.css'
 
 interface Props {
@@ -16,43 +18,63 @@ const EmissionFactorDetails = ({ emissionFactor }: Props) => {
   const tPost = useTranslations('emissionFactors.post')
   const tUnits = useTranslations('units')
   const tQuality = useTranslations('quality')
+  const [displayDetailedQuality, setDisplayDetailedQuality] = useState(false)
 
   const gases = useMemo(() => gazKeys.filter((gaz) => emissionFactor[gaz]), [emissionFactor])
   const qualities = useMemo(() => qualityKeys.filter((quality) => emissionFactor[quality]), [emissionFactor])
   const quality = useMemo(() => getQualityRating(emissionFactor), [emissionFactor])
 
+  const subPosts = emissionFactor.subPosts.reduce(
+    (res, subPost) => {
+      const post = Object.entries(subPostsByPost).find(([, value]) => value.includes(subPost))?.[0] as Post
+      return { ...res, [post]: (res[post] || []).concat([subPost]) }
+    },
+    {} as Record<Post, SubPost[]>,
+  )
+
   return (
     <>
       {emissionFactor.importedFrom !== Import.Manual && emissionFactor.version && (
         <div className={styles.info}>
-          {t(emissionFactor.importedFrom)} {emissionFactor.version.name} - {t(emissionFactor.status)}
+          {t(emissionFactor.importedFrom)} {emissionFactor.version.name}
         </div>
       )}
+      {emissionFactor.source && <div className={styles.info}>{emissionFactor.source}</div>}
       <div className={styles.info}>
-        <div>
-          {t('qualityRating')} {tQuality(quality?.toString())}
-        </div>
-        <div className={classNames(styles.list, 'flex')}>
-          {qualities.map((quality) => (
-            <div key={quality}>
-              {t(quality)} {tQuality(emissionFactor[quality]?.toString())}
-            </div>
-          ))}
+        <div className={classNames(styles.list, 'grid')}>
+          <span className="align-center">{t('qualityRating')}</span>
+          <span className="align-center">
+            {tQuality(quality?.toString())}
+            <span
+              className={classNames(styles.expandIcon, 'ml-4')}
+              onClick={() => setDisplayDetailedQuality(!displayDetailedQuality)}
+            >
+              {displayDetailedQuality ? <ShrinkIcon /> : <ExpandIcon />}
+            </span>
+          </span>
+          {displayDetailedQuality && (
+            <>
+              {qualities.map((quality) => (
+                <Fragment key={quality}>
+                  <span>{t(quality)}</span>
+                  <span>{tQuality(emissionFactor[quality]?.toString())}</span>
+                </Fragment>
+              ))}
+            </>
+          )}
         </div>
       </div>
-      {emissionFactor.subPosts.length > 0 && (
+      {Object.entries(subPosts).length > 0 && (
         <div className={styles.info}>
-          {emissionFactor.subPosts.map((subPost) => (
-            <div key={subPost}>
-              {t('post')} {tPost(Object.entries(subPostsByPost).find(([, value]) => value.includes(subPost))?.[0])} -{' '}
-              {t('subPost')} {tPost(subPost)}
+          {Object.entries(subPosts).map(([post, subPosts]) => (
+            <div key={post}>
+              {t('post')} {tPost(post)} ({subPosts.map((subPost) => tPost(subPost)).join(', ')})
             </div>
           ))}
         </div>
       )}
       {emissionFactor.metaData?.location && <div className={styles.info}>{emissionFactor.metaData.location}</div>}
       {emissionFactor.metaData?.comment && <div className={styles.info}>{emissionFactor.metaData.comment}</div>}
-      {emissionFactor.source && <div className={styles.info}>{emissionFactor.source}</div>}
       {gases.length > 0 && (
         <div className={classNames(styles.info, styles.list, 'flex')}>
           {gases.map((gaz) => (
