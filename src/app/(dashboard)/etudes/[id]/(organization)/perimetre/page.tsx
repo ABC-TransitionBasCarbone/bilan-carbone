0858495cb6901445c856bb580a34c03f1a57c5f2
lfo <louis@dewolf.fr>
2025-04-10 10:49:53 +0200

    fix study organization in perimeter view

--- src/app/(dashboard)/etudes/[id]/(organization)/perimetre/page.tsx
+++ src/app/(dashboard)/etudes/[id]/(organization)/perimetre/page.tsx
@@ -2,11 +2,14 @@ import withAuth, { UserProps } from '@/components/hoc/withAuth'
 import { StudyProps } from '@/components/hoc/withStudy'
 import withStudyDetails from '@/components/hoc/withStudyDetails'
 import StudyPerimeterPage from '@/components/pages/StudyPerimeter'
-import { getUserOrganizations } from '@/db/user'
+import { getOrganizationWithSitesById } from '@/db/organization'
 
 const StudyPerimeter = async ({ user, study }: StudyProps & UserProps) => {
-  const organizations = await getUserOrganizations(user.email)
-  return <StudyPerimeterPage study={study} user={user} organization={organizations[0]} />
+  const organization = await getOrganizationWithSitesById(study.organizationId)
+  if (!organization) {
+    return null
+  }
+  return <StudyPerimeterPage study={study} user={user} organization={organization} />
 }
 
 export default withAuth(withStudyDetails(StudyPerimeter))
