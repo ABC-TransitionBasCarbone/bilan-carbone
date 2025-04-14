0c89848636ff0fda0351f2fbfde55e45012e9e28
lfo <louis@dewolf.fr>
2025-04-14 10:10:40 +0200

    fix value for numbers < 0.1

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -4,6 +4,16 @@ import Big from 'big.js'
 export const formatNumber = (value: number, dec = 0) =>
   value.toLocaleString('fr-FR', { minimumFractionDigits: 0, maximumFractionDigits: dec })
 
+const countZerosAfterDecimal = (value: number): number => {
+  if (value >= 1 || value <= 0) {
+    return 0
+  }
+
+  const fixed = value.toFixed(20) // fix value to avoid exponential annotation e^
+  const match = fixed.match(/^0\.0*(?=\d)/)
+  return match ? match[0].length - 2 : 0
+}
+
 export const formatEmissionFactorNumber = (value: number): string => {
   if (Number.isInteger(value)) {
     return value.toString()
@@ -19,8 +29,7 @@ export const formatEmissionFactorNumber = (value: number): string => {
   if (decimalPart.length === 2) {
     return formatNumber(value, integerPart === 0 ? 2 : 1)
   }
-
-  return formatNumber(value, value >= 0.1 ? 2 : 5)
+  return formatNumber(value, value >= 0.1 ? 2 : countZerosAfterDecimal(value) + 2)
 }
 
 export const displayCA = (ca: number, factor: number) => new Big(ca).div(factor).toNumber()
