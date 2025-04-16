0fe61a8e060a30890ca774e3d28b532eb5b8b191
lfo <louis@dewolf.fr>
2025-04-16 13:45:04 +0200

    add mandatory incertainty fields

--- src/services/serverFunctions/emissionFactor.command.ts
+++ src/services/serverFunctions/emissionFactor.command.ts
@@ -38,10 +38,10 @@ export const EmissionFactorCommandValidation = z.intersection(
       })
       .min(0, 'totalCo2'),
     reliability: z.number({ required_error: 'reliability' }),
-    technicalRepresentativeness: z.number().optional(),
-    geographicRepresentativeness: z.number().optional(),
-    temporalRepresentativeness: z.number().optional(),
-    completeness: z.number().optional(),
+    technicalRepresentativeness: z.number({ required_error: 'technicalRepresentativeness' }),
+    geographicRepresentativeness: z.number({ required_error: 'geographicRepresentativeness' }),
+    temporalRepresentativeness: z.number({ required_error: 'temporalRepresentativeness' }),
+    completeness: z.number({ required_error: 'completeness' }),
     attribute: z.string().optional(),
     subPosts: z.record(z.array(z.nativeEnum(SubPost)).min(1), { required_error: 'type' }).superRefine((val, ctx) => {
       if (Object.keys(val).length === 0) {
