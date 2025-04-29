97089e72258f45a7a6eda00e8cfd3cf8474d6dd0
lfo <louis@dewolf.fr>
2025-04-29 11:11:36 +0200

    fix study creation e2e tests

--- src/tests/end-to-end/app/create-study.cy.ts
+++ src/tests/end-to-end/app/create-study.cy.ts
@@ -25,7 +25,7 @@ describe('Create study', () => {
     cy.get('[data-option-index="1"]').click()
 
     cy.getByTestId('new-study-endDate').within(() => {
-      cy.get('input').type(dayjs().add(1, 'y').format('DD/MM/YYYY'))
+      cy.get('span').first().type(dayjs().add(1, 'y').format('DD/MM/YYYY'))
     })
     cy.getByTestId('new-study-level').click()
     cy.get('[data-value="Initial"]').click()
@@ -49,7 +49,7 @@ describe('Create study', () => {
     cy.get('[data-option-index="1"]').click()
 
     cy.getByTestId('new-study-endDate').within(() => {
-      cy.get('input').type(dayjs().add(1, 'y').format('MM/DD/YYYY'))
+      cy.get('span').first().type(dayjs().add(1, 'y').format('MM/DD/YYYY'))
     })
     cy.getByTestId('new-study-level').click()
     cy.get('[data-value="Initial"]').click()
