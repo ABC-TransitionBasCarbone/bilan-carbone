5314ddf7a902c3040f3e86a3ef0e59d72c6425ee
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2025-05-06 09:50:48 +0200

    add updated date on every models

---
+++ prisma/schema/migrations/20250506074740_add_updated_at/migration.sql
@@ -0,0 +1,23 @@
+-- AlterTable
+ALTER TABLE "actualities" ALTER COLUMN "updated_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "deactivable_features_statuses" ALTER COLUMN "updated_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "documents" ALTER COLUMN "updated_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "emission_factor_parts" ALTER COLUMN "updated_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "licenses" ALTER COLUMN "updated_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "sites" ALTER COLUMN "updated_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "studies" ALTER COLUMN "updated_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "study_emission_sources" ALTER COLUMN "updated_at" DROP DEFAULT;
