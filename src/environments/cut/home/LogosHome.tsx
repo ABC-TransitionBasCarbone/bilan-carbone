62cb0ef2171cd48fa9afedc940eb45484999fca5
Lilian AUFFRET <lilian@auffret.dev>
2025-05-07 09:39:40 +0200

    fix(cut): le logo BT ne s'affiche pas

--- src/environments/cut/home/LogosHome.tsx
+++ src/environments/cut/home/LogosHome.tsx
@@ -8,7 +8,7 @@ import styles from './LogosHome.module.css'
 const logos = [
   { src: '/logos/cut/Republique_francaise.png', alt: 'Logo de la république française' },
   { src: '/logos/cut/France3_2025.png', alt: 'Logo de france 3' },
-  { src: '/logos/cut/Banques_des_Territoires.png', alt: 'Logo du groupe la caisse des dépots' },
+  { src: '/logos/cut/Banques_des_territoires.svg', alt: 'Logo du groupe la caisse des dépots' },
 ]
 
 const LogosHome = () => {
