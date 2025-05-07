486b40c64eae5e6b798ce0be5681fcadfedae25d
Lilian AUFFRET <lilian@auffret.dev>
2025-05-07 12:42:29 +0200

    feat(cut): ajouter des tests end-to-end

--- src/environments/cut/study/StudyHomeMessage.tsx
+++ src/environments/cut/study/StudyHomeMessage.tsx
@@ -9,32 +9,26 @@ const StudyHomeMessage = () => {
   const { environment } = useAppEnvironmentStore()
   const isCut = useMemo(() => environment === CUT, [environment])
 
-  if (!isCut) {
-    return null
-  }
-
   const t = useTranslations('home.message')
-  return (
-    isCut && (
-      <Alert color="info" className={styles.mb1}>
-        <Typography className={styles.whiteSpaces}>{t('core')}</Typography>
-        <List dense>
-          <ListItem disablePadding>
-            <ListItemText primary={t('0')} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary={t('1')} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary={t('2')} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary={t('3')} />
-          </ListItem>
-        </List>
-      </Alert>
-    )
-  )
+  return isCut ? (
+    <Alert color="info" className={styles.mb1}>
+      <Typography className={styles.whiteSpaces}>{t('core')}</Typography>
+      <List dense>
+        <ListItem disablePadding>
+          <ListItemText primary={t('0')} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={t('1')} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={t('2')} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={t('3')} />
+        </ListItem>
+      </List>
+    </Alert>
+  ) : null
 }
 
 export default StudyHomeMessage
