02ed09a9f2a3d3a8fa58e70f1a5c9b7304a68bf2
lfo <louis@dewolf.fr>
2025-05-13 11:51:20 +0200

    fix formation videos order : sort by creation date

--- src/db/formation.ts
+++ src/db/formation.ts
@@ -3,4 +3,4 @@ import { prismaClient } from './client'
 export const createFormation = (name: string, link: string) => prismaClient.formation.create({ data: { name, link } })
 export const deleteFormation = (name: string) => prismaClient.formation.delete({ where: { name } })
 
-export const getFormationVideos = () => prismaClient.formation.findMany()
+export const getFormationVideos = () => prismaClient.formation.findMany({ orderBy: { createdAt: 'asc' } })
