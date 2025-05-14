0d96171086d3036af9f747a0b6e4c2455bc8fce2
lfo <louis@dewolf.fr>
2025-05-14 11:11:32 +0200

    create emission factors permissions unit tests

---
+++ src/services/permissions/emissionFactor.test.ts
@@ -0,0 +1,53 @@
+import { getMockedDbUser } from '@/tests/utils/models/user'
+import { expect } from '@jest/globals'
+import { EmissionFactor, Import, User } from '@prisma/client'
+import * as emissionFactorModule from '../serverFunctions/emissionFactor'
+import { canEditEmissionFactor, canReadEmissionFactor } from './emissionFactor'
+
+jest.mock('../serverFunctions/emissionFactor', () => ({ isFromEmissionFactorOrganization: jest.fn() }))
+const mockIsFromEmissionFactorOrganization = emissionFactorModule.isFromEmissionFactorOrganization as jest.Mock
+
+describe('Study permissions service', () => {
+  describe('canReadEmissionFactor', () => {
+    it('returns true if emission factor is not manually imported', () => {
+      const user = getMockedDbUser({}) as User
+      const emissionFactor = { importedFrom: Import.BaseEmpreinte } as EmissionFactor
+
+      expect(canReadEmissionFactor(user, emissionFactor)).toBe(true)
+    })
+
+    it('returns true if manually imported and user belongs to same organization', () => {
+      const user = getMockedDbUser({ organizationId: 'mocked-organization-id' }) as User
+      const emissionFactor = {
+        importedFrom: Import.Manual,
+        organizationId: 'mocked-organization-id',
+      } as EmissionFactor
+
+      expect(canReadEmissionFactor(user, emissionFactor)).toBe(true)
+    })
+
+    it('returns false if manually imported and user belongs to different organization', () => {
+      const user = getMockedDbUser({ organizationId: 'mocked-organization-id' }) as User
+      const emissionFactor = {
+        importedFrom: Import.Manual,
+        organizationId: 'mocked-other-organization-id',
+      } as EmissionFactor
+
+      expect(canReadEmissionFactor(user, emissionFactor)).toBe(false)
+    })
+  })
+
+  describe('canEditEmissionFactor', () => {
+    it('User from same organization should be able to edit emission factor', async () => {
+      mockIsFromEmissionFactorOrganization.mockResolvedValue(true)
+      const result = await canEditEmissionFactor('mocked-id')
+      expect(result).toBe(true)
+    })
+
+    it('User from other organization should not be able to edit emission factor', async () => {
+      mockIsFromEmissionFactorOrganization.mockResolvedValue(false)
+      const result = await canEditEmissionFactor('mocked-id')
+      expect(result).toBe(false)
+    })
+  })
+})
