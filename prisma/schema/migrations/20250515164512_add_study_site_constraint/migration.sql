1a227bec4e72ba99e07565df6818f818ce138527
lfo <louis@dewolf.fr>
2025-05-20 17:29:12 +0200

    add drop trigger and function commands in migration script

--- prisma/schema/migrations/20250515164512_add_study_site_constraint/migration.sql
+++ prisma/schema/migrations/20250515164512_add_study_site_constraint/migration.sql
@@ -1,3 +1,6 @@
+DROP TRIGGER  IF EXISTS trg_validate_study_site ON study_emission_sources;
+DROP FUNCTION IF EXISTS validate_study_site_id();
+
 CREATE OR REPLACE FUNCTION validate_study_site_id()
 RETURNS TRIGGER AS $$
 BEGIN
