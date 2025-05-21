092a1c9076ae618fb118e9a306ae605e2789a4a8
lfo <louis@dewolf.fr>
2025-05-21 13:48:29 +0200

    apply withServerResponse to scaleway server functions

--- src/services/serverFunctions/study.ts
+++ src/services/serverFunctions/study.ts
@@ -655,14 +655,16 @@ export const addFlowToStudy = async (studyId: string, file: File) => {
     return NOT_AUTHORIZED
   }
   const butcketUploadResult = await uploadFileToBucket(file)
-  await createDocument({
-    name: file.name,
-    type: file.type,
-    uploader: { connect: { id: session?.user.accountId } },
-    study: { connect: { id: studyId } },
-    bucketKey: butcketUploadResult.key,
-    bucketETag: butcketUploadResult.ETag || '',
-  })
+  if (butcketUploadResult.success) {
+    await createDocument({
+      name: file.name,
+      type: file.type,
+      uploader: { connect: { id: session?.user.accountId } },
+      study: { connect: { id: studyId } },
+      bucketKey: butcketUploadResult.data.key,
+      bucketETag: butcketUploadResult.data.ETag || '',
+    })
+  }
 }
 
 export const deleteFlowFromStudy = async (document: Document, studyId: string) => {
@@ -670,7 +672,7 @@ export const deleteFlowFromStudy = async (document: Document, studyId: string) =
     return NOT_AUTHORIZED
   }
   const bucketDelete = await deleteFileFromBucket(document.bucketKey)
-  if (bucketDelete) {
+  if (bucketDelete.success) {
     await deleteDocument(document.id)
   }
 }
