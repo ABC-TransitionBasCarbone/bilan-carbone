1f41b612c2ef8b9e83177ff0453b5080cde8ea5f
Lilian AUFFRET <lilian@auffret.dev>
2025-05-27 15:57:48 +0200

    feat: theme compliant breadcrumps

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,24 +1,40 @@
+'use client'
+import { BASE, CUT, useAppEnvironmentStore } from '@/store/AppEnvironment';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
-import classNames from 'classnames'
-import Link from '../base/Link'
-import styles from './Breadcrumbs.module.css'
+import { Box, BoxProps, Link, Breadcrumbs as MUIBreadcrumbs, styled, Typography } from '@mui/material'
+import { useMemo } from 'react';
 
-const Breadcrumbs = ({ links, current }: { links: { label: string; link: string }[]; current: string }) => {
+interface Props {
+  links: { label: string; link: string }[]
+  current: string
+}
+
+const StyledContainer = styled(Box)<BoxProps>(({ theme }) => {
+  const { environment } = useAppEnvironmentStore()
+  return {
+    backgroundColor: theme.palette.primary.light,
+    borderRadius: '0 0 1rem 1rem',
+    borderColor: environment === BASE ? theme.palette.divider : theme.palette.primary.dark,
+    borderStyle: 'solid',
+    borderWidth: '0.125rem',
+    gap: '0.5rem',
+    padding: '0.5rem 0.75rem',
+  }
+})
+
+const Breadcrumbs = ({ links, current }: Props) => {
+  const { environment } = useAppEnvironmentStore()
+  const isCut = useMemo(() => environment === CUT, [environment])
   return (
     <nav role="navigation" aria-label="Breadcrumb" className="main-container">
-      <ol className={classNames(styles.container, 'flex-cc')}>
-        {links.map(({ label, link }) => (
-          <li key={label} className={classNames(styles.previousPage, 'flex-cc')}>
-            <Link className={styles.link} href={link}>
-              {label}
-            </Link>
-            <KeyboardArrowRightIcon className={styles.svg} />
-          </li>
-        ))}
-        <li className={styles.currentPage} aria-current="page">
-          {current}
-        </li>
-      </ol>
+      <StyledContainer className="flex-cc">
+        <MUIBreadcrumbs separator={<KeyboardArrowRightIcon />}>
+          {links.map(({ link, label }) => (
+            <Link href={link} color={isCut ? 'primary.contrastText' : 'primary'}>{label}</Link>
+          ))}
+          <Typography>{current}</Typography>
+        </MUIBreadcrumbs>
+      </StyledContainer>
     </nav>
   )
 }
