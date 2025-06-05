30fe0ad538d9bbb27713947cd5b28517dac72699
Chloe M <chloe.molle@neuf.fr>
2025-06-05 16:15:58 +0200

    fix switch

--- prisma/seed/utils.ts
+++ prisma/seed/utils.ts
@@ -6,8 +6,6 @@ export const getCutRoleFromBase = (role: Role): Role => {
     case Role.GESTIONNAIRE:
     case Role.SUPER_ADMIN:
       return Role.ADMIN
-    case Role.COLLABORATOR:
-    case Role.DEFAULT:
     default:
       return Role.DEFAULT
   }
