4027d12371102f86297b6c416e239816d7b6952f
jilink <jimmyfac1@gmail.com>
2025-06-10 16:36:02 +0200

    fix: better chargning between env translations

--- src/environments/core/EnvironmentInitializer.tsx
+++ src/environments/core/EnvironmentInitializer.tsx
@@ -2,11 +2,13 @@
 
 import { switchEnvironment } from '@/i18n/environment'
 import { useAppEnvironmentStore } from '@/store/AppEnvironment'
+import { CircularProgress } from '@mui/material'
 import { UserSession } from 'next-auth'
 import { useEffect } from 'react'
+import styles from './EnvironmentInitializer.module.css'
 
 const EnvironmentInitializer = ({ user }: { user: UserSession }) => {
-  const { setEnvironment } = useAppEnvironmentStore()
+  const { setEnvironment, setIsLoading, isLoading } = useAppEnvironmentStore()
 
   useEffect(() => {
     if (!user || !user.environment) {
@@ -14,9 +16,18 @@ const EnvironmentInitializer = ({ user }: { user: UserSession }) => {
     }
     setEnvironment(user.environment)
     switchEnvironment(user.environment)
+    setIsLoading(false)
   }, [user?.environment])
 
-  return <></>
+  if (isLoading) {
+    return (
+      <div className={styles.loading}>
+        <CircularProgress variant="indeterminate" color="primary" size={200} />
+      </div>
+    )
+  }
+
+  return null
 }
 
 export default EnvironmentInitializer
