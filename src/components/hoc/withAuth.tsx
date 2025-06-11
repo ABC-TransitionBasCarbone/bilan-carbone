4e841b69295d7a2567f8e65630655087181eb6c4
Chloe M <chloe.molle@neuf.fr>
2025-06-11 11:56:43 +0200

    fix mutiple accounts

--- src/components/hoc/withAuth.tsx
+++ src/components/hoc/withAuth.tsx
@@ -1,6 +1,7 @@
 import NotFound from '@/components/pages/NotFound'
 import { auth } from '@/services/auth'
 import { UserSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 export type UserSessionProps = {
@@ -12,8 +13,12 @@ const withAuth = (WrappedComponent: React.ComponentType<any & UserSessionProps>)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Component = async (props: any) => {
     const session = await auth()
-    if (!session || !session.user || session.user.needsAccountSelection) {
+    if (!session || !session.user) {
       return <NotFound />
+    }
+
+    if (session.user.needsAccountSelection) {
+      redirect('/selection-du-compte')
     }
 
     return <WrappedComponent {...props} user={session.user} />
