3c6e3fb2b53c88925b54b603f61735ac0747a307
lfo <louis@dewolf.fr>
2025-06-16 17:55:18 +0200

    create study to delete into seed for e2e test

--- src/tests/end-to-end/app/delete-study.cy.ts
+++ src/tests/end-to-end/app/delete-study.cy.ts
@@ -1,5 +1,3 @@
-import dayjs from 'dayjs'
-
 describe('Delete study', () => {
   before(() => {
     cy.exec('npx prisma db seed')
@@ -10,44 +8,22 @@ describe('Delete study', () => {
     cy.intercept('POST', '/etudes/*').as('delete')
   })
 
-  /**
-   * TODO
-   *
-   * Enlever la parti création d’une étude et la mocker
-   */
   it('should be able to delete a study', () => {
     cy.login()
 
-    cy.getByTestId('new-study').click()
-
-    cy.getByTestId('organization-sites-checkbox').first().scrollIntoView()
-    cy.getByTestId('organization-sites-checkbox')
-      .first()
+    cy.getByTestId('study')
+      .contains('Study to delete')
+      .scrollIntoView()
+      .parents('[data-testid="study"]')
       .within(() => {
-        cy.get('input').click({ force: true })
+        cy.getByTestId('study-link').click()
       })
-
-    cy.getByTestId('new-study-organization-button').scrollIntoView()
-    cy.getByTestId('new-study-organization-button').click({ force: true })
-
-    cy.getByTestId('new-study-name').type('My study to delete')
-    cy.getByTestId('new-validator-name').type('bc-collaborator-0@yopmail.com')
-    cy.get('[data-option-index="0"]').click()
-
-    cy.getByTestId('new-study-endDate').within(() => {
-      cy.get('span').first().type(dayjs().add(1, 'y').format('DD/MM/YYYY'))
-    })
-    cy.getByTestId('new-study-level').click()
-    cy.get('[data-value="Initial"]').click()
-    cy.getByTestId('new-study-create-button').click()
-
-    cy.wait('@create')
 
     cy.getByTestId('delete-study').click()
     cy.get('#delete-study-modale-title').should('be.visible')
     cy.get('#delete-study-modale-content').should('be.visible')
 
-    cy.getByTestId('delete-study-name-field').type('my study to delet')
+    cy.getByTestId('delete-study-name-field').type('Study to delet')
     cy.getByTestId('alert-toaster').should('not.exist')
     cy.getByTestId('confirm-study-deletion').click()
     cy.getByTestId('alert-toaster').should('be.visible')
