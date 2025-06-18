a7b3adb345726bd196a6c020e860193c87c335ed
Alexandre Phiev <alexandre.phiev@gmail.com>
2025-06-18 18:40:46 +0200

    fix: heavy styles in separate file

--- src/environments/cut/study/infography/PostHeader.tsx
+++ src/environments/cut/study/infography/PostHeader.tsx
@@ -1,9 +1,15 @@
 import PostIcon from '@/components/study/infography/icons/PostIcon'
 import { Post } from '@/services/posts'
-import { styled } from '@mui/material'
 import { SubPost } from '@prisma/client'
 import { useTranslations } from 'next-intl'
 import { CutProgressBar } from './CutProgressBar'
+import {
+  StyledContentColumn,
+  StyledEmissionValue,
+  StyledIconColumn,
+  StyledPostHeader,
+  StyledTitle,
+} from './PostHeader.styles'
 
 interface Props {
   post: Post | SubPost
@@ -11,53 +17,6 @@ interface Props {
   emissionValue: string
   percent: number
 }
-
-const StyledPostHeader = styled('div', { shouldForwardProp: (prop) => prop !== 'post' })<{ post: Post }>(
-  ({ theme, post }) => ({
-    position: 'relative',
-    display: 'grid',
-    gridTemplateColumns: '1fr 4fr',
-    color: 'black',
-    backgroundColor: theme.custom.postColors[post].light,
-    height: '7.75rem',
-    overflow: 'hidden',
-    borderRadius: '0.5rem',
-  }),
-)
-
-const StyledIconColumn = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: '1rem',
-  borderRight: `2px solid ${theme.palette.primary.light}`,
-}))
-
-const StyledContentColumn = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'flex-start',
-  gap: '0.25rem',
-  padding: '0rem 1rem',
-  position: 'relative',
-  height: '100%',
-  width: '100%',
-})
-
-const StyledTitle = styled('div')(({ theme }) => ({
-  color: theme.palette.primary.contrastText,
-  fontSize: '1.125rem',
-  fontWeight: 700,
-  textAlign: 'left',
-}))
-
-const StyledEmissionValue = styled('div')({
-  fontSize: '1.25rem',
-  fontWeight: 800,
-  textAlign: 'left',
-  color: 'white',
-})
 
 export const CutPostHeader = ({ post, mainPost, emissionValue, percent }: Props) => {
   const t = useTranslations('emissionFactors.post')
