7454a3abc087e64138d40a1409aeb25e99b43f71
Lilian AUFFRET <contact@auffret.dev>
2025-06-23 15:50:10 +0200

    fix: removed studies in cut home

---
+++ src/tests/end-to-end/app/home-cut.cy.ts
@@ -0,0 +1,12 @@
+describe('Home page - CUT environment', () => {
+  beforeEach(() => {
+    cy.login('cut-env-admin-0@yopmail.com', 'password-0').wait(4000)
+  })
+
+  it('should display the main title on the home page', () => {
+    cy.get('h4')
+      .should('have.length', 1)
+      .first()
+      .should('contain.text', 'Faire votre bilan d’impact vous permettra de :')
+  })
+})
