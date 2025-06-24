8fbd0d402452a243323336bb82268c67605f7f24
Alexandre Phiev <alexandre.phiev@gmail.com>
2025-06-24 09:25:07 +0200

    fix: rebase issues

--- src/environments/cut/study/new/Form.tsx
+++ src/environments/cut/study/new/Form.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import Block from '@/components/base/Block'
-import { FormTextField } from '@/components/form/TextField'
-import WeekScheduleForm from '@/components/form/WeekScheduleForm'
 import GlobalNewStudyForm from '@/components/study/new/Form'
 import { CreateStudyCommand } from '@/services/serverFunctions/study.command'
 import { Export, Level } from '@prisma/client'
@@ -29,45 +27,7 @@ const NewStudyForm = ({ form, duplicateStudyId }: Props) => {
 
   return (
     <Block title={t('title')} as="h1">
-      <GlobalNewStudyForm form={form} t={t} duplicateStudyId={duplicateStudyId}>
-        <WeekScheduleForm
-          label={t('openingHours')}
-          days={days}
-          name={'openingHours'}
-          control={form.control}
-          onCheckDay={handleCheckDay}
-        />
-        <WeekScheduleForm
-          label={t('openingHoursHoliday')}
-          days={daysHoliday}
-          name={'openingHoursHoliday'}
-          control={form.control}
-        />
-        <FormTextField
-          control={form.control}
-          name="numberOfSessions"
-          data-testid="new-study-number-of-sessions"
-          label={t('numberOfSessions')}
-          translation={t}
-          type="number"
-        />
-        <FormTextField
-          control={form.control}
-          name="numberOfTickets"
-          data-testid="new-study-number-of-tickets"
-          label={t('numberOfTickets')}
-          translation={t}
-          type="number"
-        />
-        <FormTextField
-          control={form.control}
-          name="numberOfOpenDays"
-          data-testid="new-study-number-of-open-days"
-          label={t('numberOfOpenDays')}
-          translation={t}
-          type="number"
-        />
-      </GlobalNewStudyForm>
+      <GlobalNewStudyForm form={form} t={t} duplicateStudyId={duplicateStudyId} />
     </Block>
   )
 }
