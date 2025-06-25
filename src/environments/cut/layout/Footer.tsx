d0397c5a4bba3dd8f53192b8d7662ea47065fbc5
jilink <jimmyfac1@gmail.com>
2025-06-25 16:59:49 +0200

    fix: typo footer + moving text to translation file  + removing horizontal scrollbar

--- src/environments/cut/layout/Footer.tsx
+++ src/environments/cut/layout/Footer.tsx
@@ -2,11 +2,13 @@
 import { Box, Grid, Typography, useTheme } from '@mui/material'
 import { common } from '@mui/material/colors'
 import classNames from 'classnames'
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import styles from './Footer.module.css'
 
 const Footer = () => {
   const { palette } = useTheme()
+  const t = useTranslations('footer.message')
   const size = { xs: 12, sm: 6, md: 6, lg: 3 }
   return (
     <Grid
@@ -21,7 +23,7 @@ const Footer = () => {
       <Grid className="flex justify-center align-center" size={size}>
         <Box className="flex-col" gap={2}>
           <Typography className={styles.text} color={common.white}>
-            Cet outil a été développé par l'association
+            {t('0')}
           </Typography>
           <Box display="flex">
             <Image width={86.23} height={55} src="/logos/cut/CUT.svg" alt="Cut Logo" />
@@ -32,9 +34,7 @@ const Footer = () => {
       <Grid className="flex justify-center align-center" size={size}>
         <Box component="article" className="flex-col" gap={2}>
           <Typography className={styles.text} color={common.white}>
-            En coopération avec l'ABC,
-            <br />
-            association pour la transition bas carbone
+            {t.rich('1', { br: () => <br /> })}
           </Typography>
           <Image width={154} height={55} src="/logos/cut/ABC.svg" alt="ABC Logo" />
         </Box>
@@ -43,17 +43,14 @@ const Footer = () => {
         <Box component="article" className="flex align-center" gap={2}>
           <Image width={92} height={90} src="/logos/cut/France3_2025_blanc.png" alt="Logo de France 3" />
           <Typography className={styles.text} color={common.white}>
-            Opération soutenue par l’État dans le cadre du <br />
-            dispositif « Soutenir les alternatives vertes 2 » <br />
-            de France 2030, opéré par la Banque des <br />
-            territoires (Caisse des Dépôts)
+            {t.rich('2', { br: () => <br /> })}
           </Typography>
         </Box>
       </Grid>
       <Grid className="flex justify-center align-center" size={size}>
         <Box component="article" className="flex-col" gap={2}>
           <Typography className={styles.text} color={common.white}>
-            CUT! bénéficie du soutien du CNC
+            {t('3')}
           </Typography>
           <Image width={172} height={37} src="/logos/cut/CNC.svg" alt="CNC Logo" />
         </Box>
