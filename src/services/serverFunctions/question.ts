071e891b54082780ed03f3bb4504111ddd49b75d
Lilian AUFFRET <contact@auffret.dev>
2025-06-27 16:18:04 +0200

    feat: add list map

--- src/services/serverFunctions/question.ts
+++ src/services/serverFunctions/question.ts
@@ -109,14 +109,14 @@ const emissionFactorMap: Record<string, EmissionFactorInfo> = {
   '11-quand-le-batiment-a-t-il-ete-construit': {},
   '12-a-quand-remonte-la-derniere-renovation-importante': {},
   'de-quel-type-de-renovation-sagi-t-il': {},
-  'dans-le-cas-dun-agrandissement-quelle-est-la-surface-supplementaire-ajoutee': {},
+  'dans-le-cas-dun-agrandissement-quelle-est-la-surface-supplementaire-ajoutee': { emissionFactorImportedId: '20730' },
   'le-batiment-est-il-partage-avec-une-autre-activite': {},
   'quelle-est-la-surface-totale-du-batiment': {},
   'le-cinema-dispose-t-il-dun-parking': {},
-  'si-oui-de-combien-de-places': {},
+  'si-oui-de-combien-de-places': { emissionFactorImportedId: '26008' },
   // Equipe
   '10-quel-est-le-rythme-de-travail-des-collaborateurs-du-cinema': {},
-  '11-quel-est-le-rythme-de-travail-des-collaborateurs-du-cinema': {},
+  '11-quel-est-le-rythme-de-travail-des-collaborateurs-du-cinema': { emissionFactorImportedId: '20682' },
   '12-quel-est-le-rythme-de-travail-des-collaborateurs-du-cinema': {},
   '13-quel-est-le-rythme-de-travail-des-collaborateurs-du-cinema': {},
   // DeplacementsProfessionnels
@@ -132,14 +132,15 @@ const emissionFactorMap: Record<string, EmissionFactorInfo> = {
   'quelles-etaient-les-consommations-energetiques-du-cinema': { emissionFactorImportedId: '15591' },
   gaz: { emissionFactorImportedId: '37138' },
   fuel: { emissionFactorImportedId: '14086' },
+  'reseaux-urbains-chaleurfroid': { emissionFactorImportedId: '' },
   'bois-granules': { emissionFactorImportedId: '34942' },
+  'votre-cinema-est-il-equipe-de-la-climatisation': { emissionFactorImportedId: '' },
   'le-cinema-dispose-t-il-d-un-ou-plusieurs-groupes-electrogenes': { emissionFactorImportedId: '20911' },
   'quelle-est-votre-consommation-annuelle-de-diesel': { emissionFactorImportedId: '14015' },
-  'reseaux-urbains-chaleurfroid': {},
-  'votre-cinema-est-il-equipe-de-la-climatisation': {},
   // ActivitesDeBureau
   'quel-montant-avez-vous-depense-en-petites-fournitures-de-bureau': { emissionFactorImportedId: '20556' },
   'quel-montant-avez-vous-depense-en-services': { emissionFactorImportedId: '43545' },
+  'pour-chacun-de-ces-equipements-informatiques-veuillez-indiquer': {},
   'ordinateurs-fixes-nombre-unite': { emissionFactorImportedId: '27003' },
   'ordinateurs-fixes-annee-ou-nombre-jours': {},
   'ordinateurs-portables-nombre-unite': { emissionFactorImportedId: '27002' },
@@ -154,29 +155,92 @@ const emissionFactorMap: Record<string, EmissionFactorInfo> = {
   'telephones-portables-annee-ou-nombre-jours': {},
   'tablettes-nombre-unite': { emissionFactorImportedId: '27007' },
   'tablettes-annee-ou-nombre-jours': {},
+  // Mobilité spectateurs
+  'avez-vous-deja-realise-une-enquete-mobilite-specteurs': {},
+  /** TODO: Liste 1 */
+  'si-a-quelles-sont-les-distances-parcourues-au-total-sur-lannee-pour-chacun-des-modes-de-transport-suivants': {},
+  /***** */
+  'si-b-vous-pouvez-ici-telecharger-un-modele-denquete-qui-vous-permettra-de-remplir-dici-quelques-semaines-les-informations-demandees':
+    {},
+  'si-c-de-quel-type-de-cinema-votre-etablissement-se-rapproche-le-plus': {},
+  'vos-spectateurs-sont-ils-majoritairement-des-habitants-locaux-cest-a-dire-residant-a-lannee-dans-les-environs-ou-attirez-vous-aussi-une-part-non-negligeable-de-spectateurs-de-passage-dans-la-region-touristes-notamment':
+    {},
+  next: {},
+  'quel-est-le-profil-auquel-vous-pouvez-identifier-le-plus-votre-cinema': {},
+  // Equipes recus
+  'combien-d-equipes-de-film-avez-vous-recu-en-*': {},
+  'combien-de-nuits': { emissionFactorImportedId: '17,11' },
+  'combien-d-equipes-de-repas': { emissionFactorImportedId: '20682' },
+  // Autres matériel et matériel technique
+  '10-decrivez-les-differentes-salles-du-cinema': {},
+  '11-decrivez-les-differentes-salles-du-cinema': {},
+  '12-decrivez-les-differentes-salles-du-cinema': {},
+  '13-decrivez-les-differentes-salles-du-cinema': {},
+  '14-decrivez-les-differentes-salles-du-cinema': {},
+  '15-decrivez-les-differentes-salles-du-cinema': {},
+  '16-decrivez-les-differentes-salles-du-cinema': {},
+  '10-comment-stockez-vous-les-films': {},
+  '11-comment-stockez-vous-les-films': { emissionFactorImportedId: '20894' },
+  '12-comment-stockez-vous-les-films': { emissionFactorImportedId: '20893' },
+  // TODO: Calcul ?
+  'combien-de-films-recevez-vous-en-dématérialise-par-an': {},
+  'combiende-films-recevez-vous-sur-dcp-physique-par-an': {},
+  'combien-de-donnees-stockez-vous-dans-un-cloud': {},
+  '10-de-combien-disposez-vous-de': {},
+  '11-de-combien-disposez-vous-de': { emissionFactorImportedId: '1,31' },
+  '12-de-combien-disposez-vous-de': { emissionFactorImportedId: '200' },
+  // Achats
+  '10-vendez-vous-des-boissons-et-des-confiseries': {},
+  '11-vendez-vous-des-boissons-et-des-confiseries': {},
+  // Fret
+  'quelle-est-la-distance-entre-votre-cinema-et-votre-principal-fournisseur': { emissionFactorImportedId: '28026' },
   // Electromenager
-  refrigerateurs: { emissionFactorImportedId: '26976' },
-  congelateurs: { emissionFactorImportedId: '26978' },
-  warmers: { emissionFactorImportedId: '26986' },
-  'distributeurs-snacks-boisson': { emissionFactorImportedId: '26976' },
+  '11-pour-chacun-de-ces-equipements-electromenagers-veuillez-renseigner': { emissionFactorImportedId: '26976' },
+  '12-pour-chacun-de-ces-equipements-electromenagers-veuillez-renseigner': {},
+  '13-pour-chacun-de-ces-equipements-electromenagers-veuillez-renseigner': { emissionFactorImportedId: '26978' },
+  '14-pour-chacun-de-ces-equipements-electromenagers-veuillez-renseigner': {},
+  '15-pour-chacun-de-ces-equipements-electromenagers-veuillez-renseigner': { emissionFactorImportedId: '26986' },
+  '17-pour-chacun-de-ces-equipements-electromenagers-veuillez-renseigner': { emissionFactorImportedId: '26976' },
+  '18-pour-chacun-de-ces-equipements-electromenagers-veuillez-renseigner': {},
   // DechetsOrdinaires
-  'frequence-de-ramassage-par-semaine': { emissionFactorImportedId: '34654' },
-  'frequence-de-ramassage-par-semaine-emballage': { emissionFactorImportedId: '34486' },
-  'frequence-de-ramassage-par-semaine-biodechets': { emissionFactorImportedId: '22040' },
+  '110-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '111-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '112-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '113-veuillez-renseigner-les-dechets-generes-par-semaine': { emissionFactorImportedId: '34654' },
+  '120-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '121-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '122-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '123-veuillez-renseigner-les-dechets-generes-par-semaine': { emissionFactorImportedId: '34486' },
+  '130-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '131-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '132-veuillez-renseigner-les-dechets-generes-par-semaine': {},
+  '133-veuillez-renseigner-les-dechets-generes-par-semaine': { emissionFactorImportedId: '22040' },
   // DechetsExceptionnels
   'quelle-quantite-de-materiel-technique-jetez-vous-par-an': { emissionFactorImportedId: '34620' },
   'quelle-quantite-de-lampes-xenon-jetez-vous-par-an': { emissionFactorImportedId: '50' },
   // MaterielDistributeurs
-  'affiches-120x160': { emissionFactorImportedId: '0,31218' },
-  'affiches-40x60': { emissionFactorImportedId: '0,038313' },
-  'plv-comptoir': { emissionFactorImportedId: '0,195' },
-  'plv-grand-format': { emissionFactorImportedId: '1,365' },
-  goodies: { emissionFactorImportedId: '0,231' },
-  'autres-ex-dossiers-pedagogiques-etc': { emissionFactorImportedId: '0,007095' },
+  '11-quelle-quantite-de-materiel-distributeurs-recevez-vous-en-moyenne-par-semaine': {
+    emissionFactorImportedId: '0,31218',
+  },
+  '12-quelle-quantite-de-materiel-distributeurs-recevez-vous-en-moyenne-par-semaine': {
+    emissionFactorImportedId: '0,038313',
+  },
+  '13-quelle-quantite-de-materiel-distributeurs-recevez-vous-en-moyenne-par-semaine': {
+    emissionFactorImportedId: '0,195',
+  },
+  '14-quelle-quantite-de-materiel-distributeurs-recevez-vous-en-moyenne-par-semaine': {
+    emissionFactorImportedId: '1,365',
+  },
+  '15-quelle-quantite-de-materiel-distributeurs-recevez-vous-en-moyenne-par-semaine': {
+    emissionFactorImportedId: '0,231',
+  },
+  '16-quelle-quantite-de-materiel-distributeurs-recevez-vous-en-moyenne-par-semaine': {
+    emissionFactorImportedId: '0,007095',
+  },
   // MaterielCinema
-  programme: { emissionFactorImportedId: '0,007095' },
-  affiches: { emissionFactorImportedId: '0,038313' },
-  flyers: { emissionFactorImportedId: '0,0059598' },
+  '11-quelle-quantite-de-materiel-produisez-vous-chaque-mois': { emissionFactorImportedId: '0,007095' },
+  '12-quelle-quantite-de-materiel-produisez-vous-chaque-mois': { emissionFactorImportedId: '0,038313' },
+  '13-quelle-quantite-de-materiel-produisez-vous-chaque-mois': { emissionFactorImportedId: '0,0059598' },
   // CommunicationDigitale
   'combien-de-newsletters-ont-ete-envoyees': { emissionFactorImportedId: '0,00471' },
   'combien-de-caissons-d-affichage-dynamique-sont-presents-dans-le-cinema': { emissionFactorImportedId: '892,25' },
@@ -190,8 +254,4 @@ const emissionFactorMap: Record<string, EmissionFactorInfo> = {
   'baies-de-disques': { emissionFactorImportedId: '20893' },
   // CommunicationDigitale (again)
   'ecrans-tv': { emissionFactorImportedId: '27006' },
-  // AutreMateriel
-  'lunettes-3d': { emissionFactorImportedId: '1,31' },
-  'disques-durs': { emissionFactorImportedId: '200' },
-  // Add the rest as needed...
 }
