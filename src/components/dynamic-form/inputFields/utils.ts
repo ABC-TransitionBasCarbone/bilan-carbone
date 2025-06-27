d2483d15b85026292d3e96ae16d4f9857f4deccd
jilink <jimmyfac1@gmail.com>
2025-06-27 14:47:39 +0200

    fix: fixing option regex

--- src/components/dynamic-form/inputFields/utils.ts
+++ src/components/dynamic-form/inputFields/utils.ts
@@ -1,5 +1,1 @@
-{
-  /* TODO Temporary : Find a way to do it better */
-}
-
-export const formatOption = (option: string) => option.replace(/^\d{2}-/, '')
+export const formatOption = (option: string) => option.replace(/^\d*-/, '')
