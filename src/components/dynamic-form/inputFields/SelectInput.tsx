e8320c50d11f0082a02bd4ac41400eec6babd88a
Alexandre Phiev <alexandre.phiev@gmail.com>
2025-07-08 16:18:55 +0200

    feat: enhance SelectInput component with disabled state rendering and add orderBy option in question retrieval

--- src/components/dynamic-form/inputFields/SelectInput.tsx
+++ src/components/dynamic-form/inputFields/SelectInput.tsx
@@ -1,6 +1,12 @@
-import { FormControl, FormHelperText, MenuItem, Select, SelectChangeEvent } from '@mui/material'
+import { FormControl, FormHelperText, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles'
 import { BaseInputProps } from '../types/formTypes'
 import { formatOption } from './utils'
+
+const DisabledText = styled(Typography)({
+  padding: '0.5rem 0.75rem',
+  minHeight: '1.25rem',
+})
 
 interface SelectInputProps extends Omit<BaseInputProps, 'value' | 'onChange'> {
   value: string | null
@@ -12,6 +18,15 @@ const SelectInput = ({ question, value, onChange, onBlur, errorMessage, disabled
 
   const handleChange = (event: SelectChangeEvent<string>) => {
     onChange(event.target.value)
+  }
+
+  if (disabled && value) {
+    return (
+      <FormControl fullWidth error={!!errorMessage}>
+        <DisabledText variant="body1">{formatOption(value)}</DisabledText>
+        {errorMessage && <FormHelperText>{errorMessage}</FormHelperText>}
+      </FormControl>
+    )
   }
 
   return (
