c828de674ef2f2a1b9f016cfe5c6f375104083e1
jilink <jimmyfac1@gmail.com>
2025-07-17 16:30:04 +0200

    feat-refactor: removing useless user function + blocking addUser / addAccount

--- src/db/user.ts
+++ src/db/user.ts
@@ -210,9 +210,6 @@ export const updateUser = (userId: string, data: Partial<Prisma.UserCreateInput>
     data,
   })
 
-export const createUsers = (users: Prisma.UserCreateManyInput[]) =>
-  prismaClient.user.createMany({ data: users, skipDuplicates: true })
-
 export const createUsersWithAccount = async (
   users: (Prisma.UserCreateManyInput & { account: Prisma.AccountCreateInput })[],
 ) => {
@@ -294,8 +291,20 @@ export const getUserFeedbackDate = async (accountId: string) =>
 export const updateUserFeedbackDate = async (accountId: string, feedbackDate: Date) =>
   prismaClient.account.update({ where: { id: accountId }, data: { feedbackDate } })
 
-export const addUser = async (newMember: Prisma.UserCreateInput & { role?: Exclude<Role, 'SUPER_ADMIN'> }) =>
-  prismaClient.user.create({
+export const addUser = async (newMember: Prisma.UserCreateInput & { role?: Exclude<Role, 'SUPER_ADMIN'> }) => {
+  const deactivatedFeaturesRestrictions = await getDeactivableFeatureRestrictions(DeactivatableFeature.Creation)
+  if (deactivatedFeaturesRestrictions?.active) {
+    const createAccount = Array.isArray(newMember?.accounts?.create) ? undefined : newMember?.accounts?.create
+
+    const notAllowedEnvironments =
+      createAccount?.environment === undefined ||
+      deactivatedFeaturesRestrictions.deactivatedEnvironments.includes(createAccount.environment)
+
+    if (notAllowedEnvironments) {
+      throw new Error(NOT_AUTHORIZED)
+    }
+  }
+  return prismaClient.user.create({
     data: newMember,
     select: {
       id: true,
@@ -307,6 +316,7 @@ export const addUser = async (newMember: Prisma.UserCreateInput & { role?: Exclu
       },
     },
   })
+}
 
 export const handleAddingUser = async (creator: UserSession, newUser: AddMemberCommand) => {
   const environment = creator.environment
