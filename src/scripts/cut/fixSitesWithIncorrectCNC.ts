a2778a31a41cd6d4bcb84cc75136858b51d89cd3
Chloe M <chloe.molle@neuf.fr>
2025-07-21 09:12:19 +0200

    fix incoherence in script

--- src/scripts/cut/fixSitesWithIncorrectCNC.ts
+++ src/scripts/cut/fixSitesWithIncorrectCNC.ts
@@ -4,7 +4,7 @@ import { prismaClient } from '../../db/client'
 
 const program = new Command()
 
-program.name('fix-cnc').description('Script pour réparer le problèmes des CNCs').version('1.0.0').parse(process.argv)
+program.name('fix-cnc').description('Script pour réparer le problème des CNCs').version('1.0.0').parse(process.argv)
 
 const fixCNCs = async () => {
   const orgaVersions = await prismaClient.organizationVersion.findMany({
@@ -17,13 +17,17 @@ const fixCNCs = async () => {
     where: { organizationId: { in: orgaVersions.map((v) => v.organizationId) } },
   })
 
-  const cncIds = await prismaClient.cnc.findMany({})
+  const cncList = await prismaClient.cnc.findMany({})
+
+  if (cncList.length === 0) {
+    throw Error('No CNCs found in the database. Please ensure that CNCs are created before running this script.')
+  }
 
   for (const site of sites) {
-    const cnc = cncIds.find((c) => c.id === site.cncId)
+    const cnc = cncList.find((c) => c.id === site.cncId)
     if (!site.cncId || !cnc) {
-      console.log(`CNC not found for site ${site.id}, putting random CNC`)
-      await prismaClient.site.update({ where: { id: site.id }, data: { cncId: cncIds[0].id } })
+      console.log(`CNC not found for site ${site.id}, putting first CNC`)
+      await prismaClient.site.update({ where: { id: site.id }, data: { cncId: cncList[0].id } })
     } else {
       console.log(`CNC with ID ${cnc.id} found for site ${site.id}, no action needed.`)
     }
