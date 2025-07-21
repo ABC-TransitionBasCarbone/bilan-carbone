a163e5606143cf77346c1d3e33080e3ac8a8d73e
jilink <jimmyfac1@gmail.com>
2025-07-21 15:34:53 +0200

    fix: trim siret to avoid false error

--- src/services/associationApi.ts
+++ src/services/associationApi.ts
@@ -3,16 +3,17 @@ import axios from 'axios'
 const apiUrl = 'https://siva-integ1.cegedim.cloud/apim/api-asso/api/structure'
 
 export const isValidAssociationSiret = async (siret: string) => {
-  if (!siret || siret.length !== 14) {
+  const trimmedSiret = siret.trim()
+  if (!trimmedSiret || trimmedSiret.length !== 14) {
     return false
   }
 
-  const result = await axios.get(`${apiUrl}/${siret}`)
+  const result = await axios.get(`${apiUrl}/${trimmedSiret}`)
 
   if (!result?.data?.identite?.id_siret_siege) {
     return false
   }
-  if (result.data.identite.id_siret_siege !== parseInt(siret)) {
+  if (result.data.identite.id_siret_siege !== parseInt(trimmedSiret)) {
     return false
   }
 
