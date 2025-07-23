c84d019df180ecf2b6ac7cd75fd7e44b2432b303
Lilian AUFFRET <contact@auffret.dev>
2025-07-23 09:52:39 +0200

    fix(Question): change emissionFactors

--- src/constants/emissionFactorMap.ts
+++ src/constants/emissionFactorMap.ts
@@ -157,10 +157,9 @@ export const emissionFactorMap: Record<string, EmissionFactorInfo> = {
   '15-decrivez-les-deplacements-professionnels-de-vos-collaborateurs': {},
   '16-decrivez-les-deplacements-professionnels-de-vos-collaborateurs': {
     emissionFactors: {
-      "tous types d'hôtel": '100',
+      Appartement: '100',
       'hôtel 1*': '101',
       'hôtel 2*': '102',
-      'hôtel 3*': '103',
       'hôtel 4*': '104',
       'hôtel 5*': '105',
       nuitée: '106',
