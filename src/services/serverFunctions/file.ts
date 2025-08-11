bebe0c88614a35b97fdcbd77e5542a9875760274
lfo <louis@dewolf.fr>
2025-08-11 14:53:59 +0200

    fix build

--- src/services/serverFunctions/file.ts
+++ src/services/serverFunctions/file.ts
@@ -23,5 +23,11 @@ export const prepareExcel = async (
   }[],
 ) => {
   const buffer = xlsx.build(data)
-  return buffer
+
+  const arrayBuffer = new ArrayBuffer(buffer.length)
+  const view = new Uint8Array(arrayBuffer)
+  for (let i = 0; i < buffer.length; ++i) {
+    view[i] = buffer[i]
+  }
+  return arrayBuffer
 }
