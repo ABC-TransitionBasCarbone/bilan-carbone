b0c95a3ea0717c7d2cc6d2a8babec6297bdc8000
lfo <louis@dewolf.fr>
2025-08-11 17:00:21 +0200

    apply major updates

--- src/components/formation/Video.tsx
+++ src/components/formation/Video.tsx
@@ -1,19 +1,23 @@
 'use client'
 
 import { Formation } from '@prisma/client'
+import classNames from 'classnames'
 import dynamic from 'next/dynamic'
 import Box from '../base/Box'
+import styles from './Formation.module.css'
 
 interface Props {
   formation: Formation
 }
 
-const ReactPlayer = dynamic(() => import('react-player/youtube'), { ssr: false })
+const ReactPlayer = dynamic(() => import('react-player'), { ssr: false })
 
 const Video = ({ formation }: Props) => (
   <Box>
-    <div className="justify-center mb-2">{formation.name}</div>
-    <ReactPlayer url={formation.link} style={{ maxWidth: '100%' }} controls />
+    <div className="justify-center mb-2">{formation.name}</div>{' '}
+    <div className={classNames(styles.videoContainer, 'w100')}>
+      <ReactPlayer src={formation.link} controls style={{ width: '100%', height: '100%' }} />
+    </div>
   </Box>
 )
 
