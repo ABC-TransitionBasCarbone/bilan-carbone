d24f58b77a51883e776e8cfe00364e8e4a2af308
Alexandre Phiev <alexandre.phiev@gmail.com>
2025-10-13 11:49:01 +0200

    feat: refacto page logic in HOC

--- src/app/(dashboard)/etudes/[id]/(organization)/trajectoires/page.tsx
+++ src/app/(dashboard)/etudes/[id]/(organization)/trajectoires/page.tsx
@@ -1,29 +1,11 @@
 import withAuth, { UserSessionProps } from '@/components/hoc/withAuth'
 import { StudyProps } from '@/components/hoc/withStudy'
 import withStudyDetails from '@/components/hoc/withStudyDetails'
+import withTransitionPlan, { TransitionPlanProps } from '@/components/hoc/withTransitionPlan'
 import TrajectoryReductionPage from '@/components/pages/TrajectoryReductionPage'
-import {
-  canEditTransitionPlan,
-  canViewTransitionPlan,
-  isFeatureTransitionPlanActive,
-} from '@/services/permissions/study'
-import { redirect } from 'next/navigation'
 
-const TrajectoryReduction = async ({ study, user }: StudyProps & UserSessionProps) => {
-  const isTransitionPlanActive = await isFeatureTransitionPlanActive(study.organizationVersion.environment)
-
-  if (!isTransitionPlanActive) {
-    redirect(`/etudes/${study.id}`)
-  }
-
-  const canView = await canViewTransitionPlan(user, study)
-  if (!canView) {
-    redirect(`/etudes/${study.id}`)
-  }
-
-  const canEdit = await canEditTransitionPlan(user, study)
-
+const TrajectoryReduction = async ({ study, canEdit }: StudyProps & UserSessionProps & TransitionPlanProps) => {
   return <TrajectoryReductionPage study={study} canEdit={canEdit} />
 }
 
-export default withAuth(withStudyDetails(TrajectoryReduction))
+export default withAuth(withStudyDetails(withTransitionPlan(TrajectoryReduction)))
