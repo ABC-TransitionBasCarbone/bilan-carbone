e2f39a56e29752df7ac9bbba2903424da2c20685
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2025-10-13 15:17:55 +0200

    quick fixes association

--- src/services/associationApi.ts
+++ src/services/associationApi.ts
@@ -1,14 +1,12 @@
 import axios from 'axios'
 
-const apiUrl = 'https://siva-integ1.cegedim.cloud/apim/api-asso/api/structure'
-
 export const isValidAssociationSiret = async (siret: string) => {
   const trimmedSiret = siret.trim()
   if (!trimmedSiret || trimmedSiret.length !== 14) {
     return false
   }
 
-  const result = await axios.get(`${apiUrl}/${trimmedSiret}`)
+  const result = await axios.get(`${process.env.ASSOCIATION_SERVICE_URL}/${trimmedSiret}`)
 
   if (!result?.data?.identite?.id_siret_siege) {
     return false
