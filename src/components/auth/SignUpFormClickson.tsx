4075f2201f7f014d0fca374c1c4923b28d81e0e7
jilink <jimmyfac1@gmail.com>
2025-11-25 14:45:39 +0100

    feat: front register page for clickson

---
+++ src/components/auth/SignUpFormClickson.tsx
@@ -0,0 +1,156 @@
+'use client'
+
+import { useServerFunction } from '@/hooks/useServerFunction'
+import { getEnvVar } from '@/lib/environment'
+import { getEnvRoute } from '@/services/email/utils'
+import { UNKNOWN_SCHOOL } from '@/services/permissions/check'
+import { getSchoolsFromPostalCode, School } from '@/services/schoolApi'
+import { signUpWithSchoolPostalCode } from '@/services/serverFunctions/user'
+import { SignUpClicksonCommand, SignUpClicksonCommandValidation } from '@/services/serverFunctions/user.command'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormControl } from '@mui/material'
+import { Environment } from '@prisma/client'
+import classNames from 'classnames'
+import { useTranslations } from 'next-intl'
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import Form from '../base/Form'
+import LoadingButton from '../base/LoadingButton'
+import { FormAutocomplete } from '../form/Autocomplete'
+import { FormTextField } from '../form/TextField'
+import authStyles from './Auth.module.css'
+
+const SignUpFormClickson = () => {
+  const contactMail = getEnvVar('SUPPORT_EMAIL', Environment.CLICKSON)
+  const faq = getEnvVar('FAQ_LINK', Environment.CLICKSON)
+
+  const t = useTranslations('signupClickson')
+  const tForm = useTranslations('login.form')
+  const [submitting, setSubmitting] = useState(false)
+  const [message, setMessage] = useState('')
+  const [success, setSuccess] = useState(false)
+  const [schools, setSchools] = useState<School[]>([])
+  const [schoolPostalCode, setSchoolPostalCode] = useState('')
+  const { callServerFunction } = useServerFunction()
+
+  const searchParams = useSearchParams()
+
+  const { control, getValues, setValue, handleSubmit } = useForm<SignUpClicksonCommand>({
+    resolver: zodResolver(SignUpClicksonCommandValidation),
+    mode: 'onBlur',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+      schoolName: '',
+    },
+  })
+
+  useEffect(() => {
+    const fetchSchools = async () => {
+      const response = await callServerFunction(async () => {
+        const data = await getSchoolsFromPostalCode(schoolPostalCode)
+        return { success: true, data }
+      })
+
+      if (response.success && response.data && response.data.length > 0) {
+        setSchools(response.data)
+      }
+    }
+
+    fetchSchools()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [schoolPostalCode])
+
+  useEffect(() => {
+    const email = searchParams.get('email')
+    if (email) {
+      setValue('email', email)
+    }
+  }, [searchParams, setValue])
+
+  const onSubmit = async () => {
+    setMessage('')
+    setSubmitting(true)
+
+    const school = schools.find((s) => s.nom_etablissement === getValues().schoolName)
+    if (!school) {
+      setSubmitting(false)
+      setSuccess(false)
+      setMessage(UNKNOWN_SCHOOL)
+      return
+    }
+
+    const activation = await signUpWithSchoolPostalCode(getValues().email, school, Environment.CLICKSON)
+    setSubmitting(false)
+
+    if (activation.success) {
+      setSuccess(true)
+      setMessage(activation.data)
+    } else {
+      setSuccess(false)
+      setMessage(activation.errorMessage)
+    }
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)} className="grow justify-center">
+      <FormControl className={authStyles.form}>
+        <FormTextField
+          control={control}
+          name="email"
+          className={authStyles.input}
+          label={t('email')}
+          placeholder={t('emailPlaceholder')}
+          data-testid="activation-email"
+        />
+        <FormAutocomplete
+          data-testid="activation-school"
+          control={control}
+          translation={t}
+          options={
+            schoolPostalCode.length < 5
+              ? []
+              : schools.map((school) => ({
+                  label: `${school.nom_etablissement} - ${school.adresse_1} (${school.code_postal})`,
+                  value: school.nom_etablissement,
+                }))
+          }
+          name="schoolName"
+          label={t('schoolPostalCode')}
+          helperText={t('schoolPostalCodePlaceholder')}
+          freeSolo
+          disableClearable
+          onInputChange={(_, value) => {
+            setSchoolPostalCode(value)
+            setValue('schoolName', value)
+          }}
+        />
+        <LoadingButton data-testid="activation-button" type="submit" loading={submitting} variant="contained" fullWidth>
+          {t('validate')}
+        </LoadingButton>
+        {message && (
+          <p className={classNames(!success ? 'error' : '')} data-testid="activation-form-message">
+            {t.rich(message, {
+              support: (children) => <Link href={`mailto:${contactMail}`}>{children}</Link>,
+              link: (children) => (
+                <Link href={faq} target="_blank" rel="noreferrer noopener">
+                  {children}
+                </Link>
+              ),
+            })}
+          </p>
+        )}
+        <div className={authStyles.bottomLink}>
+          {tForm('alreadyRegistered')}
+          <Link className="ml-2" href={getEnvRoute('login', Environment.CLICKSON)} prefetch={false}>
+            {tForm('login')}
+          </Link>
+        </div>
+      </FormControl>
+    </Form>
+  )
+}
+
+export default SignUpFormClickson
