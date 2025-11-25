ba71e7bca003f19286ccaf7e28e919f5abda08ea
Emile Rolley <emile.rolley@tuta.io>
2025-11-25 14:35:14 +0100

    refactor(publicodes): directly use publicodes FormBuilder with a singleton pattern like the engine

--- src/environments/cut/publicodes/cut-engine.ts
+++ src/environments/cut/publicodes/cut-engine.ts
@@ -1,7 +1,10 @@
-import { getOrCreateEngine } from '@/lib/publicodes/engine'
+import { FormLayout, TableFormLayout } from '@/components/publicodes-form/formLayout/formLayout'
+import { getOrCreateEngine, getOrCreateFormBuilder } from '@/lib/publicodes/singletons'
+import { isInNamespace } from '@/lib/publicodes/utils'
 import rules from '@abc-transitionbascarbone/publicodes-count'
+import { FormBuilder, FormPages, groupByNamespace } from '@publicodes/forms'
 import Engine from 'publicodes'
-import { CutPublicodesEngine } from './types'
+import { CutPublicodesEngine, CutRuleName } from './types'
 
 /**
  * Returns a singleton instance of the Publicodes {@link Engine} configured
@@ -16,4 +19,50 @@ export function getCutEngine(): CutPublicodesEngine {
       },
     })
   })
+}
+
+export function getCutFormBuilder(): FormBuilder<CutRuleName> {
+  const engine = getCutEngine()
+  const pageBuilder = cutPageBuilder
+
+  return getOrCreateFormBuilder('CUT', () => {
+    return new FormBuilder<CutRuleName>({ engine, pageBuilder })
+  })
+}
+
+export function cutPageBuilder(fields: FormLayout<CutRuleName>[]): FormPages<FormLayout<CutRuleName>> {
+  const groupedByNamespace = groupByNamespace<CutRuleName>(fields as CutRuleName[])
+  return groupedByNamespace.map(({ title, elements }) => {
+    if (isInNamespace<CutRuleName>(title as CutRuleName, 'déchets . ordinaires')) {
+      return { title, elements: [DECHETS_ORDINAIRES_TABLE] }
+    }
+    return { title, elements }
+  })
+}
+
+const DECHETS_ORDINAIRES_TABLE: TableFormLayout<CutRuleName> = {
+  title: 'Renseignez ici les quantités de déchets générés chaque semaine',
+  headers: ['Nombre de bennes', 'Taille des bennes', 'Fréquence de ramassage (par semaine)'],
+  rows: [
+    [
+      'déchets . ordinaires . ordures ménagères . nb bennes',
+      'déchets . ordinaires . ordures ménagères . taille benne',
+      'déchets . ordinaires . ordures ménagères . fréquence ramassage',
+    ],
+    [
+      'déchets . ordinaires . emballages et papier . nb bennes',
+      'déchets . ordinaires . emballages et papier . taille benne',
+      'déchets . ordinaires . emballages et papier . fréquence ramassage',
+    ],
+    [
+      'déchets . ordinaires . biodéchets . nb bennes',
+      'déchets . ordinaires . biodéchets . taille benne',
+      'déchets . ordinaires . biodéchets . fréquence ramassage',
+    ],
+    [
+      'déchets . ordinaires . verre . nb bennes',
+      'déchets . ordinaires . verre . taille benne',
+      'déchets . ordinaires . verre . fréquence ramassage',
+    ],
+  ],
 }
