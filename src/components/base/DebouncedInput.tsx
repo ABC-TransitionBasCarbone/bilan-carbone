6eeef0a1b0c2539f9d673ae1199c818ba4dcab05
Emile Rolley <emile.rolley@tuta.io>
2025-12-01 10:48:02 +0100

    fix(inputs): call onChange only if the value is new in DeboncedInput to avoid rerenders

--- src/components/base/DebouncedInput.tsx
+++ src/components/base/DebouncedInput.tsx
@@ -1,5 +1,5 @@
 import { TextField, TextFieldProps } from '@mui/material'
-import { InputHTMLAttributes, useEffect, useState } from 'react'
+import { InputHTMLAttributes, useEffect, useRef, useState } from 'react'
 import styles from '../form/Form.module.css'
 
 interface Props {
@@ -10,21 +10,27 @@ interface Props {
 }
 
 const DebouncedInput = ({
-  value: initialValue,
+  value: initialValue = '',
   onChange,
   debounce,
   size = 'medium',
   ...props
 }: Props & Omit<TextFieldProps & InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'size'>) => {
-  const [value, setValue] = useState(initialValue || '')
+  const [value, setValue] = useState(initialValue)
+  const previousValue = useRef(initialValue)
 
   useEffect(() => {
     setValue(initialValue)
   }, [initialValue])
 
   useEffect(() => {
+    if (previousValue.current === value) {
+      return
+    }
+
     const timeout = setTimeout(() => {
       onChange(value)
+      previousValue.current = value
     }, debounce)
 
     return () => clearTimeout(timeout)
@@ -41,7 +47,9 @@ const DebouncedInput = ({
         },
       }}
       value={value}
-      onChange={(e) => setValue(e.target.value)}
+      onChange={(e) => {
+        setValue(e.target.value)
+      }}
     />
   )
 }
