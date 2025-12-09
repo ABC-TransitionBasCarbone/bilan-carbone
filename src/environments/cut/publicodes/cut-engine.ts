0fb3572725a592da4bc9d637c75ffe4f264fbc8c
Emile Rolley <emile.rolley@tuta.io>
2025-12-09 11:49:01 +0100

    refactor: prettier -w + remove unused functions

--- src/environments/cut/publicodes/cut-engine.ts
+++ src/environments/cut/publicodes/cut-engine.ts
@@ -1,8 +1,7 @@
-import { getOrCreateEngine, getOrCreateFormBuilder } from '@/lib/publicodes/singletons'
+import { getOrCreateEngine } from '@/lib/publicodes/singletons'
 import rules from '@abc-transitionbascarbone/publicodes-count'
-import { FormBuilder, FormLayout, FormPages, groupLayout, simpleLayout, tableLayout } from '@publicodes/forms'
 import Engine from 'publicodes'
-import { CutPublicodesEngine, CutRuleName } from './types'
+import { CutPublicodesEngine } from './types'
 
 /**
  * Returns a singleton instance of the Publicodes {@link Engine} configured
@@ -16,14 +15,5 @@ export function getCutEngine(): CutPublicodesEngine {
         filterNotApplicablePossibilities: true,
       },
     })
-  })
-}
-
-export function getCutFormBuilder(): FormBuilder<CutRuleName> {
-  const engine = getCutEngine()
-
-  return getOrCreateFormBuilder('CUT', () => {
-    // TODO: implement a custom page builder to manage complex layouts
-    return new FormBuilder<CutRuleName>({ engine })
   })
 }
