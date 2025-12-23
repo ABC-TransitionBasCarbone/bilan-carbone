78e3fc5f7980078a3b8a28f0f9b8a4ae803ac8ed
jilink <jimmyfac1@gmail.com>
2025-12-23 11:34:06 +0100

    feat: select All posts/subposts default when adding contributor

--- src/components/study/rights/NewStudyContributorForm.tsx
+++ src/components/study/rights/NewStudyContributorForm.tsx
@@ -43,7 +43,6 @@ const NewStudyContributorForm = ({ study, accounts }: Props) => {
     defaultValues: {
       studyId: study.id,
       email: '',
-      subPosts: {},
     },
   })
 
@@ -82,7 +81,7 @@ const NewStudyContributorForm = ({ study, accounts }: Props) => {
         label={t('email')}
         trim
       />
-      <MultiplePosts form={form} context="studyContributor" />
+      <MultiplePosts form={form} context="studyContributor" selectAll />
       <LoadingButton type="submit" loading={form.formState.isSubmitting} data-testid="study-contributor-create-button">
         {t('create')}
       </LoadingButton>
