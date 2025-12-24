d4bfd4353afd4f9fda24336bbbe82b5c2ee9640b
lfo <louis@dewolf.fr>
2025-12-24 11:39:49 +0100

    add constructionYear in emission source forms

--- src/tests/utils/models/emissionSource.ts
+++ src/tests/utils/models/emissionSource.ts
@@ -16,6 +16,7 @@ export const mockedDbEmissionSource = {
   createdAt: new Date('2025-01-01T00:00:00.000Z'),
   updatedAt: new Date('2025-01-01T00:00:00.000Z'),
   caracterisation: null,
+  constructionYear: null,
   value: null,
   reliability: null,
   technicalRepresentativeness: null,
