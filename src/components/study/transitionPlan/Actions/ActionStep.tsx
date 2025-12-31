7ba86c96268be03f31e066aad1b5467f2e261cba
Chloe M <chloe.molle@neuf.fr>
2025-12-31 11:58:30 +0100

    first try

--- src/components/study/transitionPlan/Actions/ActionStep.tsx
+++ src/components/study/transitionPlan/Actions/ActionStep.tsx
@@ -2,7 +2,7 @@ import { FormTextField } from '@/components/form/TextField'
 import { ActionStepCommand, AddActionFormCommand } from '@/services/serverFunctions/transitionPlan.command'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { Delete, DragIndicator } from '@mui/icons-material'
+import { ArrowRight, Delete, DragIndicator } from '@mui/icons-material'
 import { Box, IconButton } from '@mui/material'
 import classNames from 'classnames'
 import { useTranslations } from 'next-intl'
@@ -34,40 +34,43 @@ export const ActionStep = ({ step, index, control, onDelete }: Props) => {
   }
 
   return (
-    <Box
-      ref={setNodeRef}
-      style={styleProps}
-      className={classNames(styles.stepBlock, 'flex-col py-2 px1')}
-      {...attributes}
-      {...listeners}
-    >
-      <Box className={classNames('flex justify-between align-center mb-2')}>
-        <Box className={classNames('flex align-center')}>
-          <Box className={classNames(styles.dragHandle, 'flex align-center')}>
-            <DragIndicator fontSize="small" />
-          </Box>
-          <Box className={styles.stepNumber}>{index + 1}</Box>
-        </Box>
-        <IconButton size="small" onClick={() => onDelete(index)} onPointerDown={(e) => e.stopPropagation()}>
-          <Delete fontSize="small" />
-        </IconButton>
-      </Box>
+    <div className="flex flex-row grow align-center justify-center">
       <Box
-        className={classNames('flex-col gapped-2 grow')}
-        onPointerDown={(e) => e.stopPropagation()}
-        onKeyDown={(e) => e.stopPropagation()}
+        ref={setNodeRef}
+        style={styleProps}
+        className={classNames(styles.stepBlock, 'flex-col py-2 px1 grow')}
+        {...attributes}
+        {...listeners}
       >
-        <FormTextField
-          control={control}
-          name={`steps.${index}.title`}
-          fullWidth
-          placeholder={t('stepTitlePlaceholder')}
-          defaultValue={step.title || ''}
-          multiline
-          expandable={false}
-          maxRows={2}
-        />
+        <Box className={classNames('flex justify-between align-center mb-2')}>
+          <Box className={classNames('flex align-center')}>
+            <Box className={classNames(styles.dragHandle, 'flex align-center')}>
+              <DragIndicator fontSize="small" />
+            </Box>
+            <Box className={styles.stepNumber}>{index + 1}</Box>
+          </Box>
+          <IconButton size="small" onClick={() => onDelete(index)} onPointerDown={(e) => e.stopPropagation()}>
+            <Delete fontSize="small" />
+          </IconButton>
+        </Box>
+        <Box
+          className={classNames('flex-col gapped-2 grow')}
+          onPointerDown={(e) => e.stopPropagation()}
+          onKeyDown={(e) => e.stopPropagation()}
+        >
+          <FormTextField
+            control={control}
+            name={`steps.${index}.title`}
+            fullWidth
+            placeholder={t('stepTitlePlaceholder')}
+            defaultValue={step.title || ''}
+            multiline
+            expandable
+            maxRows={2}
+          />
+        </Box>
       </Box>
-    </Box>
+      {index + (1 % 3) !== 0 && <ArrowRight key={`arrow-${index}`} />}
+    </div>
   )
 }
--- src/components/study/transitionPlan/Actions/ActionStepsList.tsx
+++ src/components/study/transitionPlan/Actions/ActionStepsList.tsx
@@ -2,7 +2,7 @@ import { CustomFormLabel } from '@/components/form/CustomFormLabel'
 import { AddActionFormCommand } from '@/services/serverFunctions/transitionPlan.command'
 import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core'
 import { SortableContext, rectSortingStrategy } from '@dnd-kit/sortable'
-import { Add, ArrowRight } from '@mui/icons-material'
+import { Add } from '@mui/icons-material'
 import { Box, FormHelperText, IconButton } from '@mui/material'
 import classNames from 'classnames'
 import { useTranslations } from 'next-intl'
@@ -50,23 +50,17 @@ const ActionStepsList = ({ control, errors }: Props) => {
       <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
         <SortableContext items={fields.map((f) => f.id)} strategy={rectSortingStrategy}>
           <Box className={classNames('flex wrap align-center w100', styles.stepsContainer)}>
-            {fields.flatMap((field, index) => {
-              const elements = [
-                <ActionStep
-                  key={field.id}
-                  step={field}
-                  index={index}
-                  control={control}
-                  onDelete={handleDelete}
-                  errors={errors}
-                />,
-              ]
-              if ((index + 1) % 3 !== 0) {
-                elements.push(<ArrowRight key={`arrow-${field.id}`} />)
-              }
-              return elements
-            })}
-            <Box className={classNames(styles.addStepButtonContainer, 'flex-cc')}>
+            {fields.flatMap((field, index) => (
+              <ActionStep
+                key={field.id}
+                step={field}
+                index={index}
+                control={control}
+                onDelete={handleDelete}
+                errors={errors}
+              />
+            ))}
+            <Box className={classNames(styles.addStepButtonContainer, 'flex-cc w100')}>
               <IconButton onClick={handleAdd} className={styles.addStepButton}>
                 <Add />
               </IconButton>
