f6d271548b61beb40b7242cb2e985ddc6e070033
lfo <louis@dewolf.fr>
2026-01-07 16:33:47 +0100

    apply suggestions

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -5,3 +5,5 @@ export const toTitleCase = (str: string) => {
   const formatted = str.toLowerCase().replace(/_/g, ' ')
   return formatted.charAt(0).toUpperCase() + formatted.slice(1)
 }
+
+export const toCamelCase = (str: string) => str.charAt(0).toLowerCase() + str.slice(1)
