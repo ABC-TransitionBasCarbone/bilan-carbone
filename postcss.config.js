ef65c4d5fa01ce03b65ded878fed1597b5fb5fcc
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2026-01-08 10:11:38 +0100

    change postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,21 @@
+const postcssImport = require('postcss-import');
+
 module.exports = {
   plugins: [
-    [
-      'postcss-import',
-      {
-        // Do not transform @import css rules
-        filter: () => false,
+    postcssImport({
+      filter: () => false,
+    }),
+    require('postcss-nested'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009',
+        grid: 'autoplace',
       },
-    ],
-    'postcss-nested',
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-          grid: 'autoplace',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
+      stage: 3,
+      features: {
+        'custom-properties': false,
       },
-    ],
+    }),
   ],
-}
+};
