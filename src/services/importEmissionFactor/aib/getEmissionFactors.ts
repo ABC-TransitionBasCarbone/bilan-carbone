4967f4ec250ea7ba596636763c61809462ff2c88
lfo <louis@dewolf.fr>
2026-01-13 17:41:30 +0100

    add aib emission factors

---
+++ src/services/importEmissionFactor/aib/getEmissionFactors.ts
@@ -0,0 +1,15 @@
+import { EmissionFactorBase, Import, SubPost } from '@prisma/client'
+import { getEmissionFactorsFromCSV as getEmissionFactors } from '../getEmissionFactorsFromCSV'
+import { ImportEmissionFactor, mapEmissionFactors } from '../import'
+
+const source = Import.AIB
+
+const getSubPostsFunc = () => [SubPost.Electricite]
+
+const mapAIBEmissionFactors = (emissionFactor: ImportEmissionFactor, versionId: string) => ({
+  ...mapEmissionFactors(emissionFactor, source, versionId, getSubPostsFunc),
+  base: EmissionFactorBase.MarketBased,
+})
+
+export const getEmissionFactorsFromCSV = async (name: string, file: string) =>
+  getEmissionFactors(name, file, source, mapAIBEmissionFactors)
