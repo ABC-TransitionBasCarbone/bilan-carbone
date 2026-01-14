f6afabdb41d044338ded4e0d46244543944bc3dc
lfo <louis@dewolf.fr>
2026-01-14 11:24:19 +0100

    add emission factor base in selection modal

--- src/components/emissionFactor/BaseChip.tsx
+++ src/components/emissionFactor/BaseChip.tsx
@@ -14,7 +14,7 @@ const BaseChip = ({ base, withLabel }: Props) => {
   const Icon = base === EmissionFactorBase.LocationBased ? LocationOn : LocalGroceryStore
 
   return (
-    <div className={classNames(styles.chip, styles[base], 'flex align-center px1 py-2')}>
+    <div className={classNames(styles.chip, styles[base], 'flex align-center px1 py-2')} title={t(base)}>
       <Icon />
       {withLabel && <span className="ml-2">{t(base)}</span>}
     </div>
