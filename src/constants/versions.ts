47642f8447d45114d67dee55331b1d094f3693a1
Emile Rolley <emile.rolley@tuta.io>
2026-01-19 17:11:49 +0100

    fix(publicodes): add the package name in the stored model version
    It's to differenciates between multiple publicodes models

--- src/constants/versions.ts
+++ src/constants/versions.ts
@@ -2,6 +2,6 @@ import mainPackage from '../../package.json'
 import clicksonPackage from '../../publicodes-packages/publicodes-clickson/package.json'
 import countPackage from '../../publicodes-packages/publicodes-count/package.json'
 
-export const PUBLICODES_COUNT_VERSION = countPackage.version // "0.1.0"
-export const PUBLICODES_CLICKSON_VERSION = clicksonPackage.version // "0.1.0"
+export const PUBLICODES_COUNT_VERSION = `${countPackage.name}@${countPackage.version}`
+export const PUBLICODES_CLICKSON_VERSION = `${clicksonPackage.name}@${clicksonPackage.version}`
 export const PUBLICODES_ENGINE_VERSION = mainPackage.dependencies.publicodes.replace('^', '') // "1.9.1"
