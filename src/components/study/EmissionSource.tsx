274c91472ac8f848d3c5b9db1c6b6824fe57bdbc
lfo <louis@dewolf.fr>
2026-01-20 15:05:19 +0100

    fix rebase conflicts

--- src/components/study/EmissionSource.tsx
+++ src/components/study/EmissionSource.tsx
@@ -278,7 +278,7 @@ const EmissionSource = ({
               <p className={styles.resultText} data-testid="emission-source-value">
                 {`${formatNumber(emissionResults.emissionValue / STUDY_UNIT_VALUES[study.resultsUnit])} ${tResultstUnits(study.resultsUnit)}`}
               </p>
-              {emissionResults.emissionValue && (
+              {!!emissionResults.emissionValue && (
                 <p
                   className={classNames(styles.resultQuality, styles.resultText)}
                   data-testid="emission-source-quality"
