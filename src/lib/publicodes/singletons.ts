852f139d5cde6d6daf1237168acb97b5eb7665d9
Emile Rolley <emile.rolley@tuta.io>
2026-01-26 10:54:04 +0100

    fix(test): correctly use local publicodes packages for unit-testing

--- src/lib/publicodes/singletons.ts
+++ src/lib/publicodes/singletons.ts
@@ -3,10 +3,6 @@ import Engine from 'publicodes'
 
 const engineInstances = new Map<Environment, Engine>()
 
-/**
- * Generic function to get or create a singleton instance for a given environment.
- * If an instance does not already exist, it uses the provided factory function to create one.
- */
 function getOrCreateInstance<T>(cache: Map<Environment, unknown>, key: Environment, factory: () => T): T {
   if (!cache.has(key)) {
     cache.set(key, factory())
@@ -15,11 +11,6 @@ function getOrCreateInstance<T>(cache: Map<Environment, unknown>, key: Environme
   return cache.get(key) as T
 }
 
-/**
- * Returns a singleton instance of a Publicodes {@link Engine} for the given
- * environment. If an instance does not already exist for the specified
- * environment, it uses the provided `createEngine` function to create one.
- */
 export function getOrCreateEngine<RuleName extends string>(
   key: Environment,
   createEngine: () => Engine<RuleName>,
