597821cacc289ae27ec80678558bfef6dd7437ae
Chloe M <chloe.molle@neuf.fr>
2026-01-29 14:41:32 +0100

    filter fe by base

--- src/components/base/MultiSelectAll.tsx
+++ src/components/base/MultiSelectAll.tsx
@@ -1,35 +1,48 @@
 import { useUnitLabel } from '@/services/unit'
+import { Translations } from '@/types/translation'
 import { Checkbox, ListItemText, MenuItem, Select, SelectChangeEvent } from '@mui/material'
-import { useMemo } from 'react'
+import { useTranslations } from 'next-intl'
 
-interface Props {
+interface Props<T> {
   id: string
-  renderValue: () => string
-  value: string[]
-  allValues: string[]
-  setValues: (allValues: string[]) => void
+  values: T[]
+  allValues: T[]
+  setValues: (allValues: T[]) => void
+  t: Translations | ReturnType<typeof useUnitLabel>
 }
 
-const MultiSelect = ({ id, renderValue, value, allValues, setValues }: Props) => {
-  const t = useUnitLabel()
-  const allUnitsSelected = useMemo(
-    () => value.filter((unit) => unit !== 'all').length === allValues.length,
-    [value, allValues],
-  )
+const MultiSelect = <T extends string>({ id, values, allValues, setValues, t }: Props<T>) => {
+  const tCommon = useTranslations('common')
+  const valuesWithAllHandled = (allValues.length === values.length ? [...values, 'all'] : values) as (T | 'all')[]
 
-  const onChange = (event: SelectChangeEvent<string[]>) => {
+  const renderValue = () => {
+    if (valuesWithAllHandled.includes('all')) {
+      return tCommon('all')
+    } else if (valuesWithAllHandled.length === 0) {
+      return tCommon('none')
+    }
+
+    return valuesWithAllHandled.map((v) => t(v)).join(', ')
+  }
+
+  const onChange = (event: SelectChangeEvent<string | (T | 'all')[]>) => {
     const {
-      target: { value },
+      target: { value: newValues },
     } = event
 
-    const allSelected = (value as unknown as string[]).filter((unit) => unit !== 'all').length === allValues.length
+    if (!Array.isArray(newValues)) {
+      return
+    }
 
-    if ((value as unknown as string[]).includes('all') !== allUnitsSelected) {
-      setValues(allSelected ? [] : [...allValues, 'all'])
+    const allWasSelected = values.length === allValues.length
+    const allSelected = newValues.includes('all') || (newValues.length === allValues.length && !allWasSelected)
+
+    if (allSelected && !allWasSelected) {
+      setValues(allValues)
+    } else if (allWasSelected && !allSelected) {
+      setValues([])
     } else {
-      const target = allSelected
-        ? [...allValues, 'all']
-        : (value as unknown as string[]).filter((unit) => unit !== 'all')
+      const target = newValues.filter((val): val is T => val !== 'all')
       setValues(target)
     }
   }
@@ -38,21 +51,24 @@ const MultiSelect = ({ id, renderValue, value, allValues, setValues }: Props) =>
     <Select
       id={`${id}-selector`}
       labelId={`${id}-selector`}
-      value={value}
+      value={valuesWithAllHandled}
       onChange={onChange}
       renderValue={renderValue}
       multiple
+      displayEmpty
     >
       <MenuItem key={`${id}-item-all`} value="all">
-        <Checkbox checked={value.includes('all')} />
-        <ListItemText primary={t(value.includes('all') ? 'unSelectAll' : 'selectAll')} />
+        <Checkbox checked={valuesWithAllHandled.includes('all')} />
+        <ListItemText
+          primary={tCommon(valuesWithAllHandled.includes('all') ? 'action.unselectAll' : 'action.selectAll')}
+        />
       </MenuItem>
       {allValues
         .filter((option) => option !== '')
         .sort((a, b) => t(a).localeCompare(t(b)))
         .map((option) => (
           <MenuItem key={`${id}-item-${option}`} value={option || ''}>
-            <Checkbox checked={value.includes(option)} />
+            <Checkbox checked={valuesWithAllHandled.includes(option)} />
             <ListItemText primary={t(option)} />
           </MenuItem>
         ))}
