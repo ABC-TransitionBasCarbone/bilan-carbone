03f15919b88e0586fcad143c16f182665df2ff80
jilink <jimmyfac1@gmail.com>
2026-02-10 10:01:31 +0100

    feat: getting actualities from locale

--- src/db/actuality.ts
+++ src/db/actuality.ts
@@ -1,9 +1,16 @@
+import { getLocale } from '@/i18n/locale'
 import { Prisma } from '@prisma/client'
 import { prismaClient } from './client'
 
-export const getAllActualities = () => prismaClient.actuality.findMany({ orderBy: { createdAt: 'desc' } })
+export const getAllActualitiesLocale = async () => {
+  const locale = await getLocale()
+  return prismaClient.actuality.findMany({ where: { language: locale }, orderBy: { createdAt: 'desc' } })
+}
 
-export const getMainActualities = () => prismaClient.actuality.findMany({ orderBy: { createdAt: 'desc' }, take: 3 })
+export const getMainActualitiesLocale = async () => {
+  const locale = await getLocale()
+  return prismaClient.actuality.findMany({ where: { language: locale }, orderBy: { createdAt: 'desc' }, take: 3 })
+}
 
 export const createActualities = async (data: Prisma.ActualityCreateInput[]) =>
   prismaClient.actuality.createMany({ data })
