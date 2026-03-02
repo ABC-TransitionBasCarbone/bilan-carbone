a6e1c0083190b9fd786547de89cdf98c42609b5f
Alexandre Phiev <alexandre.phiev@gmail.com>
2026-03-02 09:14:20 +0100

    feat: add onboarding clickson

--- src/environments/clickson/home/UserView.tsx
+++ src/environments/clickson/home/UserView.tsx
@@ -1,16 +1,35 @@
+import Onboarding from '@/components/onboarding/Onboarding'
 import StudiesContainer from '@/components/study/StudiesContainer'
+import { environmentWithOnboarding } from '@/constants/environments'
+import { getAccountOrganizationVersions } from '@/db/account'
+import { OrganizationVersionWithOrganization } from '@/db/organization'
 import { default as SimplifiedUserView } from '@/environments/simplified/home/UserView'
 import { UserSession } from 'next-auth'
+
 interface Props {
   account: UserSession
 }
 
-const UserView = ({ account }: Props) => {
+const UserView = async ({ account }: Props) => {
   if (!account.organizationVersionId) {
     return <StudiesContainer user={account} />
   }
 
-  return <SimplifiedUserView account={account} />
+  const organizationVersions = await getAccountOrganizationVersions(account.accountId)
+  const userOrganizationVersion = organizationVersions.find(
+    (organizationVersion) => organizationVersion.id === account.organizationVersionId,
+  ) as OrganizationVersionWithOrganization
+
+  return (
+    <>
+      <SimplifiedUserView account={account} />
+      {userOrganizationVersion &&
+        !userOrganizationVersion.onboarded &&
+        environmentWithOnboarding.includes(userOrganizationVersion.environment) && (
+          <Onboarding user={account} organizationVersion={userOrganizationVersion} />
+        )}
+    </>
+  )
 }
 
 export default UserView
