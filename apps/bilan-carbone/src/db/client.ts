47084d9ff2a1c2abc90b9cd5f0c9a2614bf42153
jilink <jimmyfac1@gmail.com>
2026-03-23 13:52:05 +0100

    dependency: prisma 7 install and config + env enum

--- apps/bilan-carbone/src/db/client.ts
+++ apps/bilan-carbone/src/db/client.ts
@@ -1,12 +1,19 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 const globalForPrisma = global as unknown as {
   prismaClient: PrismaClient | undefined
 }
 
+const adapter = new PrismaPg({
+  connectionString: process.env.POSTGRES_PRISMA_URL,
+  ssl: { rejectUnauthorized: false },
+})
+
 // https://www.prisma.io/docs/orm/prisma-client/queries/excluding-fields
 const defaultPrismaClient = new PrismaClient({
+  adapter,
   omit: {
     user: { password: true, resetToken: true },
   },
