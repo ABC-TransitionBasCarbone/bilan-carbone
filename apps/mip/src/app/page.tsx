c04baee76e1e106850c3aa735da9c880f816e5bf
jilink <jimmyfac1@gmail.com>
2026-05-04 12:02:19 +0200

    feat: can continue or restart + fix hydration

--- apps/mip/src/app/page.tsx
+++ apps/mip/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation'
-import { v4 as uuidv4 } from 'uuid'
 
 export default function Home() {
-  redirect(`/survey/${uuidv4()}`)
+  redirect(`/survey/sample-survey-1`)
 }
