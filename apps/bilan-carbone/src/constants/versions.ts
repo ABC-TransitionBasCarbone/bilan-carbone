8088571e166e573f3795a03a2930d0c51c72a483
jilink <jimmyfac1@gmail.com>
2026-05-07 14:38:06 +0200

    refactor: moving publicodes package to common and adding mip publicodes

--- apps/bilan-carbone/src/constants/versions.ts
+++ apps/bilan-carbone/src/constants/versions.ts
@@ -1,7 +1,7 @@
+import clicksonPackage from '../../../../packages/publicodes-packages/publicodes-clickson/package.json'
+import countPackage from '../../../../packages/publicodes-packages/publicodes-count/package.json'
+import tiltPackage from '../../../../packages/publicodes-packages/publicodes-tilt/package.json'
 import mainPackage from '../../package.json'
-import clicksonPackage from '../../publicodes-packages/publicodes-clickson/package.json'
-import countPackage from '../../publicodes-packages/publicodes-count/package.json'
-import tiltPackage from '../../publicodes-packages/publicodes-tilt/package.json'
 
 export const PUBLICODES_COUNT_VERSION = `${countPackage.name}@${countPackage.version}`
 export const PUBLICODES_CLICKSON_VERSION = `${clicksonPackage.name}@${clicksonPackage.version}`
