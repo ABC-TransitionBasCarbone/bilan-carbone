de81fce42bcdc1ba923282965189b93d7954f81c
jilink <jimmyfac1@gmail.com>
2026-05-19 16:32:43 +0200

    revert: mip publicodes package and translations

--- apps/mip/src/publicodes/mip-engine.ts
+++ apps/mip/src/publicodes/mip-engine.ts
@@ -1,16 +1,14 @@
+import rules from '@abc-transitionbascarbone/publicodes-mip'
 import Engine from 'publicodes'
 
 let instance: Engine | null = null
 
 export function getMipEngine(): Engine {
   if (!instance) {
-    instance = new Engine(
-      {},
-      {
-        flag: { filterNotApplicablePossibilities: true },
-        strict: { situation: false },
-      },
-    )
+    instance = new Engine(rules, {
+      flag: { filterNotApplicablePossibilities: true },
+      strict: { situation: false },
+    })
   }
   return instance
 }
