5dc102577163ac2cf9161b03847619c80342f044
jilink <jimmyfac1@gmail.com>
2026-06-04 16:19:09 +0200

    feat: Team member functions

--- apps/mip/src/app/(dashboard)/equipe/page.tsx
+++ apps/mip/src/app/(dashboard)/equipe/page.tsx
@@ -1,19 +1,18 @@
 import withAuth, { UserSessionProps } from '@/components/hoc/withAuth'
+import TeamPage from '@/components/pages/Team'
+import { getAccountMipFromUserOrganization } from '@/db/accountMip'
 // import NotFound from '@/components/pages/NotFound'
 
 export const revalidate = 0
 
 const Team = async ({ user }: UserSessionProps) => {
+  console.log('sesssion, ', user)
   // if (!user.organizationVersionId) {
   //   return <NotFound />
   // }
-  // const [team, isCR] = await Promise.all([
-  //   getAccountFromUserOrganization(user),
-  //   getOrganizationVersionIsCR(user.organizationVersionId),
-  // ])
+  const team = await getAccountMipFromUserOrganization(user)
 
-  // return <TeamPage user={user} team={team} crOrga={isCR} />
-  return <p>Team page</p>
+  return <TeamPage user={user} team={team} />
 }
 
 export default withAuth(Team)
