b009008f82e27fd0482efb9d100010f61a6214d9
jilink <jimmyfac1@gmail.com>
2026-06-04 17:27:25 +0200

    refactor: common ResetLinkAlreadyUsed

--- apps/bilan-carbone/src/components/auth/ResetForm.tsx
+++ apps/bilan-carbone/src/components/auth/ResetForm.tsx
@@ -9,7 +9,7 @@ import { UserSession } from 'next-auth'
 import { useTranslations } from 'next-intl'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import ResetLinkAlreadyUsed from '../pages/ResetLinkAlreadyUsed'
+import ResetLinkAlreadyUsed from '@abc-transitionbascarbone/components/src/pages/ResetLinkAlreadyUsed'
 
 interface Props {
   user?: UserSession
