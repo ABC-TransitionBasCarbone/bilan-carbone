5e6ee04db358bbd9206b6dd7dcdd12dedfa2a206
jilink <jimmyfac1@gmail.com>
2026-06-04 16:19:09 +0200

    feat: Team member functions

--- apps/mip/src/db/accountMip.ts
+++ apps/mip/src/db/accountMip.ts
@@ -1,9 +1,29 @@
+import { UserSession } from 'next-auth'
 import { AccountMipWithUserSelect } from './accountMip.select'
 import { prismaClient } from './client.server'
-
 
 export const getAccountMipById = (id: string) =>
   prismaClient.accountMip.findUnique({
     where: { id },
     select: AccountMipWithUserSelect,
   })
+
+export const getAccountMipFromUserOrganization = (user: UserSession) =>
+  prismaClient.accountMip.findMany({
+    select: {
+      user: {
+        select: {
+          email: true,
+          firstName: true,
+          lastName: true,
+          level: true,
+          updatedAt: true,
+        },
+      },
+      status: true,
+      role: true,
+      updatedAt: true,
+    },
+    orderBy: { user: { email: 'asc' } },
+  })
+export type TeamMember = AsyncReturnType<typeof getAccountMipFromUserOrganization>[number]
