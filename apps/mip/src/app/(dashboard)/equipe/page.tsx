6089c35d52eb1aa6d3d2ebdefb8bf25ce85a0227
jilink <jimmyfac1@gmail.com>
2026-06-08 09:09:02 +0200

    feat: notfound page translations

--- apps/mip/src/app/(dashboard)/equipe/page.tsx
+++ apps/mip/src/app/(dashboard)/equipe/page.tsx
@@ -1,15 +1,14 @@
 import withAuth, { UserSessionProps } from '@/components/hoc/withAuth'
 import TeamPage from '@/components/pages/Team'
 import { getAccountMipFromUserOrganization } from '@/db/accountMip'
-// import NotFound from '@abc-transitionbascarbone/components/src/pages/NotFound'
+import NotFound from '@abc-transitionbascarbone/components/src/pages/NotFound'
 
 export const revalidate = 0
 
 const Team = async ({ user }: UserSessionProps) => {
-  console.log('sesssion, ', user)
-  // if (!user.organizationVersionId) {
-  //   return <NotFound />
-  // }
+  if (!user.organizationVersionMipId) {
+    return <NotFound />
+  }
   const team = await getAccountMipFromUserOrganization(user)
 
   return <TeamPage user={user} team={team} />
