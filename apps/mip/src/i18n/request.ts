6c4947bf631765beb47b4b55ca87938baab4457b
jilink <jimmyfac1@gmail.com>
2026-06-09 09:30:20 +0200

    removing publicode rules translations mip

--- apps/mip/src/i18n/request.ts
+++ apps/mip/src/i18n/request.ts
@@ -13,16 +13,8 @@ export default getRequestConfig(async () => {
     .then((m) => m.default)
     .catch(() => import(`./translations/${Locale.FR}/mip.json`).then((m) => m.default))
 
-  let publicodesRules = {}
-  try {
-    publicodesRules = (await import(`../../../../packages/i18n/translations/${locale}/publicodes/mip-rules.json`))
-      .default
-  } catch {
-    console.log(`No publicodes rules translation file for locale: ${locale}`)
-  }
-
   return {
     locale,
-    messages: { ...commonMessages, ...mipMessages, ...publicodesRules },
+    messages: { ...commonMessages, ...mipMessages },
   }
 })
