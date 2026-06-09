f4ae31c03cffbbc1527c5b8bbcdf6edb3987744e
Romain Crevecoeur <romain.crevecoeur@lilo.org>
2026-06-09 11:07:24 +0200

    create standalone build for mip

--- apps/mip/next.config.mjs
+++ apps/mip/next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // we use the standalone output to be able to reduce bundle size by copying only the necessary assets in the standalone folder (see copy-assets.js)
   reactStrictMode: true,
   transpilePackages: ['@mui/material', '@mui/icons-material', '@abc-transitionbascarbone/survey'],
   reactCompiler: {
