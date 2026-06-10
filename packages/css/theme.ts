b71442e89b8b95e35ce8eb93d291904bb3d536bb
jilink <jimmyfac1@gmail.com>
2026-06-10 14:32:26 +0200

    feat: theme

--- packages/css/theme.ts
+++ packages/css/theme.ts
@@ -4,7 +4,7 @@
 
 import { createTheme } from '@mui/material/styles'
 
-const globalTheme = createTheme({
+const base = createTheme({
   cssVariables: true,
   typography: {
     fontFamily: 'gilroy-regular, sans-serif',
@@ -38,6 +38,143 @@ const globalTheme = createTheme({
     h6: {
       fontSize: '1.25rem',
       lineHeight: '1.75rem',
+    },
+  },
+})
+
+const globalTheme = createTheme(base, {
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1360,
+      xl: 1536,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '0.5rem',
+        },
+        outlined: {
+          backgroundColor: base.palette.common.white,
+          borderStyle: 'solid',
+        },
+        containedSecondary: {
+          '&:hover': {
+            backgroundColor: '#002D7A',
+          },
+        },
+      },
+    },
+    MuiLinearProgress: {
+      styleOverrides: {
+        '& .MuiLinearProgress-bar1': {
+          backgroundColor: base.palette.success.main,
+        },
+        '& .MuiLinearProgress-bar2': {
+          backgroundColor: base.palette.grey[200],
+        },
+        '& .MuiLinearProgress-barColorPrimary': {
+          backgroundColor: base.palette.success.main,
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& .MuiOutlinedInput-root': {
+            borderRadius: '0.5rem',
+            '& fieldset': {
+              borderRadius: '0.5rem',
+            },
+          },
+          '& input': {
+            '&:-webkit-autofill': {
+              WebkitBoxShadow: '0 0 0 1000px #ffffff inset',
+              WebkitTextFillColor: 'inherit',
+            },
+          },
+        },
+      },
+    },
+    MuiPickersOutlinedInput: {
+      styleOverrides: {
+        root: {
+          borderRadius: '0.5rem',
+          '& fieldset': {
+            borderRadius: '0.5rem',
+          },
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          borderRadius: '0.5rem',
+          '& fieldset': {
+            borderRadius: '0.5rem',
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          borderRadius: '0.5rem',
+          '& fieldset': {
+            borderRadius: '0.5rem',
+          },
+        },
+      },
+    },
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          fontSize: '1rem',
+          lineHeight: 'normal',
+        },
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          fontSize: '1rem !important',
+        },
+      },
+    },
+  },
+  custom: {
+    palette: {
+      error: {
+        background: '#f6b8b8',
+      },
+    },
+    box: {
+      backgroundColor: base.palette.background.paper,
+      color: base.palette.text.primary,
+      borderRadius: '1rem',
+      borderStyle: 'solid',
+      borderWidth: '0.0625rem',
+      borderColor: base.palette.grey[300],
+      padding: '1rem',
+    },
+    navbar: {
+      organizationToolbar: {
+        border: '0.125rem solid rgba(27, 91, 245, 0.1)',
+      },
+      text: {
+        fontFamily: 'gilroy-regular, sans-serif',
+        color: '#FFFFFF',
+        fontWeight: 600,
+        textTransform: 'uppercase',
+        fontSize: '1rem',
+        '&:hover': {
+          color: base.palette.secondary.main,
+        },
+      },
     },
   },
 })
