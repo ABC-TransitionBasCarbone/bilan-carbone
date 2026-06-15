688363842fac380e5232ce46baecd311f38aebc3
jilink <jimmyfac1@gmail.com>
2026-06-15 09:18:18 +0200

    fix after review
    - canChangeRole function

--- apps/mip/src/services/permissions/user.ts
+++ apps/mip/src/services/permissions/user.ts
@@ -1,3 +1,30 @@
+import { AccountMipWithUser } from '@/types/accountMip.types'
+import { canEditMemberRole } from '@/utils/user'
 import { Role } from '@abc-transitionbascarbone/db-common/enums'
+import { UserSession } from 'next-auth'
 
 export const canEditSelfRole = (userRole: Role) => userRole === Role.ADMIN || userRole === Role.GESTIONNAIRE
+
+export const canChangeRole = (user: UserSession, member: AccountMipWithUser | null, newRole: Role) => {
+  if (!member) {
+    return false
+  }
+
+  if (user.accountMipId === member.id && !canEditSelfRole(user.role)) {
+    return false
+  }
+
+  if (!canEditMemberRole(user)) {
+    return false
+  }
+
+  if (user.organizationVersionMipId !== member.organizationVersionMipId) {
+    return false
+  }
+
+  if (newRole === Role.SUPER_ADMIN) {
+    return false
+  }
+
+  return true
+}
