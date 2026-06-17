8da19a7d1793ad3b694fc44c6af800f76fb53c11
Romain Crevecoeur <romain.crevecoeur@lilo.org>
2026-06-17 10:53:03 +0200

    fix build

--- copy-assets.js
+++ copy-assets.js
@@ -1,11 +1,14 @@
 const fs = require('fs').promises
 const path = require('path')
 
-const staticSrcPath = path.join(__dirname, '.next/static')
-const staticDestPath = path.join(__dirname, '.next/standalone/apps/bilan-carbone/.next/static')
+const appPathArg = process.argv[2] || 'apps/bilan-carbone'
+const appPath = path.resolve(__dirname, appPathArg)
 
-const publicSrcPath = path.join(__dirname, 'public')
-const publicDestPath = path.join(__dirname, '.next/standalone/apps/bilan-carbone/public')
+const staticSrcPath = path.join(appPath, '.next/static')
+const staticDestPath = path.join(appPath, '.next/standalone', appPathArg, '.next/static')
+
+const publicSrcPath = path.join(appPath, 'public')
+const publicDestPath = path.join(appPath, '.next/standalone', appPathArg, 'public')
 
 function copyAssets(src, dest) {
   return fs
@@ -34,13 +37,24 @@ const redCross = `\x1b[31m\u274C\x1b[0m`
 const greenTick = `\x1b[32m\u2713\x1b[0m`
 
 // Also copy i18n translation files for production i18n support
-const i18nSrcPath = path.join(__dirname, 'src', 'i18n', 'translations')
-const i18nDestPath = path.join(__dirname, '.next/standalone/apps/bilan-carbone/src/i18n/translations')
+const i18nSrcPath = path.join(appPath, 'src', 'i18n', 'translations')
+const i18nDestPath = path.join(appPath, '.next/standalone', appPathArg, 'src/i18n/translations')
 
-Promise.all([
-  copyAssets(staticSrcPath, staticDestPath),
-  copyAssets(publicSrcPath, publicDestPath),
-  copyAssets(i18nSrcPath, i18nDestPath),
-])
-  .then(() => console.log(`${greenTick} Assets copied successfully`))
-  .catch((err) => console.error(`${redCross} Failed to copy assets: ${err}`))
+async function main() {
+  const copyPromises = [copyAssets(staticSrcPath, staticDestPath), copyAssets(publicSrcPath, publicDestPath)]
+
+  try {
+    await fs.access(i18nSrcPath)
+    copyPromises.push(copyAssets(i18nSrcPath, i18nDestPath))
+  } catch {
+    // Some apps do not expose i18n translations from src/i18n/translations.
+  }
+
+  await Promise.all(copyPromises)
+  console.log(`${greenTick} Assets copied successfully for ${appPathArg}`)
+}
+
+main().catch((err) => {
+  console.error(`${redCross} Failed to copy assets for ${appPathArg}: ${err}`)
+  process.exit(1)
+})
