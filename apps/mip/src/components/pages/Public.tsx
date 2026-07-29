e8837b2e3e5679b997467de29c12a4f45e627443
RomainCrevecoeur <romain.crevecoeur@lilo.org>
2026-07-29 16:58:18 +0200

    change mip public text + logos

--- apps/mip/src/components/pages/Public.tsx
+++ apps/mip/src/components/pages/Public.tsx
@@ -19,23 +19,23 @@ const PublicPage = ({ children }: Props) => {
       <div className={classNames('flex wrap w100')}>
         <div className={classNames(styles.info, 'grow p2 text-center')}>
           <p className="title-h4 mb1">{t('explanation.title')}</p>
-          <p className="mb1">{t('explanation.cover.subtitle')}</p>
           <p className="mb1">{t('explanation.why.summary')}</p>
-          <p>{t('explanation.note.summary')}</p>
         </div>
         <div className={classNames(styles.loginForm, 'grow flex-col p2')}>
           <div className={classNames('wrap justify-center gapped-2 mb1')}>
-            {partnerLogos.map((logo) => (
-              <div key={logo.src} className={classNames(styles.loginLogoItem, 'flex-cc')}>
-                <Image
-                  src={logo.src}
-                  alt={logo.alt}
-                  width={88}
-                  height={36}
-                  className={classNames(styles.loginLogoImage, 'wauto h100')}
-                />
-              </div>
-            ))}
+            {partnerLogos
+              .filter((logo) => logo.alt === 'ABC')
+              .map((logo) => (
+                <div key={logo.src} className="flex-cc">
+                  <Image
+                    src={logo.src}
+                    alt={logo.alt}
+                    width={176}
+                    height={72}
+                    className={classNames(styles.loginLogoImage, 'wauto h100')}
+                  />
+                </div>
+              ))}
           </div>
           {children}
         </div>
