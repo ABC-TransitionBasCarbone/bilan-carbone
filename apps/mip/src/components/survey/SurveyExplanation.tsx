57e8f4febd068ec66efef4a37163f9f3f2a1a7b7
Romain Crevecoeur <romain.crevecoeur@lilo.org>
2026-07-29 13:45:21 +0200

    change piublic page for MIP

--- apps/mip/src/components/survey/SurveyExplanation.tsx
+++ apps/mip/src/components/survey/SurveyExplanation.tsx
@@ -1,23 +1,11 @@
 'use client'
 
+import { partnerLogos } from '@/constant'
 import { Button, Container, Typography } from '@mui/material'
 import classNames from 'classnames'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import styles from './Survey.module.css'
-
-type PartnerLogo = {
-  src: string
-  alt: string
-}
-
-const partnerLogos: PartnerLogo[] = [
-  { src: '/logos/partners/abc.png', alt: 'ABC' },
-  { src: '/logos/partners/grdf.png', alt: 'GRDF' },
-  { src: '/logos/partners/ag2r-la-mondiale.png', alt: 'AG2R La Mondiale' },
-  { src: '/logos/partners/edf.png', alt: 'EDF' },
-  { src: '/logos/partners/france-travail.png', alt: 'France Travail' },
-]
 
 interface Props {
   onStart: () => void
