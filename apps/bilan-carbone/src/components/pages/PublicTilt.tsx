e78fa01fc0da76896c043d608dfaeec705008a86
Gabriel Chabah <gabriel.chabah@abc-transitionbascarbone.fr>
2026-08-03 22:16:08 +0200

    Corrected customRich call

--- apps/bilan-carbone/src/components/pages/PublicTilt.tsx
+++ apps/bilan-carbone/src/components/pages/PublicTilt.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { customRich } from '@/i18n/customRich'
+import { customRich } from '@abc-transitionbascarbone/utils/customRich'
 import { switchEnvironment } from '@/i18n/environment'
 import { getLocale, switchLocale } from '@/i18n/locale'
 import PublicContainer from '@abc-transitionbascarbone/components/src/base/PublicContainer'
