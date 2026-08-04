08704a49dd290c3f2f961cb4b0d9180c5ec86d96
Chloe M <chloe.molle@neuf.fr>
2026-08-04 15:01:36 +0200

    add script

---
+++ packages/db-common/prisma/schema/migrations/202608041450_depreciation_in_fabrication/migration.sql
@@ -0,0 +1,6 @@
+UPDATE bilan_carbone.study_emission_sources
+SET depreciation_period = 1
+WHERE bilan_carbone.study_emission_sources.sub_post in (
+  'DeplacementsFabricationDesVehicules',
+  'TransportFabricationDesVehicules'
+)
