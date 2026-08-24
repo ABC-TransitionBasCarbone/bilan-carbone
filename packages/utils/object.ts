4f645432fec6291373b95685a8e18e87c9e458f4
RomainCrevecoeur <romain.crevecoeur@lilo.org>
2026-08-24 14:23:46 +0200

    lint

--- packages/utils/object.ts
+++ packages/utils/object.ts
@@ -1,3 +1,3 @@
 export const isObject = <T extends Record<string, unknown>>(value: unknown): value is T => {
-  return typeof value === 'object' && value !== null && !Array.isArray(value)
+    return typeof value === 'object' && value !== null && !Array.isArray(value)
 }
