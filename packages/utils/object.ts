f6a421059a75edf62d34d173e9d41f7b5d8b1065
RomainCrevecoeur <romain.crevecoeur@lilo.org>
2026-08-26 12:14:46 +0200

    merge deepMerge and mergeObjects into one util function in common

--- packages/utils/object.ts
+++ packages/utils/object.ts
@@ -1,3 +1,25 @@
 export const isObject = <T extends Record<string, unknown>>(value: unknown): value is T => {
   return typeof value === 'object' && value !== null && !Array.isArray(value)
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const mergeObjects = <T extends Record<string, any>>(target: T, ...sources: Partial<T>[]): T => {
+  for (const source of sources) {
+    if (!isObject(source)) {
+      continue
+    }
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key] || !isObject(target[key])) {
+          target[key] = {} as T[Extract<keyof T, string>]
+        }
+        mergeObjects(target[key] as T[Extract<keyof T, string>], source[key] as Partial<T[Extract<keyof T, string>]>)
+      } else {
+        if (source[key] !== undefined) {
+          target[key] = source[key]
+        }
+      }
+    }
+  }
+  return target
+}
