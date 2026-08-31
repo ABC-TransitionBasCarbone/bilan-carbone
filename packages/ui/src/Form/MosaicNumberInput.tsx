d985a73a49cc6469fe535eb23a625474b141c13a
Chloe M <chloe.molle@neuf.fr>
2026-08-31 10:25:47 +0200

    add unit in mosaic input

--- packages/ui/src/Form/MosaicNumberInput.tsx
+++ packages/ui/src/Form/MosaicNumberInput.tsx
@@ -1,21 +1,26 @@
+import { InputAdornment, OutlinedInput } from '@mui/material'
+import type { CSSProperties } from 'react'
 import Button from '../Button/Button'
 import styles from './MosaicNumberInput.module.css'
+import classNames from 'classnames'
 
 type Props = {
   title?: string
   icons?: string
   description?: string
+  unit?: string
   onChange: (value: number) => void
   value?: number
 }
 
-export default function MosaicNumberInput({ title, icons, description, onChange, value, ...props }: Props) {
+export default function MosaicNumberInput({ title, icons, description, onChange, value, unit, ...props }: Props) {
   const rounded = value != null ? Math.ceil(value) : undefined
+  const numLength = String(rounded ?? '').length || 1
 
   return (
     <div className={`${styles.element} p125`}>
       <div className="justify-between align-center">
-        <div className="flex-col">
+        <div className={`flex-col ${styles.text}`}>
           {title && icons ? (
             <span className={`${styles.title} block`}>
               {title}&nbsp;{icons}
@@ -27,20 +32,21 @@ export default function MosaicNumberInput({ title, icons, description, onChange,
           )}
           {description ? <p className={`${styles.description} m0`}>{description.split('\n')[0]}</p> : null}
         </div>
-        <div className="align-center">
+        <div className={`align-center ${styles.controls}`}>
           <Button disabled={!rounded} onClick={() => onChange((rounded ?? 0) - 1)} className={styles.button}>
             <span>-</span>
           </Button>
-          <input
-            className={styles.input}
-            type="number"
-            inputMode="numeric"
-            value={rounded ?? ''}
-            size={String(rounded ?? 0).length}
-            placeholder="0"
-            onChange={(event) => onChange(Math.ceil(Number(event.target.value)))}
-            {...props}
-          />
+          <div className="relative">
+            <OutlinedInput
+              value={rounded ?? ''}
+              className={styles.input}
+              inputProps={{ className: styles.inputField, style: { '--num-len': numLength } as CSSProperties }}
+              placeholder="0"
+              onChange={(event) => onChange(Math.ceil(Number(event.target.value)))}
+              {...props}
+              endAdornment={unit ? <InputAdornment position="end" className={styles.unit}>{unit}</InputAdornment> : undefined}
+            />
+          </div>
           <Button onClick={() => onChange((rounded ?? 0) + 1)} className={styles.button}>
             <span>+</span>
           </Button>
