71fa4727e5b842be764f4c9ef919e9992a9b7423
RomainCrevecoeur <romain.crevecoeur@lilo.org>
2026-09-03 18:26:05 +0200

    remove ununsed call

--- packages/publicodes/form/suggestions.ts
+++ packages/publicodes/form/suggestions.ts
@@ -1,6 +1,5 @@
 import { isObject } from '@abc-transitionbascarbone/utils/object'
 import {
-  getNumericSuggestionEntries,
   getRelativeRuleName,
   isSuggestionInputValue,
   NumericSuggestionEntry,
