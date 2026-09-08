a49ebbb424723ac7736d8c077ba1138e696a7685
RomainCrevecoeur <romain.crevecoeur@lilo.org>
2026-09-08 15:40:36 +0200

    implement ai suggestions

--- packages/publicodes/form/inputFields/SelectInput.tsx
+++ packages/publicodes/form/inputFields/SelectInput.tsx
@@ -46,7 +46,9 @@ const SelectInput = <RuleName extends string>({
     onChange(formElement.id, event.target.value)
   }
 
-  if (disabled && formElement.value) {
+  const hasSetValue = formElement.value !== undefined && formElement.value !== null && formElement.value !== ''
+
+  if (disabled && hasSetValue) {
     return (
       <StyledFormControl fullWidth error={!!errorMessage}>
         <DisabledText variant="body1">{getCurrentValueLabel()}</DisabledText>
