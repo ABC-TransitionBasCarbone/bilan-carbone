43bb2b8d468b8c6dccf8e7651c2600e2ad500121
Romain Crevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2026-09-17 11:07:06 +0200

    lint

--- apps/bilan-carbone/src/services/serverFunctions/user.ts
+++ apps/bilan-carbone/src/services/serverFunctions/user.ts
@@ -132,48 +132,48 @@ export const sendInvitation = async (
     if (existingAccount && existingAccount.status === UserStatus.ACTIVE) {
       return roleOnStudy
         ? sendUserOnStudyInvitationEmail(
-          email,
-          study.name,
-          study.id,
-          organization.name,
-          `${creator.firstName} ${creator.lastName}`,
-          existingAccount.user.firstName,
-          roleOnStudy,
-          env,
-        )
+            email,
+            study.name,
+            study.id,
+            organization.name,
+            `${creator.firstName} ${creator.lastName}`,
+            existingAccount.user.firstName,
+            roleOnStudy,
+            env,
+          )
         : sendContributorInvitationEmail(
-          email,
-          study.name,
-          study.id,
-          organization.name,
-          `${creator.firstName} ${creator.lastName}`,
-          existingAccount.user.firstName,
-          env,
-        )
+            email,
+            study.name,
+            study.id,
+            organization.name,
+            `${creator.firstName} ${creator.lastName}`,
+            existingAccount.user.firstName,
+            env,
+          )
     }
 
     const token = await updateUserResetToken(email, 1 * DAY)
 
     return roleOnStudy
       ? sendNewUserOnStudyInvitationEmail(
-        email,
-        token,
-        study.name,
-        study.id,
-        organization.name,
-        `${creator.firstName} ${creator.lastName}`,
-        roleOnStudy,
-        env,
-      )
+          email,
+          token,
+          study.name,
+          study.id,
+          organization.name,
+          `${creator.firstName} ${creator.lastName}`,
+          roleOnStudy,
+          env,
+        )
       : sendNewContributorInvitationEmail(
-        email,
-        token,
-        study.name,
-        study.id,
-        organization.name,
-        `${creator.firstName} ${creator.lastName}`,
-        env,
-      )
+          email,
+          token,
+          study.name,
+          study.id,
+          organization.name,
+          `${creator.firstName} ${creator.lastName}`,
+          env,
+        )
   })
 
 const sendActivation = async (email: string, fromReset: boolean, env: Environment) => {
@@ -653,9 +653,9 @@ export const signUpWithSiretOrCNC = async (email: string, siretOrCNC: string, en
       organizationVersion = organization?.id
         ? await getOrganizationVersionByOrganizationIdAndEnvironment(organization.id, environment)
         : await createOrganizationWithVersion(
-          { wordpressId: siretOrCNC, name: companyName },
-          { environment: environment },
-        )
+            { wordpressId: siretOrCNC, name: companyName },
+            { environment: environment },
+          )
     }
 
     if (!organizationVersion) {
