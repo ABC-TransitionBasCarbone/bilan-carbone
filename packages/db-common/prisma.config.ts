21a57c3c49385face6f20655863790b997f35465
Romain Crevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2026-09-22 10:54:59 +0200

    refactor: update prisma.config.ts to use a conditional database URL

--- packages/db-common/prisma.config.ts
+++ packages/db-common/prisma.config.ts
@@ -4,6 +4,11 @@ import { defineConfig, env } from 'prisma/config'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const databaseUrl =
+  process.env.POSTGRES_PRISMA_URL ??
+  (process.argv.includes('generate')
+    ? 'postgresql://prisma:prisma@localhost:5432/prisma'
+    : env('POSTGRES_PRISMA_URL'))
 
 export default defineConfig({
   schema: path.join(__dirname, 'prisma/schema'),
@@ -12,6 +17,6 @@ export default defineConfig({
     seed: 'tsx prisma/seed/index.ts',
   },
   datasource: {
-    url: env('POSTGRES_PRISMA_URL'),
+    url: databaseUrl,
   },
 })
